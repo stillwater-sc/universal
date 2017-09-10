4cac59bc6401d334d8a8aeacb37c9d9e109187ea
Ravenwater <theo@stillwater-sc.com>
2017-09-10 08:43:08 -0400

    Adding standard 16-bit posit test skeleton

---
+++ tests/posit/16bit_posit.cpp
@@ -0,0 +1,29 @@
+// create_small_posit_lookup_table.cpp
+
+#include "stdafx.h"
+#include <sstream>
+#include "../../posit/posit.hpp"
+#include "../../posit/posit_operators.hpp"
+
+using namespace std;
+
+/*
+Standard posits with nbits = 16 have 1 exponent bit.
+*/
+
+int main(int argc, char** argv)
+{
+	cout << "Standard posit<16,1> configuration tests" << endl;
+	try {
+        const size_t nbits = 16;
+        const size_t es = 1;
+        posit<nbits,es> p;
+
+		cout << spec_to_string(p) << endl;
+	}
+	catch (char* e) {
+		cerr << e << endl;
+	}
+
+	return 0;
+}
