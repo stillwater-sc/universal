bef8529671b8bdf6ba1889789ca7efd331e4c456
Peter Gottschling <peter.gottschling@simunova.com>
2017-09-18 16:46:31 +0200

    Boiler plate for utilities test.

---
+++ tests/utilities/selector_test.cpp
@@ -0,0 +1,21 @@
+
+// selector_test.cpp: Test the run-time selection of posit formats
+//
+// Copyright (C) 2017 Stillwater Supercomputing, Inc.
+//
+// This file is part of the universal numbers project, which is released under an MIT Open Source license.
+
+#include <iostream>
+
+// #include "../../posit/posit.hpp"
+// #include "../../posit/posit_operators.hpp"
+
+using namespace std;
+
+
+int main(int argc, char** argv)
+{
+    cout << "This is the posit selector test.\n";
+
+    return 0;
+}
