415f3adaa71f11cb2af70661072f428605840266
Ravenwater <theo@stillwater-sc.com>
2017-10-26 14:48:55 -0400

    simple dot product

--- examples/blas/fused_dot_product.cpp
+++ examples/blas/fused_dot_product.cpp
@@ -4,21 +4,61 @@
 //
 // This file is part of the universal numbers project, which is released under an MIT Open Source license.
 
+// enable the mathematical constants in cmath
+#define _USE_MATH_DEFINES
 #include "stdafx.h"
 
+#include <vector>
 #include <posit>
 
 using namespace std;
 
+/*
+
+Mathematical 	C++ Symbol	Decimal Representation
+Expression
+pi				M_PI		3.14159265358979323846
+pi/2			M_PI_2		1.57079632679489661923
+pi/4			M_PI_4		0.785398163397448309616
+1/pi			M_1_PI		0.318309886183790671538
+2/pi			M_2_PI		0.636619772367581343076
+2/sqrt(pi)		M_2_SQRTPI	1.12837916709551257390
+sqrt(2)			M_SQRT2		1.41421356237309504880
+1/sqrt(2)		M_SQRT1_2	0.707106781186547524401
+e				M_E			2.71828182845904523536
+log_2(e)		M_LOG2E		1.44269504088896340736
+log_10(e)		M_LOG10E	0.434294481903251827651
+log_e(2)		M_LN2		0.693147180559945309417
+log_e(10)		M_LN10		2.30258509299404568402
+
+*/
 
 int main(int argc, char** argv)
 try 
 {
-	posit<5,1> pa, pb, pdiv;
-	pa = 1.0f;
-	pb = 1.0f;
-	pdiv = pa / pb;
-	cout << pa << " " << pb << " " << pdiv << endl;
+	const size_t nbits = 16;
+	const size_t es = 1;
+	const size_t vecSize = 32;
+
+	posit<nbits, es> p;
+	vector< posit<nbits,es> > sinusoid(vecSize), cosinusoid(vecSize);
+
+	for (int i = 0; i < vecSize; i++) {
+		p = sin( (float(i) / float(vecSize)) *2.0 * M_PI);
+		sinusoid[i] = p;
+		p = cos((float(i) / float(vecSize)) *2.0 * M_PI);
+		cosinusoid[i] = p;
+	}
+
+	// dot product
+	posit<nbits, es> dot_product;
+	dot_product = 0.0f;
+	for (int i = 0; i < vecSize; i++) {
+		p += sinusoid[i] * cosinusoid[i];
+	}
+
+	cout << "Dot product is " << dot_product << endl;
+
 	return 0;
 
 }
