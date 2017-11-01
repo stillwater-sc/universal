02d903b2e0681373aaffe26d837e88d412d602e5
Peter Gottschling <peter.gottschling@simunova.com>
2017-11-01 09:15:53 +0100

    Turned them into compile-time constants (minor detail).

--- posit/trace_constants.hpp
+++ posit/trace_constants.hpp
@@ -6,12 +6,13 @@
 // This file is part of the universal numbers project, which is released under an MIT Open Source license.
 
 // posit decode and conversion
-const bool _trace_decode     = false;
-const bool _trace_conversion = false;
-const bool _trace_rounding   = false;
+constexpr bool _trace_decode     = false;
+constexpr bool _trace_conversion = false;
+constexpr bool _trace_rounding   = false;
 
 // arithmetic operator tracing
-const bool _trace_add        = false;
-const bool _trace_sub        = false;
-const bool _trace_mul        = false;
-const bool _trace_div        = false;
+constexpr bool _trace_add        = false;
+constexpr bool _trace_sub        = false;
+constexpr bool _trace_mul        = false;
+constexpr bool _trace_div        = false;
+
