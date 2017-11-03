8e9da87bfb9f22d40063c99040106e03703884a9
Peter Gottschling <peter.gottschling@simunova.com>
2017-11-03 16:28:43 +0100

    Cleanup.

--- tests/posit/arithmetic_add.cpp
+++ tests/posit/arithmetic_add.cpp
@@ -8,11 +8,6 @@
 
 #include <vector>
 
-#define POSIT_THROW_FOR_INCORRECT_CALCULATION               // to stop at first addition bugs
-// #define POSIT_VERBOSE_OUTPUT                                // trace everything
-// #define POSIT_ROUND_TIES_TO_ZERO                            // In tie case round toward zero (for consistency with Theo's tests)
-#define POSIT_USE_LONG_VALUE_IN_CONVERSION                  // Use all mantissa bit in value and round later
-
 #include "../../bitset/bitset_helpers.hpp"
 #include "../../posit/posit_regime_lookup.hpp"
 #include "../../posit/posit.hpp"
@@ -51,29 +46,6 @@ try
 	int nrOfFailedTests = 0;
 	bool bReportIndividualTestCases = false;
 
-	// generate individual testcases to hand trace/debug
-// 	GenerateTestCase<5, 0>(-0.625f,  4.000f);
-// 	GenerateTestCase<5, 0>(-0.500f,  4.000f);
-
-// 	GenerateTestCase<3, 0>(1.0f,  1.0f);
-//      GenerateTestCase<4, 0>(0.25f,  0.75f);
-//         GenerateTestCase<4, 0>(0.25f,  1.5f);
-//         GenerateTestCase<4, 0>(0.75f,  0.75f);
-//         posit<4, 0> p = 2.75;
-//         GenerateTestCase<4, 0>(0.75f,  2.0f);
-        
-        posit<5, 1> p = -1.625;
-        GenerateTestCase<5, 1>(0.375f, -2.0f);              // need one more bit at the end
-
-	/*
-	// previous bugs that where hand traced
-	GenerateTestCase<5, 0>( 0.125f,  0.250f);
-	GenerateTestCase<5, 0>( 0.125f,  1.000f);
-	GenerateTestCase<5, 0>( 0.250f, -1.000f);
-	GenerateTestCase<5, 0>( 0.250f,  0.375f);
-	*/
-
-#if 1
 	nrOfFailedTests += ReportTestResult(ValidateAddition<3, 0>("Posit<3,0> addition failed: ", bReportIndividualTestCases), "posit<3,0>", "addition");
 
 	nrOfFailedTests += ReportTestResult(ValidateAddition<4, 0>("Posit<4,0> addition failed: ", bReportIndividualTestCases), "posit<4,0>", "addition");
@@ -97,10 +69,9 @@ try
 	nrOfFailedTests += ReportTestResult(ValidateAddition<8, 1>("Posit<8,1> addition failed: ", bReportIndividualTestCases), "posit<8,1>", "addition");
 	nrOfFailedTests += ReportTestResult(ValidateAddition<8, 2>("Posit<8,2> addition failed: ", bReportIndividualTestCases), "posit<8,2>", "addition");
 	nrOfFailedTests += ReportTestResult(ValidateAddition<8, 3>("Posit<8,3> addition failed: ", bReportIndividualTestCases), "posit<8,3>", "addition");
-#endif
 	// very long running, so disabled
 	//ReportTestResult(ValidateAddition<16, 1>("Posit<16,1> addition failed: ", bReportIndividualTestCases), "posit<16,1>", "addition");
-	return nrOfFailedTests;
+	return nrOfFailedTests > 0  ? 1 : 0;
 }
 catch (char* e) {
 	cerr << e << endl;
