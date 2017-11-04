764c6de1e53eccd06a0ff3b8227670d9c8d8d210
Ravenwater <theo@stillwater-sc.com>
2017-11-04 09:15:59 -0400

    Cleanup of postfix/prefix tests

--- tests/posit/postfix.cpp
+++ tests/posit/postfix.cpp
@@ -23,22 +23,38 @@ try {
 	bool bReportIndividualTestCases = false;
 	int nrOfFailedTestCases = 0;
 
+	nrOfFailedTestCases += ReportTestResult(ValidatePostfix<3, 0>("Postfix failed", bReportIndividualTestCases), "posit<3,0>", "posit++");
 
+	nrOfFailedTestCases += ReportTestResult(ValidatePostfix<4, 0>("Postfix failed", bReportIndividualTestCases), "posit<4,0>", "posit++");
+	nrOfFailedTestCases += ReportTestResult(ValidatePostfix<4, 1>("Postfix failed", bReportIndividualTestCases), "posit<4,1>", "posit++");
+
+	return (nrOfFailedTestCases > 0 ? EXIT_FAILURE : EXIT_SUCCESS);
+}
+catch (char* msg) {
+	cerr << msg << endl;
+	return EXIT_FAILURE;
+}
+
+
+// just because you can, doesn't mean you should
+void DoNotDoStuffLikeThis() {
+	// order of function evaluation is not defined, so there is no
+	// mechanism for these methods to evaluate left to right
 	// integer example
 	// DON'T
-// 	int i = 0;
-// 	cout << i << " " << --i << " " << i << " " << i++ << " " << i << endl;
-// 	cout << i << " " << i++ << " " << i << endl;
-// 	i = 0;
-// 	cout << --i << " " << --i << " " << --i << endl;
-// 	i = 0;
-// 	cout << --(--(--i)) << endl;
-// 	i = 0;
-// 	cout << ------i << " " << endl;
+	int i = 0;
+	cout << i << " " << --i << " " << i << " " << i++ << " " << i << endl;
+	cout << i << " " << i++ << " " << i << endl;
+	i = 0;
+	cout << --i << " " << --i << " " << --i << endl;
+	i = 0;
+	cout << --(--(--i)) << endl;
+	i = 0;
+	cout << ------i << " " << endl;
 
 	// equivalent posit example
 	const size_t nbits = 4;
-	const size_t es = 0;	
+	const size_t es = 0;
 	posit<nbits, es> result, p = 0.0f;
 	cout << p << " " << --p << " " << p << " " << p++ << " " << p << endl;
 	cout << p << " " << p++ << " " << p << endl;
@@ -47,12 +63,16 @@ try {
 	p = 0.0f;
 	cout << --(--(--p)) << endl;
 
+
 	p = 0.0f;
 	result = --p++;
 	cout << "result " << result << endl;
+
+	int nrOfFailedTestCases = 0;
+	p = 0.0f;
 	if (!p.isZero()) {
 		cout << "FAIL 1 " << p << endl; nrOfFailedTestCases++;
-	}	
+	}
 	p = 0.0f; --(--(--(p++)++)++);
 	if (!p.isZero()) {
 		cout << "FAIL 2 " << p << endl; nrOfFailedTestCases++;
@@ -69,15 +89,4 @@ try {
 	if (p != posit<nbits, es>(1.0f)) {
 		cout << "FAIL 5 " << p << endl; nrOfFailedTestCases++;
 	}
-
-	nrOfFailedTestCases += ReportTestResult(ValidatePostfix<3, 0>("Increment failed", bReportIndividualTestCases), "posit<3,0>", "posit++");
-
-	nrOfFailedTestCases += ReportTestResult(ValidatePostfix<4, 0>("Increment failed", bReportIndividualTestCases), "posit<4,0>", "posit++");
-	nrOfFailedTestCases += ReportTestResult(ValidatePostfix<4, 1>("Increment failed", bReportIndividualTestCases), "posit<4,1>", "posit++");
-
-	return (nrOfFailedTestCases > 0 ? EXIT_FAILURE : EXIT_SUCCESS);
-}
-catch (char* msg) {
-	cerr << msg << endl;
-	return EXIT_FAILURE;
 }
--- tests/posit/prefix.cpp
+++ tests/posit/prefix.cpp
@@ -23,52 +23,6 @@ try {
 	bool bReportIndividualTestCases = false;
 	int nrOfFailedTestCases = 0;
 
-	// integer example
-	// Oh no!
-// 	int i = 0;
-// 	cout << i << " " << --i << " " << i << " " << i++ << " " << i << endl;
-// 	cout << i << " " << i++ << " " << i << endl;
-// 	i = 0;
-// 	cout << --i << " " << --i << " " << --i << endl;
-// 	i = 0;
-// 	cout << --(--(--i)) << endl;
-// 	i = 0;
-// 	cout << ------i << " " << endl;
-
-	// equivalent posit example
-	const size_t nbits = 4;
-	const size_t es = 0;
-	posit<nbits, es> result, p = 0.0f;
-	cout << p << " " << --p << " " << p << " " << p++ << " " << p << endl;
-	cout << p << " " << p++ << " " << p << endl;
-	p = 0.0f;
-	cout << --p << " " << --p << " " << --p << endl;
-	p = 0.0f;
-	cout << --(--(--p)) << endl;
-
-	p = 0.0f;
-	result = --p++;
-	cout << "result " << result << endl;
-	if (!p.isZero()) {
-		cout << "FAIL 1 " << p << endl; nrOfFailedTestCases++;
-	}
-	p = 0.0f; --(--(--(p++)++)++);
-	if (!p.isZero()) {
-		cout << "FAIL 2 " << p << endl; nrOfFailedTestCases++;
-	}
-	p = 0.0f; ++(++(++(p--)--)--);
-	if (!p.isZero()) {
-		cout << "FAIL 3 " << p << endl; nrOfFailedTestCases++;
-	}
-	p = 0.0f; ----------p++++++++++;
-	if (!p.isZero()) {
-		cout << "FAIL 4 " << p << endl; nrOfFailedTestCases++;
-	}
-	p = 0.0f; p++++++++++;
-	if (p != posit<nbits, es>(1.0f)) {
-		cout << "FAIL 5 " << p << endl; nrOfFailedTestCases++;
-	}
-
 	nrOfFailedTestCases += ReportTestResult(ValidatePrefix<3, 0>("Increment failed", bReportIndividualTestCases), "posit<3,0>", "prefix++(posit)");
 
 	nrOfFailedTestCases += ReportTestResult(ValidatePrefix<4, 0>("Increment failed", bReportIndividualTestCases), "posit<4,0>", "prefix++(posit)");
