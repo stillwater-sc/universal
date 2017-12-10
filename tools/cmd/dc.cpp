f8ed17585d660445a7e0c2962b4e6e092bdd6c2a
Ravenwater <theo@stillwater-sc.com>
2017-12-10 11:01:28 -0500

    Returning EXIT_SUCCESS when usage condition is hit to placate ctest

--- tools/cmd/dc.cpp
+++ tools/cmd/dc.cpp
@@ -19,12 +19,12 @@ try {
 		cerr << "Usage: dc double_value" << endl;
 		cerr << "Example: dc 0.03124999" << endl;
 		cerr << "double: 0.03124998999999999832466457405644177924842 Sign: 0 Scale: -6 Fraction: b1111111111111111111101010100001100111000100011101110" << endl;
-		return 1;
+		return EXIT_SUCCESS;   // signal successful completion for ctest
 	}
 	double d = atof(argv[1]);
 	value<52> v(d);
 	cout << "double: " << setprecision(40) << d << components(v) << endl;
-	return 0;
+	return EXIT_SUCCESS;
 }
 catch (char* msg) {
 	cerr << msg << endl;
