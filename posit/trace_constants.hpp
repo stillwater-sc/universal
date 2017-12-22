523a69412d27de4ac5061aaa1bd29562a82c2473
Ravenwater <theo@stillwater-sc.com>
2017-12-22 13:02:42 -0500

    Improving granularity of tracing arithmetic intermediate results

--- posit/trace_constants.hpp
+++ posit/trace_constants.hpp
@@ -25,17 +25,49 @@ constexpr bool _trace_reciprocate = false;
 
 # else // !POSIT_VERBOSE_OUTPUT
 
+#ifndef POSIT_TRACE_CONVERSION
+// posit decode and conversion
+constexpr bool _trace_decode      = false;
+constexpr bool _trace_conversion  = false;
+constexpr bool _trace_rounding    = false;
+#else
 // posit decode and conversion
 constexpr bool _trace_decode      = true;
 constexpr bool _trace_conversion  = true;
 constexpr bool _trace_rounding    = true;
+#endif // !POSIT_VERBOSE_CONVERSION
 
 // arithmetic operator tracing
-constexpr bool _trace_add         = true;
-constexpr bool _trace_sub         = true;
-constexpr bool _trace_mul         = true;
-constexpr bool _trace_div         = true;
+#ifndef POSIT_TRACE_ADD
+constexpr bool _trace_add = false;
+#else
+constexpr bool _trace_add = true;
+#endif
+
+#ifndef POSIT_TRACE_SUB
+constexpr bool _trace_sub = false;
+#else
+constexpr bool _trace_sub = true;
+#endif
+
+#ifndef POSIT_TRACE_MUL
+constexpr bool _trace_mul = false;
+#else
+constexpr bool _trace_mul = true;
+#endif
+
+#ifndef POSIT_TRACE_DIV
+constexpr bool _trace_div = false;
+#else
+constexpr bool _trace_div = true;
+#endif
+
+#ifndef POSIT_TRACE_RECIPROCATE
+constexpr bool _trace_reciprocate = false;
+#else
 constexpr bool _trace_reciprocate = true;
+#endif
+
 
 # endif
 
