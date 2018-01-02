9675ab71314f4e9194d9a80c1a2694914f8799e4
Ravenwater <theo@stillwater-sc.com>
2018-01-02 18:55:04 -0500

    Cleaning up fraction assignment code

--- tests/posit/conversion_functions.cpp
+++ tests/posit/conversion_functions.cpp
@@ -411,8 +411,9 @@ void posit_component_conversion(float x, bool bPrintIntermediateSteps = false) {
 	if (bPrintIntermediateSteps) cout << "exponent = " << _exponent << endl;
 	unsigned nf = (unsigned)std::max<int>(0, (nbits + 1) - (2 + run + es));
 	if (bPrintIntermediateSteps) cout << "nf       = " << nf << endl;
+	std::bitset<23> fraction_bitset = v.fraction();
 	fraction<23> _fraction;
-	bool sb = _fraction.template assign<23>(nf, v.fraction(), nf+1);  // assign and create sticky bit
+	bool sb = _fraction.assign<23>(nf, fraction_bitset, nf+1);  // assign and create sticky bit
 	if (bPrintIntermediateSteps) cout << "sb       = " << sb << endl;
 	// 	assess if we need to round up the truncated posit
 /*
