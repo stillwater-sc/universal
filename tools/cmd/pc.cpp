4ce3f2a034389a3ecd9592ed5045d26e4c3e0004
Ravenwater <theo@stillwater-sc.com>
2018-01-08 09:16:34 -0500

    Bug fix: fraction assignment when the nr of fraction bits in the input is less than the nr of fraction bits in the posit

--- tools/cmd/pc.cpp
+++ tools/cmd/pc.cpp
@@ -12,6 +12,22 @@ using namespace std;
 using namespace sw::unum;
 
 typedef std::numeric_limits< double > dbl;
+const char* msg = "posit< 8, 0> = s1 r1111111 e f qNW v - 64\n\
+posit< 8, 1> = s1 r1111111 e f qNW v - 4096\n\
+posit< 8, 2> = s1 r1111111 e f qNW v - 16777216\n\
+posit< 8, 3> = s1 r1111111 e f qNW v - 281474976710656\n\
+posit<16, 1> = s1 r111111111111111 e f qNW v - 268435456\n\
+posit<16, 2> = s1 r111111111111111 e f qNW v - 72057594037927936\n\
+posit<16, 3> = s1 r111111110 e000 f100 qNW v - 1.080863910568919e+17\n\
+posit<32, 1> = s1 r111111111111111111111111111110 e1 f qNW v - 1.4411518807585587e+17\n\
+posit<32, 2> = s1 r1111111111111110 e00 f1000111100100 qNW v - 1.1234370007964058e+17\n\
+posit<32, 3> = s1 r111111110 e000 f1000111100100001110 qNW v - 1.1234562422498918e+17\n\
+posit<48, 1> = s1 r111111111111111111111111111110 e0 f1000111100100010 qNW v - 1.1234589910289613e+17\n\
+posit<48, 2> = s1 r1111111111111110 e00 f10001111001000011100110010111 qNW v - 1.1234567885160448e+17\n\
+posit<48, 3> = s1 r111111110 e000 f10001111001000011100110010111010111 qNW v - 1.1234567889983898e+17\n\
+posit<64, 1> = s1 r111111111111111111111111111110 e0 f10001111001000011100110010111011 qNW v - 1.1234567890193613e+17\n\
+posit<64, 2> = s1 r1111111111111110 e00 f100011110010000111001100101110101110001001111 qNW v - 1.1234567890000077e+17\n\
+posit<64, 3> = s1 r111111110 e000 f100011110010000111001100101110101110001001110101000 qNW v - 1.123456789e+17";
 
 // receive a float and print its components
 int main(int argc, char** argv)
@@ -20,10 +36,7 @@ try {
 		cerr << "Show the sign/scale/regime/exponent/fraction components of a posit." << endl;
 	    cerr << "Usage: pc float_value" << endl;
 		cerr << "Example: pc -1.123456789e17" << endl;
-		cerr << "posit< 8,0>: " << endl;
-		cerr << "posit<16,1>: " << endl;
-		cerr << "posit<32,2>: " << endl;
-		cerr << "posit<64,3>: " << endl;
+		cerr <<  msg << endl;
 		return EXIT_SUCCESS;  // signal successful completion for ctest
 	}
 	double d = atof(argv[1]);
