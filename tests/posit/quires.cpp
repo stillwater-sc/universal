740c8cc5562ee0af7b519feeae77b774b938a8f0
Ravenwater <theo@stillwater-sc.com>
2018-01-28 15:00:11 -0500

    Removing std:: ambiguity warnings

--- tests/posit/quires.cpp
+++ tests/posit/quires.cpp
@@ -19,10 +19,10 @@ using namespace sw::unum;
 int TestQuireAccumulationResult(int nrOfFailedTests, string descriptor)
 {
 	if (nrOfFailedTests > 0) {
-		cout << descriptor << " quire accumulation FAIL" << endl;
+		std::cout << descriptor << " quire accumulation FAIL" << std::endl;
 	}
 	else {
-		cout << descriptor << " quire accumulation PASS" << endl;
+		std::cout << descriptor << " quire accumulation PASS" << std::endl;
 	}
 	return nrOfFailedTests;
 }
@@ -39,27 +39,27 @@ int ValidateQuireAccumulation() {
 template<size_t nbits, size_t es, size_t capacity>
 void GenerateTestCase(int input, const quire<nbits, es, capacity>& reference, const quire<nbits, es, capacity>& qresult) {
 
-	cout << endl;
+	std::cout << std::endl;
 }
 
 template<size_t nbits, size_t es, size_t capacity>
 void GenerateUnsignedIntAssignments() {
 	quire<nbits, es, capacity> q;
 	unsigned upper_range = q.upper_range();
-	cout << "Upper range = " << upper_range << endl;
+	std::cout << "Upper range = " << upper_range << std::endl;
 	uint64_t i;
-	q = 0; cout << q << endl;
+	q = 0; std::cout << q << std::endl;
 	unsigned v = 1;
 	for (i = 1; i < uint64_t(1) << (upper_range + capacity); i <<= 1) {
 		q = i;
-		cout << q << endl;
+		std::cout << q << std::endl;
 	}
 	i <<= 1;
 	try {
 		q = i;
 	}
-	catch (char* msg) {
-		cerr << "Caught the exception: " << msg << ". Value was " << i << endl;
+	catch (char const* msg) {
+		std::cerr << "Caught the exception: " << msg << ". Value was " << i << std::endl;
 	}
 }
 
@@ -67,20 +67,20 @@ template<size_t nbits, size_t es, size_t capacity>
 void GenerateSignedIntAssignments() {
 	quire<nbits, es, capacity> q;
 	unsigned upper_range = q.upper_range();
-	cout << "Upper range = " << upper_range << endl;
+	std::cout << "Upper range = " << upper_range << std::endl;
 	int64_t i, upper_limit = -(int64_t(1) << (upper_range + capacity));
-	q = 0; cout << q << endl;
+	q = 0; std::cout << q << std::endl;
 	unsigned v = 1;
 	for (i = -1; i > upper_limit; i *= 2) {
 		q = i;
-		cout << q << endl;
+		std::cout << q << std::endl;
 	}
 	i <<= 1;
 	try {
 		q = i;
 	}
-	catch (char* msg) {
-		cerr << "Caught the exception: " << msg << ". RHS was " << i << endl;
+	catch (char const* msg) {
+		std::cerr << "Caught the exception: " << msg << ". RHS was " << i << std::endl;
 	}
 }
 
@@ -89,7 +89,7 @@ void GenerateValueAssignments() {
 	quire<nbits, es, capacity> q;
 	int upper_range = q.upper_range();
 	int lower_range = q.lower_range();
-	cout << "Max scale = " << upper_range-1 << " Minimum scale = " << -lower_range << endl;
+	std::cout << "Max scale = " << upper_range-1 << " Minimum scale = " << -lower_range << std::endl;
 	int scale;
 	value<fbits> v;
 	double d = pow(2.0, upper_range);
@@ -97,10 +97,10 @@ void GenerateValueAssignments() {
 		v = d;
 		try {
 			q = v;
-			cout << setw(10) << v << q << endl;
+			std::cout << setw(10) << v << q << std::endl;
 		}
-		catch (char* msg) {
-			cerr << "Caught the exception: " << msg << ". RHS was " << v << " " << components(v) << endl;
+		catch (char const* msg) {
+			std::cerr << "Caught the exception: " << msg << ". RHS was " << v << " " << components(v) << std::endl;
 		}
 		d /= 2.0;
 	}
@@ -128,45 +128,45 @@ try {
 
 	GenerateValueAssignments<nbits, es, capacity, fbits>();
 
-	cout << endl;
-	cout << "Nothing prohibiting us from creating quires for float and double arithmetic" << endl;
+	std::cout << endl;
+	std::cout << "Nothing prohibiting us from creating quires for float and double arithmetic" << std::endl;
 	float f = 1.555555555555e-10f;
 	quire<10, 2, 2> fquire(f);
-	cout << "float:  " << setw(15) << f << " " << fquire << endl;
+	std::cout << "float:  " << setw(15) << f << " " << fquire << std::endl;
 
 	double d = 1.555555555555e16;
 	quire<10, 2, 2> dquire(d);
-	cout << "double: " << setw(15) << d << " " << dquire << endl;
+	std::cout << "double: " << setw(15) << d << " " << dquire << std::endl;
 
-	cout << endl;
-	cout << "Addition" << endl;
+	std::cout << std::endl;
+	std::cout << "Addition" << std::endl;
 	quire<nbits, es, capacity> q;
 	value<5> v(32.0);
 	q += v;
-	cout << q << endl;
+	std::cout << q << std::endl;
 	q += v;
-	cout << q << endl;
+	std::cout << q << std::endl;
 
 	q = 0;
 	v = 0.5;
 	q += v;
-	cout << q << endl;
+	std::cout << q << std::endl;
 	q += v;
-	cout << q << endl;
+	std::cout << q << std::endl;
 
 	q = 0;
-	v = 3.875 + 0.0625; cout << "v " << components(v) << endl;
+	v = 3.875 + 0.0625; std::cout << "v " << components(v) << std::endl;
 	q += v;
-	cout << q << endl;
+	std::cout << q << std::endl;
 	q += v;
-	cout << q << endl;
+	std::cout << q << std::endl;
 	q += v;
-	cout << q << endl;
+	std::cout << q << std::endl;
 
 
 #else
 
-	cout << "Quire validation" << endl;
+	std::cout << "Quire validation" << std::endl;
 	TestQuireAccumulationResult(ValidateQuireAccumulation<8,0,5>(), "quire<8,0,5>");
 
 #ifdef STRESS_TESTING
@@ -179,6 +179,6 @@ try {
 	return (nrOfFailedTestCases > 0 ? EXIT_FAILURE : EXIT_SUCCESS);
 }
 catch (char const* msg) {
-	cerr << msg << endl;
+	std::cerr << msg << std::endl;
 	return EXIT_FAILURE;
 }
