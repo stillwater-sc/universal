afce9a9de56d3e2f0b9d979543c18b5221f0ff3f
Ravenwater <theo@stillwater-sc.com>
2018-02-06 15:05:26 -0500

    Adding a playground skeleton program for easy experimentation

---
+++ examples/playground/stdafx.h
@@ -0,0 +1,18 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+//#include <tchar.h>
+#include <cstdint>	// uint8_t, etc.
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+#include <cmath>        // for frexp/frexpf
+#include <bitset>
