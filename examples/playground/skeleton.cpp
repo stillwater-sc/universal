42fd8bb9931a4807492c22bdc162664db9ea2ba0
Ravenwater <theo@stillwater-sc.com>
2018-02-06 17:16:41 -0500

    Adding color printing experiment for posits

--- examples/playground/skeleton.cpp
+++ examples/playground/skeleton.cpp
@@ -1,6 +1,6 @@
 // skeleton.cpp example showing the basic program structure to use custom posit configurations
 //
-// Copyright (C) 2017 Stillwater Supercomputing, Inc.
+// Copyright (C) 2017-2018 Stillwater Supercomputing, Inc.
 //
 // This file is part of the universal numbers project, which is released under an MIT Open Source license.
 
@@ -33,7 +33,119 @@ log_e(10)		M_LN10		2.30258509299404568402
 
 */
 
-constexpr double pi = 3.14159265358979323846;  // best practice for C++11
+// best practice for C++11
+constexpr double m_pi       = 3.14159265358979323846;  
+constexpr double m_pi_2     = 1.57079632679489661923;
+constexpr double m_pi_4     = 0.785398163397448309616;
+constexpr double m_1_pi     = 0.318309886183790671538;
+constexpr double m_2_pi     = 0.636619772367581343076;
+constexpr double m_2_sqrtpi = 1.12837916709551257390;
+constexpr double m_sqrt2    = 1.41421356237309504880;
+constexpr double m_sqrt1_2  = 0.707106781186547524401;
+constexpr double m_e        = 2.71828182845904523536;
+constexpr double m_log2e    = 1.44269504088896340736;
+constexpr double m_log10e   = 0.434294481903251827651;
+constexpr double m_ln2      = 0.693147180559945309417;
+constexpr double m_ln10     = 2.30258509299404568402;
+
+
+
+namespace sw {
+	namespace unum {
+		enum ColorCode {
+			FG_DEFAULT = 39,
+			FG_BLACK = 30,
+			FG_RED = 31,
+			FG_GREEN = 32,
+			FG_YELLOW = 33,
+			FG_BLUE = 34,
+			FG_MAGENTA = 35,
+			FG_CYAN = 36,
+			FG_LIGHT_GRAY = 37,
+			FG_DARK_GRAY = 90,
+			FG_LIGHT_RED = 91,
+			FG_LIGHT_GREEN = 92,
+			FG_LIGHT_YELLOW = 93,
+			FG_LIGHT_BLUE = 94,
+			FG_LIGHT_MAGENTA = 95,
+			FG_LIGHT_CYAN = 96,
+			FG_WHITE = 97,
+
+			BG_DEFAULT = 49,
+			BG_BLACK = 40,
+			BG_RED = 41,
+			BG_GREEN = 42,
+			BG_YELLOW = 43,
+			BG_BLUE = 44,
+			BG_MAGENTA = 45,
+			BG_CYAN = 46,
+			BG_LIGHT_GRAY = 47,
+			BG_DARK_GRAY = 100,
+			BG_LIGHT_RED = 101,
+			BG_LIGHT_GREEN = 102,
+			BG_LIGHT_YELLOW = 103,
+			BG_LIGHT_BLUE = 104,
+			BG_LIGHT_MAGENTA = 105,
+			BG_LIGHT_CYAN = 106,
+			BG_WHITE = 107
+
+		};
+		class Color {
+			ColorCode code;
+		public:
+			Color(ColorCode pCode) : code(pCode) {}
+			friend std::ostream&
+				operator<<(std::ostream& os, const Color& mod) {
+				return os << "\033[" << mod.code << "m";
+			}
+		};
+
+		template<size_t nbits, size_t es>
+		std::string color_print(const posit<nbits, es>& p) {
+			std::stringstream ss;
+			Color red(ColorCode::FG_RED);
+			Color yellow(ColorCode::FG_YELLOW);
+			Color blue(ColorCode::FG_BLUE);
+			Color magenta(ColorCode::FG_MAGENTA);
+			Color cyan(ColorCode::FG_CYAN);
+			Color white(ColorCode::FG_WHITE);
+			Color def(ColorCode::FG_DEFAULT);
+			ss << red << (p.isNegative() ? "1" : "0");
+
+			std::bitset<nbits - 1> r = p.get_regime().get();
+			int regimeBits = (int)p.get_regime().nrBits();
+			int nrOfRegimeBitsProcessed = 0;
+			for (int i = nbits - 2; i >= 0; --i) {
+				if (regimeBits > nrOfRegimeBitsProcessed++) {
+					ss << yellow << (r[i] ? "1" : "0");
+				}
+			}
+
+			std::bitset<es> e = p.get_exponent().get();
+			int exponentBits = (int)p.get_exponent().nrBits();
+			int nrOfExponentBitsProcessed = 0;
+			for (int i = int(es) - 1; i >= 0; --i) {
+				if (exponentBits > nrOfExponentBitsProcessed++) {
+					ss << cyan << (e[i] ? "1" : "0");
+				}
+			}
+
+			std::bitset<posit<nbits,es>::fbits> f = p.get_fraction().get();
+			int fractionBits = (int)p.get_fraction().nrBits();
+			int nrOfFractionBitsProcessed = 0;
+			for (int i = int(p.fbits) - 1; i >= 0; --i) {
+				if (fractionBits > nrOfFractionBitsProcessed++) {
+					ss << magenta << (f[i] ? "1" : "0");
+				}
+			}
+
+			ss << def;
+			return ss.str();
+		}
+	};
+};
+
+
 
 int main(int argc, char** argv)
 try {
@@ -46,8 +158,8 @@ try {
 		posit<nbits, es> p;
 
 		// assign PI to posit<8,1>
-		p = M_PI;
-		cout << "posit<8,1> value of PI    = " << p << " " << pretty_print(p) << endl;
+		p = m_pi;
+		cout << "posit<8,1> value of PI    = " << p << " " << color_print(p) << " " << pretty_print(p) << endl;
 
 		// convert posit back to float
 		float f = float(p);
@@ -55,26 +167,7 @@ try {
 
 		// calculate PI/2
 		p = p / posit<nbits, es>(2.0);  // explicit conversions of literals
-		cout << "posit<8,1> value of PI/2  = " << p << " " << pretty_print(p) << endl;
-	}
-	cout << endl;
-	{
-		constexpr size_t nbits = 16;
-		constexpr size_t es = 1;
-
-		posit<nbits, es> p;
-
-		// assign PI to posit<16,1>
-		p = M_PI;
-		cout << "posit<16,1> value of PI   = " << p << " " << pretty_print(p) << endl;
-
-		// convert posit back to float
-		float f = float(p);
-		cout << "float value               = " << f << endl;
-
-		// calculate PI/2
-		p = p / posit<nbits, es>(2.0);  // explicit conversions of literals
-		cout << "posit<16,1> value of PI/2 = " << p << " " << pretty_print(p) << endl;
+		cout << "posit<8,1> value of PI/2  = " << p << " " << color_print(p) << " " << pretty_print(p) << endl;
 	}
 
 	return (bSuccess ? EXIT_SUCCESS : EXIT_FAILURE);
