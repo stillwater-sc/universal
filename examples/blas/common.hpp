eee77c616b0b87099172fa6c825c9d8e66ffcfa7
Ravenwater <theo@stillwater-sc.com>
2018-04-15 15:43:11 -0400

    Comment fix

--- examples/blas/common.hpp
+++ examples/blas/common.hpp
@@ -1,12 +1,8 @@
-// stdafx.h : include file for standard system include files,
-// or project specific include files that are used frequently, but
-// are changed infrequently
-//
-
+// common.hpp : include file for standard system include files,
 #pragma once
 
-#include <cstdint>	// uint8_t, etc.
-#include <cmath>        // for frexp/frexpf
+#include <cstdint>	 // uint8_t, etc.
+#include <cmath>     // for frexp/frexpf
 #include <string>
 #include <sstream>
 #include <iostream>
