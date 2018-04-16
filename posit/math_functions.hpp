ecab8d444afac4fd9abab9689f5fb1b24dbb5700
Ravenwater <theo@stillwater-sc.com>
2018-04-16 15:06:30 -0400

    Reworking the BLAS examples to highlight quire use for error free linear algebra

--- posit/math_functions.hpp
+++ posit/math_functions.hpp
@@ -1,10 +1,30 @@
 #pragma once
-
 // math_functions.hpp: simple math functions for posits
 //
 // Copyright (C) 2017-2018 Stillwater Supercomputing, Inc.
 //
 // This file is part of the universal numbers project, which is released under an MIT Open Source license.
+
+
+/*
+
+Mathematical 	C++ Symbol	Decimal Representation
+Expression
+pi				M_PI		3.14159265358979323846
+pi/2			M_PI_2		1.57079632679489661923
+pi/4			M_PI_4		0.785398163397448309616
+1/pi			M_1_PI		0.318309886183790671538
+2/pi			M_2_PI		0.636619772367581343076
+2/sqrt(pi)		M_2_SQRTPI	1.12837916709551257390
+sqrt(2)			M_SQRT2		1.41421356237309504880
+1/sqrt(2)		M_SQRT1_2	0.707106781186547524401
+e				M_E			2.71828182845904523536
+log_2(e)		M_LOG2E		1.44269504088896340736
+log_10(e)		M_LOG10E	0.434294481903251827651
+log_e(2)		M_LN2		0.693147180559945309417
+log_e(10)		M_LN10		2.30258509299404568402
+
+*/
 
 #include "sqrt_tables.hpp"
 
