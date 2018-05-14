77b2c591af0b3cde7f466ebe20d7f1be2ee59a3b
Ravenwater <theo@stillwater-sc.com>
2018-05-14 15:17:17 -0400

    WIP: additional mathematical function shims

--- math/complex.hpp
+++ math/complex.hpp
@@ -1,10 +1,10 @@
 #pragma once
-// exponent.hpp: exponent functions for posits
+// complex.hpp: functions for complex posits
 //
 // Copyright (C) 2017-2018 Stillwater Supercomputing, Inc.
 //
 // This file is part of the universal numbers project, which is released under an MIT Open Source license.
-
+#include <complex>
 
 namespace sw {
 	namespace unum {
@@ -12,22 +12,22 @@ namespace sw {
 		// the current shims are NON-COMPLIANT with the posit standard, which says that every function must be
 		// correctly rounded for every input value. Anything less sacrifices bitwise reproducibility of results.
 
-		// Base-e exponential function
+		// Real component of a complex posit
 		template<size_t nbits, size_t es>
-		posit<nbits,es> exp(posit<nbits,es> x) {
-			return posit<nbits,es>(std::exp(double(x)));
+		posit<nbits,es> real(std::complex< posit<nbits,es> > x) {
+			return posit<nbits,es>(std::real(x));
 		}
 
-		// Base-2 exponential function
+		// Imaginary component of a complex posit
 		template<size_t nbits, size_t es>
-		posit<nbits,es> exp2(posit<nbits,es> x) {
-			return posit<nbits,es>(std::exp2(double(x)));
+		posit<nbits,es> imag(std::complex< posit<nbits,es> > x) {
+			return posit<nbits,es>(std::imag(x));
 		}
 
-		// Base-10 exponential function
+		// Conjucate of a complex posit
 		template<size_t nbits, size_t es>
-		posit<nbits, es> exp10(posit<nbits, es> x) {
-			return posit<nbits, es>(std::pow(10.0, double(x)));
+		std::complex< posit<nbits,es> > conj(std::complex< posit<nbits,es> > x) {
+			return std::conj(x);
 		}
 
 	}  // namespace unum
