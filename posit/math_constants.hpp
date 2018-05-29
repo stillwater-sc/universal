5d27281c1abf25a321bbd0282d6113cb18557e0e
Ravenwater <theo@stillwater-sc.com>
2018-05-29 14:52:54 -0400

    Restructuring math constants to its own entity

---
+++ posit/math_constants.hpp
@@ -0,0 +1,32 @@
+#pragma once
+// math_constants.hpp: definition of math constants
+//
+// Copyright (C) 2017 Stillwater Supercomputing, Inc.
+//
+// This file is part of the universal numbers project, which is released under an MIT Open Source license.
+
+
+namespace sw {
+
+	namespace unum {
+
+		// best practice for C++11
+		// mathematical constants in our namespace
+
+		constexpr double m_pi = 3.14159265358979323846;
+		constexpr double m_pi_2 = 1.57079632679489661923;
+		constexpr double m_pi_4 = 0.785398163397448309616;
+		constexpr double m_1_pi = 0.318309886183790671538;
+		constexpr double m_2_pi = 0.636619772367581343076;
+		constexpr double m_2_sqrtpi = 1.12837916709551257390;
+		constexpr double m_sqrt2 = 1.41421356237309504880;
+		constexpr double m_sqrt1_2 = 0.707106781186547524401;
+		constexpr double m_e = 2.71828182845904523536;
+		constexpr double m_log2e = 1.44269504088896340736;
+		constexpr double m_log10e = 0.434294481903251827651;
+		constexpr double m_ln2 = 0.693147180559945309417;
+		constexpr double m_ln10 = 2.30258509299404568402;
+
+	}  // namespace unum
+
+}  // namespace sw
