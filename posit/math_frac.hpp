daa5d648567c72d32b2058ca7a0b036431432c14
Matthias Moeller <m.moller@tudelft.nl>
2018-05-30 16:40:31 +0200

    WIP: additional mathematical function shims

--- posit/math_frac.hpp
+++ posit/math_frac.hpp
@@ -13,9 +13,21 @@ namespace sw {
 		// correctly rounded for every input value. Anything less sacrifices bitwise reproducibility of results.
 
 		template<size_t nbits, size_t es>
+		posit<nbits,es> fmod(posit<nbits,es> x, posit<nbits,es> y) {
+			return posit<nbits,es>(std::fmod(double(x), double(y)));
+		}
+		
+		template<size_t nbits, size_t es>
+		posit<nbits,es> remainder(posit<nbits,es> x, posit<nbits,es> y) {
+			return posit<nbits,es>(std::remainder(double(x), double(y)));
+		}
+		
+		
+		template<size_t nbits, size_t es>
 		posit<nbits,es> frac(posit<nbits,es> x) {
 			return posit<nbits,es>(double(x)-long(x));
 		}
+		
 		
 	}  // namespace unum
 
