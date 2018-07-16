73290d935e64a96416919654e4db6f4df104c3f4
Ravenwater <theo@stillwater-sc.com>
2018-07-16 12:17:13 -0400

    WIP: adding fused matvec example

--- examples/blas/l2_fused_mv.cpp
+++ examples/blas/l2_fused_mv.cpp
@@ -9,11 +9,44 @@
 
 int main(int argc, char** argv)
 try {
+	using namespace std;
+
 	const size_t nbits = 8;
 	const size_t es = 0;
 	const size_t vecSize = 32;
 
 	int nrOfFailedTestCases = 0;
+
+	{
+		vector<double> A = {
+			1.0, 0.0, 0.0,
+			0.0, 1.0, 0.0,
+			0.0, 0.0, 1.0
+		};
+		vector<double> x = { DBL_EPSILON, DBL_EPSILON, DBL_EPSILON };
+		vector<double> b(3);
+		matvec(A, x, b);
+		cout << setprecision(21);
+		print(cout, 3, b, 1);
+		cout << endl;
+	}
+
+	{
+		// we can represent DBL_EPSILON with a 32-bit posit
+		using posit_32_2 = sw::unum::posit<32, 2>;
+
+		vector< posit_32_2 > A = {
+			1.0, 0.0, 0.0,
+			0.0, 1.0, 0.0,
+			0.0, 0.0, 1.0
+		};
+		vector<posit_32_2> x = { DBL_EPSILON, DBL_EPSILON, DBL_EPSILON };
+		vector<posit_32_2> b(3);
+		matvec(A, x, b);
+		print(cout, 3, b, 1);
+		cout << endl;
+
+	}
 
 	return (nrOfFailedTestCases > 0 ? EXIT_FAILURE : EXIT_SUCCESS);
 }
