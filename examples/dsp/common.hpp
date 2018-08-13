9e51a5dc31d56d7dda7e2b2c797bd602788ee0a5
Ravenwater <theo@stillwater-sc.com>
2018-08-13 08:55:31 -0400

    Unification of the common include file

--- examples/dsp/common.hpp
+++ examples/dsp/common.hpp
@@ -1,18 +1,6 @@
 // common.hpp : include file for standard system include files,
 #pragma once
 
-#include <cstdint>	 // uint8_t, etc.
-#include <cmath>     // for frexp/frexpf
-#include <string>
-#include <sstream>
-#include <iostream>
-#include <iomanip>
-
-#include <chrono>
-
-#include <vector>
-
-
 #ifdef WINDOWS
 // Including SDKDDKVer.h defines the highest available Windows platform.
 
@@ -21,3 +9,23 @@
 
 #include <SDKDDKVer.h>
 #endif // WINDOWS
+
+// enable the mathematical constants in cmath
+#define _USE_MATH_DEFINES
+
+#include <cstdint>	 // uint8_t, etc.
+#include <cmath>     // for frexp/frexpf
+#include <cfloat>	 // for DBL_EPSILON, etc.
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <chrono>
+#include <vector>
+
+#if defined(__GNUC__)
+#if __GNUC__ < 5
+#define hexfloat     scientific
+#define defaultfloat scientific
+#endif
+#endif
