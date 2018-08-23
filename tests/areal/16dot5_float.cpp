561e2f9b94f5d13569e2b26dbb00a6987ddb36a7
Ravenwater <theo@stillwater-sc.com>
2018-08-23 15:23:18 -0400

    WIP: adding arbitrary real functionality and tests that are follow the same configuration as posits

---
+++ tests/areal/16dot5_float.cpp
@@ -0,0 +1,70 @@
+// 16dot5_float.cpp: Functionality tests for half precision floats
+//
+// Copyright (C) 2017-2018 Stillwater Supercomputing, Inc.
+//
+// This file is part of the universal numbers project, which is released under an MIT Open Source license.
+
+#include "common.hpp"
+// minimum set of include files to reflect source code dependencies
+#include "../../bitblock/bitblock.hpp"
+#include "../../posit/bit_functions.hpp"
+#include "../../areal/areal.hpp"
+#include "../test_helpers.hpp"
+#include "areal_test_helpers.hpp"
+
+/*
+Standard posit with nbits = 16 have es = 1 exponent bit.
+*/
+
+int main(int argc, char** argv)
+try {
+	using namespace std;
+	using namespace sw::unum;
+
+	const size_t RND_TEST_CASES = 500000;
+
+	const size_t nbits = 16;
+	const size_t es = 5;
+
+	int nrOfFailedTestCases = 0;
+	bool bReportIndividualTestCases = false;
+	std::string tag = " areal<16,5>";
+
+	cout << "Standard areal<16,5> configuration tests" << endl;
+
+	areal<nbits, es> r;
+
+#if 0
+	cout << "Arithmetic tests " << RND_TEST_CASES << " randoms each" << endl;
+	nrOfFailedTestCases += ReportTestResult(ValidateThroughRandoms<nbits, es>(tag, bReportIndividualTestCases, OPCODE_ADD, RND_TEST_CASES), tag, "addition      ");
+	nrOfFailedTestCases += ReportTestResult(ValidateThroughRandoms<nbits, es>(tag, bReportIndividualTestCases, OPCODE_SUB, RND_TEST_CASES), tag, "subtraction   ");
+	nrOfFailedTestCases += ReportTestResult(ValidateThroughRandoms<nbits, es>(tag, bReportIndividualTestCases, OPCODE_MUL, RND_TEST_CASES), tag, "multiplication");
+	nrOfFailedTestCases += ReportTestResult(ValidateThroughRandoms<nbits, es>(tag, bReportIndividualTestCases, OPCODE_DIV, RND_TEST_CASES), tag, "division      ");
+#endif
+
+	return (nrOfFailedTestCases > 0 ? EXIT_FAILURE : EXIT_SUCCESS);
+}
+catch (char const* msg) {
+	std::cerr << msg << std::endl;
+	return EXIT_FAILURE;
+}
+catch (const posit_arithmetic_exception& err) {
+	std::cerr << "Uncaught posit arithmetic exception: " << err.what() << std::endl;
+	return EXIT_FAILURE;
+}
+catch (const quire_exception& err) {
+	std::cerr << "Uncaught quire exception: " << err.what() << std::endl;
+	return EXIT_FAILURE;
+}
+catch (const posit_internal_exception& err) {
+	std::cerr << "Uncaught posit internal exception: " << err.what() << std::endl;
+	return EXIT_FAILURE;
+}
+catch (const std::runtime_error& err) {
+	std::cerr << "Uncaught runtime exception: " << err.what() << std::endl;
+	return EXIT_FAILURE;
+}
+catch (...) {
+	std::cerr << "Caught unknown exception" << std::endl;
+	return EXIT_FAILURE;
+}
