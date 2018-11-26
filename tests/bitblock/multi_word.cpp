c59d97b825dadb32449f22cd0904ec6de02ae9c2
Ravenwater <theo@stillwater-sc.com>
2018-11-25 19:03:04 -0500

    compilation fixes for GCC

--- tests/bitblock/multi_word.cpp
+++ tests/bitblock/multi_word.cpp
@@ -9,6 +9,8 @@
 #include "../tests/test_helpers.hpp"
 #include "../bitblock_test_helpers.hpp"
 
+#if defined(_MSC_VER)
+// Disable warnings for Microsoft Visual Studio
 //  warning C4293: '<<': shift count negative or too big, undefined behavior
 // for 64 bit shifts on the mask
 #pragma warning (disable : 4293)
@@ -16,6 +18,73 @@
 // disable this warning: as the logic is correct
 // warning C4146 : unary minus operator applied to unsigned type, result still unsigned
 #pragma warning( disable : 4146)
+
+void CheckUnsignedNegationBehavior() {
+	unsigned long x = 0x7fffffff;
+	cout << hex;
+	cout << " +x = " << x << endl;
+	cout << " -x = " << -x << endl;
+	cout << dec;
+	cout << " #1 = " << universal_bitset::popcnt(x) << endl;
+}
+
+void CheckMultiWordBehavior() {
+
+	using WordT = unsigned char;
+
+	// checking UBB_WORDS
+	cout << "UBB_WORDS for key sizes\n";
+	cout << "UBB_WORDS(8)    : " << NrWords<8, sizeof(WordT)>() << endl;
+	cout << "UBB_WORDS(12)   : " << NrWords<12, sizeof(WordT)>() << endl;
+	cout << "UBB_WORDS(16)   : " << NrWords<16, sizeof(WordT)>() << endl;
+	cout << "UBB_WORDS(20)   : " << NrWords<20, sizeof(WordT)>() << endl;
+	cout << "UBB_WORDS(24)   : " << NrWords<24, sizeof(WordT)>() << endl;
+	cout << "UBB_WORDS(32)   : " << NrWords<32, sizeof(WordT)>() << endl;
+	cout << "UBB_WORDS(40)   : " << NrWords<40, sizeof(WordT)>() << endl;
+	cout << "UBB_WORDS(48)   : " << NrWords<48, sizeof(WordT)>() << endl;
+	cout << "UBB_WORDS(56)   : " << NrWords<56, sizeof(WordT)>() << endl;
+	cout << "UBB_WORDS(64)   : " << NrWords<64, sizeof(WordT)>() << endl;
+	cout << "UBB_WORDS(80)   : " << NrWords<80, sizeof(WordT)>() << endl;
+	cout << "UBB_WORDS(96)   : " << NrWords<96, sizeof(WordT)>() << endl;
+	cout << "UBB_WORDS(112)  : " << NrWords<112, sizeof(WordT)>() << endl;
+	cout << "UBB_WORDS(128)  : " << NrWords<128, sizeof(WordT)>() << endl;
+
+	// this creates and masks the size of the requested number of bits Nb
+	// val & ~((~static_cast<unsigned long long>(0)) << Nb)
+	cout << hex << ((~static_cast<unsigned long long>(0)) << 8) << endl;
+	cout << hex << ((~static_cast<unsigned long long>(0)) << 16) << endl;
+	cout << hex << ((~static_cast<unsigned long long>(0)) << 32) << endl;
+	cout << hex << ((~static_cast<unsigned long long>(0)) << 64) << endl;
+	cout << hex << "0x" << (0x5555555555555555 & ~((~static_cast<unsigned long long>(0)) << 8)) << endl;
+	cout << hex << "0x" << (0x5555555555555555 & ~((~static_cast<unsigned long long>(0)) << 16)) << endl;
+	cout << hex << "0x" << (0x5555555555555555 & ~((~static_cast<unsigned long long>(0)) << 32)) << endl;
+	cout << hex << "0x" << (0x5555555555555555 & ~((~static_cast<unsigned long long>(0)) << 64)) << endl;
+	cout << dec;
+
+	bitblock<8> bb008;
+	bitblock<16> bb016;
+	bitblock<32> bb032;
+	bitblock<64> bb064;
+	bitblock<128> bb128, bbu64, bbl64;
+
+	bb008 = 0x55u;
+	bb016 = 0x5555u;
+	bb032 = 0x55555555ul;
+	bb064 = 0x5555555555555555;
+	bbl64 = 0x5555555555555555;
+	bbu64 = 0x5555555555555555;
+	bbu64 <<= 64;
+	bb128 = bbu64;
+	bb128 |= bbl64;
+
+	cout << "bb008: " << bb008 << " size in bytes: " << sizeof(bb008) << endl;
+	cout << "bb016: " << bb016 << " size in bytes: " << sizeof(bb016) << endl;
+	cout << "bb032: " << bb032 << " size in bytes: " << sizeof(bb032) << endl;
+	cout << "bb064: " << bb064 << " size in bytes: " << sizeof(bb064) << endl;
+	cout << "bb128: " << bb128 << " size in bytes: " << sizeof(bb128) << endl;
+}
+
+#endif
 
 int Conversions() {
 	using namespace sw::unum;
@@ -112,75 +181,21 @@ try {
 
 	std::string tag = "Bitblock arithmetic operation failed";
 
-	using WordT = unsigned char;
-
 #if MANUAL_TESTING
 
-	unsigned long x = 0x7fffffff;
-	cout << hex;
-	cout << " +x = " << x << endl;
-	cout << " -x = " << -x << endl;
-	cout << dec;
-	cout << " #1 = " << universal_bitset::popcnt(x) << endl;
+#if defined(_MSC_VER)
+	CheckUnsignedNegationBehavior();
+	CheckMultiWordBehavior();
+#endif
 
-	// checking UBB_WORDS
-	cout << "UBB_WORDS for key sizes\n";
-	cout << "UBB_WORDS(8)    : " << NrWords<8, sizeof(WordT)>() << endl;
-	cout << "UBB_WORDS(12)   : " << NrWords<12, sizeof(WordT)>() << endl;
-	cout << "UBB_WORDS(16)   : " << NrWords<16, sizeof(WordT)>() << endl;
-	cout << "UBB_WORDS(20)   : " << NrWords<20, sizeof(WordT)>() << endl;
-	cout << "UBB_WORDS(24)   : " << NrWords<24, sizeof(WordT)>() << endl;
-	cout << "UBB_WORDS(32)   : " << NrWords<32, sizeof(WordT)>() << endl;
-	cout << "UBB_WORDS(40)   : " << NrWords<40, sizeof(WordT)>() << endl;
-	cout << "UBB_WORDS(48)   : " << NrWords<48, sizeof(WordT)>() << endl;
-	cout << "UBB_WORDS(56)   : " << NrWords<56, sizeof(WordT)>() << endl;
-	cout << "UBB_WORDS(64)   : " << NrWords<64, sizeof(WordT)>() << endl;
-	cout << "UBB_WORDS(80)   : " << NrWords<80, sizeof(WordT)>() << endl;
-	cout << "UBB_WORDS(96)   : " << NrWords<96, sizeof(WordT)>() << endl;
-	cout << "UBB_WORDS(112)  : " << NrWords<112, sizeof(WordT)>() << endl;
-	cout << "UBB_WORDS(128)  : " << NrWords<128, sizeof(WordT)>() << endl;
-
-	// this creates and masks the size of the requested number of bits Nb
-	// val & ~((~static_cast<unsigned long long>(0)) << Nb)
-	cout << hex << ((~static_cast<unsigned long long>(0)) << 8) << endl;
-	cout << hex << ((~static_cast<unsigned long long>(0)) << 16) << endl;
-	cout << hex << ((~static_cast<unsigned long long>(0)) << 32) << endl;
-	cout << hex << ((~static_cast<unsigned long long>(0)) << 64) << endl;
-	cout << hex << ((~static_cast<unsigned long long>(0)) << 128) << endl;
-	cout << hex << "0x" << (0x5555555555555555 & ~((~static_cast<unsigned long long>(0)) << 8)) << endl;
-	cout << hex << "0x" << (0x5555555555555555 & ~((~static_cast<unsigned long long>(0)) << 16)) << endl;
-	cout << hex << "0x" << (0x5555555555555555 & ~((~static_cast<unsigned long long>(0)) << 32)) << endl;
-	cout << hex << "0x" << (0x5555555555555555 & ~((~static_cast<unsigned long long>(0)) << 64)) << endl;
-	cout << dec;
-
-	bitblock<8> bb008;
-	bitblock<16> bb016;
-	bitblock<32> bb032;
-	bitblock<64> bb064;
-	bitblock<128> bb128, bbu64, bbl64;
-
-	bb008 = 0x55u;
-	bb016 = 0x5555u;
-	bb032 = 0x55555555ul;
-	bb064 = 0x5555555555555555;
-	bbl64 = 0x5555555555555555;
-	bbu64 = 0x5555555555555555;
-	bbu64 <<= 64;
-	bb128 = bbu64;
-	bb128 |= bbl64;
-
-	cout << "bb008: " << bb008 << " size in bytes: " << sizeof(bb008) << endl;
-	cout << "bb016: " << bb016 << " size in bytes: " << sizeof(bb016) << endl;
-	cout << "bb032: " << bb032 << " size in bytes: " << sizeof(bb032) << endl;
-	cout << "bb064: " << bb064 << " size in bytes: " << sizeof(bb064) << endl;
-	cout << "bb128: " << bb128 << " size in bytes: " << sizeof(bb128) << endl;
-
-	/*
 	nrOfFailedTestCases += ReportTestResult(ValidateBitsetAddition<3>(true), "bitblock<3>", "+");
 	nrOfFailedTestCases += ReportTestResult(ValidateBitsetSubtraction<3>(true), "bitblock<3>", "-");
 	nrOfFailedTestCases += ReportTestResult(ValidateBitsetMultiplication<3>(true), "bitblock<3>", "*");
 	nrOfFailedTestCases += ReportTestResult(ValidateBitsetDivision<3>(true), "bitblock<3>", "/");
-	*/
+	if (bReportIndividualTestCases) {
+		cout << "/n";
+	}
+
 #else
 
 	cout << "Test of operators on bitblocks" << endl;
