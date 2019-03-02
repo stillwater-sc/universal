9b8f3df047d4affb47c3847a0907dd671682fd51
Ravenwater <theo@stillwater-sc.com>
2019-03-02 16:22:51 -0500

    bug fix: fast mul for posit<8,0>

--- tests/posit/8bit_posit.cpp
+++ tests/posit/8bit_posit.cpp
@@ -18,6 +18,126 @@
 Standard posits with nbits = 8 have no exponent bits, i.e. es = 0.
 */
 
+#define signP8UI( a ) ((bool) ((uint8_t) (a)>>7))
+#define signregP8UI( a ) ((bool) (((uint8_t) (a)>>6) & 0x1))
+#define packToP8UI( regime, fracA) ((uint8_t) regime + ((uint8_t)(fracA)) )
+
+// mul reference from SoftPosit
+using posit8_t = uint8_t;
+
+posit8_t p8_mul(posit8_t pA, posit8_t pB) {
+	using namespace std;
+	posit8_t uA, uB, uZ;
+	uint_fast8_t uiA, uiB;
+	uint_fast8_t regA, fracA, regime, tmp;
+	bool signA, signB, signZ, regSA, regSB, bitNPlusOne = 0, bitsMore = 0, rcarry;
+	int_fast8_t kA = 0;
+	uint_fast16_t frac16Z;
+
+	uA = pA;
+	uiA = pA;
+	uB = pB;
+	uiB = pB;
+
+	cout << hex;
+	cout << "a = 0x" << int(uiA) << endl;
+	cout << "b = 0x" << int(uiB) << endl;
+	cout << dec;
+
+	//NaR or Zero
+	if (uiA == 0x80 || uiB == 0x80) {
+		uZ = 0x80;
+		return uZ;
+	}
+	else if (uiA == 0 || uiB == 0) {
+		uZ = 0;
+		return uZ;
+	}
+
+	signA = signP8UI(uiA);
+	signB = signP8UI(uiB);
+	signZ = signA ^ signB;
+
+	if (signA) uiA = (-uiA & 0xFF);
+	if (signB) uiB = (-uiB & 0xFF);
+
+	regSA = signregP8UI(uiA);
+	regSB = signregP8UI(uiB);
+
+	tmp = (uiA << 2) & 0xFF;
+	if (regSA) {
+		while (tmp >> 7) {
+			kA++;
+			tmp = (tmp << 1) & 0xFF;
+		}
+	}
+	else {
+		kA = -1;
+		while (!(tmp >> 7)) {
+			kA--;
+			tmp = (tmp << 1) & 0xFF;
+		}
+		tmp &= 0x7F;
+	}
+	fracA = (0x80 | tmp);
+
+	tmp = (uiB << 2) & 0xFF;
+	if (regSB) {
+		while (tmp >> 7) {
+			kA++;
+			tmp = (tmp << 1) & 0xFF;
+		}
+	}
+	else {
+		kA--;
+		while (!(tmp >> 7)) {
+			kA--;
+			tmp = (tmp << 1) & 0xFF;
+		}
+		tmp &= 0x7F;
+	}
+	frac16Z = (uint_fast16_t)fracA * (0x80 | tmp);
+	cout << hex << (frac16Z) << dec << endl;
+
+	rcarry = frac16Z >> 15;//1st bit of frac32Z
+	if (rcarry) {
+		kA++;
+		frac16Z >>= 1;
+	}
+
+	if (kA<0) {
+		regA = (-kA & 0xFF);
+		regSA = 0;
+		regime = 0x40 >> regA;
+	}
+	else {
+		regA = kA + 1;
+		regSA = 1;
+		regime = 0x7F - (0x7F >> regA);
+	}
+
+	if (regA>6) {
+		//max or min pos. exp and frac does not matter.
+		(regSA) ? (uZ = 0x7F) : (uZ = 0x1);
+	}
+	else {
+		//remove carry and rcarry bits and shift to correct position
+		frac16Z = (frac16Z & 0x3FFF) >> regA;
+		fracA = (uint_fast8_t)(frac16Z >> 8);
+		bitNPlusOne = (0x80 & frac16Z);
+		uZ = packToP8UI(regime, fracA);
+
+		//n+1 frac bit is 1. Need to check if another bit is 1 too if not round to even
+		if (bitNPlusOne) {
+			(0x7F & frac16Z) ? (bitsMore = 1) : (bitsMore = 0);
+			uZ += (uZ & 1) | bitsMore;
+		}
+	}
+
+	if (signZ) uZ = -uZ & 0xFF;
+	return uZ;
+}
+
 int main(int argc, char** argv)
 try {
 	using namespace std;
@@ -38,7 +158,22 @@ try {
 	cout << "Standard posit<8,0> configuration tests" << endl;
 #endif
 
-	posit<nbits,es> p;
+	// FAIL       +0.015625 *       +0.015625 !=       +0.015625 instead it yielded              +1 00000001 vs 01000000
+	// FAIL       +0.015625 *             -64 !=              -1 instead it yielded       -0.015625 11000000 vs 11111111
+	posit8_t a, b, c;
+	a = 0x01;
+	b = 0x81;
+	c = p8_mul(a, b);
+	cout << "c = 0x" << hex << int(c) << dec << endl;
+
+	posit<nbits, es> x, y, z;
+	x = 0.015625f;
+	y = -64.0f;
+	z = x * y;
+	cout << "x = " << x << " y = " << y << " " << posit_format(y) << endl;
+	cout << "z = " << z << endl;
+
+	posit<nbits, es> p;
 	cout << dynamic_range(p) << endl;
 
 	// logic tests
@@ -54,7 +189,7 @@ try {
 	// arithmetic tests
 	nrOfFailedTestCases += ReportTestResult( ValidateAddition         <nbits, es>(tag, bReportIndividualTestCases), tag, "add            ");
 	nrOfFailedTestCases += ReportTestResult( ValidateSubtraction      <nbits, es>(tag, bReportIndividualTestCases), tag, "subtract       ");
-//	nrOfFailedTestCases += ReportTestResult( ValidateMultiplication   <nbits, es>(tag, bReportIndividualTestCases), tag, "multiply       ");
+	nrOfFailedTestCases += ReportTestResult( ValidateMultiplication   <nbits, es>(tag, bReportIndividualTestCases), tag, "multiply       ");
 //	nrOfFailedTestCases += ReportTestResult( ValidateDivision         <nbits, es>(tag, bReportIndividualTestCases), tag, "divide         ");
 	nrOfFailedTestCases += ReportTestResult( ValidateNegation         <nbits, es>(tag, bReportIndividualTestCases), tag, "negate         ");
 //	nrOfFailedTestCases += ReportTestResult( ValidateReciprocation    <nbits, es>(tag, bReportIndividualTestCases), tag, "reciprocate    ");
