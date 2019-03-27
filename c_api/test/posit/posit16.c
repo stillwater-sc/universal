12b78ce1211ab4e3ce818e57851936259273611c
Ravenwater <theo@stillwater-sc.com>
2019-03-27 00:14:20 -0400

    streamlining C api naming convention

--- c_api/test/posit/posit16.c
+++ c_api/test/posit/posit16.c
@@ -3,9 +3,7 @@
 // Copyright (C) 2017-2019 Stillwater Supercomputing, Inc.
 //
 // This file is part of the universal numbers project, which is released under an MIT Open Source license.
-#include <stdlib.h>
-#include <stdio.h>
-#include <stdbool.h>
+
 #include <posit_c_api.h>
 
 int main(int argc, char* argv[]) 
@@ -18,45 +16,41 @@ int main(int argc, char* argv[])
 	pa = NAR16;
 	pb = ZERO16;
 	pc = padd16(pa, pb);
-	posit_format16(pc, str);
+	pformat16(pc, str);
 	printf("posit value = %s\n", str);
-	printf("posit value = 16.1x%04xp\n", pc);
 
 	pa = NAR16;
 	pb = ZERO16;
 	pc = psub16(pa, pb);
-	posit_format16(pc, str);
+	pformat16(pc, str);
 	printf("posit value = %s\n", str);
-	printf("posit value = 16.1x%04xp\n", pc);
 
 	pa = NAR16;
 	pb = ZERO16;
 	pc = pmul16(pa, pb);
-	posit_format16(pc, str);
+	pformat16(pc, str);
 	printf("posit value = %s\n", str);
-	printf("posit value = 16.1x%04xp\n", pc);
 
 	pa = NAR16;
 	pb = ZERO16;
 	pc = pdiv16(pa, pb);
-	posit_format16(pc, str);
+	pformat16(pc, str);
 	printf("posit value = %s\n", str);
-	printf("posit value = 16.1x%04xp\n", pc);
 
 	// partial state space
 	int fails = 0;
 	for (int a = 0; a < 256; ++a) {
-		pa = posit_bit_assign16(a);
+		pa = (posit16_t)a;
 		for (int b = 0; b < 256; ++b) {
-			pb = posit_bit_assign16(b);
+			pb = (posit16_t)b;
 			pc = padd16(pa, pb);
 
 			double da, db, dref;
-			da = posit_value16(pa);
-			db = posit_value16(pb);
+			da = pvalue16(pa);
+			db = pvalue16(pb);
 			dref = da + db;
 
-			posit16_t pref = posit_float_assign16((float)dref);
+			posit16_t pref = passign16f((float)dref);
 			if (pref != pc) {
 				printf("FAIL: 16.1x%04xp + 16.1x%04xp produced 16.1x%04xp instead of 16.1x%04xp\n", pa, pb, pc, pref);
 				++fails;
@@ -74,17 +68,17 @@ int main(int argc, char* argv[])
 	// partial state space
 	fails = 0;
 	for (int a = 0; a < 256; ++a) {
-		pa = posit_bit_assign16(a);
+		pa = (posit16_t)a;
 		for (int b = 0; b < 256; ++b) {
-			pb = posit_bit_assign16(b);
+			pb = (posit16_t)b;
 			pc = psub16(pa, pb);
 
 			double da, db, dref;
-			da = posit_value16(pa);
-			db = posit_value16(pb);
+			da = pvalue16(pa);
+			db = pvalue16(pb);
 			dref = da - db;
 
-			posit16_t pref = posit_float_assign16((float)dref);
+			posit16_t pref = passign16f((float)dref);
 			if (pref != pc) {
 				printf("FAIL: 16.1x%04xp - 16.1x%04xp produced 16.1x%04xp instead of 16.1x%04xp\n", pa, pb, pc, pref);
 				++fails;
@@ -102,17 +96,17 @@ int main(int argc, char* argv[])
 	// partial state space
 	fails = 0;
 	for (int a = 0; a < 256; ++a) {
-		pa = posit_bit_assign16(a);
+		pa = (posit16_t)a;
 		for (int b = 0; b < 256; ++b) {
-			pb = posit_bit_assign16(b);
+			pb = (posit16_t)b;
 			pc = pmul16(pa, pb);
 
 			double da, db, dref;
-			da = posit_value16(pa);
-			db = posit_value16(pb);
+			da = pvalue16(pa);
+			db = pvalue16(pb);
 			dref = da * db;
 
-			posit16_t pref = posit_float_assign16((float)dref);
+			posit16_t pref = passign16f((float)dref);
 			if (pref != pc) {
 				printf("FAIL: 16.1x%04xp * 16.1x%04xp produced 16.1x%04xp instead of 16.1x%04xp\n", pa, pb, pc, pref);
 				++fails;
@@ -130,17 +124,17 @@ int main(int argc, char* argv[])
 	// partial state space
 	fails = 0;
 	for (int a = 0; a < 256; ++a) {
-		pa = posit_bit_assign16(a);
+		pa = (posit16_t)a;
 		for (int b = 0; b < 256; ++b) {
-			pb = posit_bit_assign16(b);
+			pb = (posit16_t)b;
 			pc = pdiv16(pa, pb);
 
 			double da, db, dref;
-			da = posit_value16(pa);
-			db = posit_value16(pb);
+			da = pvalue16(pa);
+			db = pvalue16(pb);
 			dref = da / db;
 
-			posit16_t pref = posit_float_assign16((float)dref);
+			posit16_t pref = passign16f((float)dref);
 			if (pref != pc) {
 				printf("FAIL: 16.1x%04xp / 16.1x%04xp produced 16.1x%04xp instead of 16.1x%04xp\n", pa, pb, pc, pref);
 				++fails;
