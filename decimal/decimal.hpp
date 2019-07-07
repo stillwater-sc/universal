15fd52fa57daf7f2fb67a335d2c151cc5e6d9f11
Ravenwater <theo@stillwater-sc.com>
2019-07-06 20:08:16 -0400

    adding iomanip include file

--- decimal/decimal.hpp
+++ decimal/decimal.hpp
@@ -6,6 +6,8 @@
 // This file is part of the universal numbers project, which is released under an MIT Open Source license.
 
 #include <sstream>
+#include <iostream>
+#include <iomanip>
 #include <vector>
 #include <regex>
 
--- integer/integer.hpp
+++ integer/integer.hpp
@@ -6,6 +6,8 @@
 // This file is part of the universal numbers project, which is released under an MIT Open Source license.
 
 #include <sstream>
+#include <iostream>
+#include <iomanip>
 #include <regex>
 
 #if defined(__clang__)
