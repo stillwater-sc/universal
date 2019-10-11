3d77c28e35b038af312250cee3fc7e721420bbb8
Ravenwater <theo@stillwater-sc.com>
2019-10-11 17:18:59 -0400

    WIP: adding fastdiv experiment

--- tests/integer/integers.cpp
+++ tests/integer/integers.cpp
@@ -336,6 +336,160 @@ void GenerateMulTest(const Scalar& x, const Scalar& y, Scalar& z) {
 	std::cout << typeid(Scalar).name() << ": " << x << " * " << y << " = " << z << std::endl;
 }
 
+namespace fid {
+// fast integer divide
+class int_fastdiv
+{
+public:
+	// divisor != 0 
+	int_fastdiv(int divisor = 0) : d(divisor) {
+		update_magic_numbers();
+	}
+
+	int_fastdiv& operator =(int divisor) {
+		this->d = divisor;
+		update_magic_numbers();
+		return *this;
+	}
+
+	operator int() const { return d; }
+
+	void info() {
+		std::cout << "d   : " << d << std::endl;
+		std::cout << "M   : " << M << std::endl;
+		std::cout << "s   : " << s << std::endl;
+		std::cout << "n_add_sign : " << n_add_sign << std::endl;
+	}
+
+private:
+	int d;
+	int M;
+	int s;
+	int n_add_sign;
+
+	// Hacker's Delight, Second Edition, Chapter 10, Integer Division By Constants
+	void update_magic_numbers()
+	{
+		if (d == 1)	{
+			M = 0;
+			s = -1;
+			n_add_sign = 1;
+			return;
+		}
+		else if (d == -1) {
+			M = 0;
+			s = -1;
+			n_add_sign = -1;
+			return;
+		}
+
+		int p;
+		unsigned int ad, anc, delta, q1, r1, q2, r2, t;
+		const unsigned two31 = 0x80000000;
+		ad = (d == 0) ? 1 : abs(d);
+		t = two31 + ((unsigned int)d >> 31);
+		anc = t - 1 - t % ad;
+		p = 31;
+		q1 = two31 / anc;
+		r1 = two31 - q1 * anc;
+		q2 = two31 / ad;
+		r2 = two31 - q2 * ad;
+		do {
+			++p;
+			q1 = 2 * q1;
+			r1 = 2 * r1;
+			if (r1 >= anc) {
+				++q1;
+				r1 -= anc;
+			}
+			q2 = 2 * q2;
+			r2 = 2 * r2;
+			if (r2 >= ad) {
+				++q2;
+				r2 -= ad;
+			}
+			delta = ad - r2;
+		} while (q1 < delta || (q1 == delta && r1 == 0));
+		this->M = q2 + 1;
+		if (d < 0)		this->M = -this->M;
+		this->s = p - 32;
+
+		if ((d > 0) && (M < 0)) {
+			n_add_sign = 1;
+		} 
+		else if ((d < 0) && (M > 0)) {
+			n_add_sign = -1;
+		}
+		else {
+			n_add_sign = 0;
+		}
+	}
+
+	friend int operator/(const int divident, const int_fastdiv& divisor);
+};
+
+int operator/(const int n, const int_fastdiv& divisor) {
+	int q = (((unsigned long long)((long long)divisor.M * (long long)n)) >> 32);
+	q += n * divisor.n_add_sign;
+	if (divisor.s >= 0)	{
+		q >>= divisor.s; // we rely on this to be implemented as arithmetic shift
+		q += (((unsigned int)q) >> 31);
+	}
+	return q;
+}
+
+int operator%(const int n, const int_fastdiv& divisor){
+	int quotient = n / divisor;
+	int remainder = n - quotient * divisor;
+	return remainder;
+}
+
+int operator/(const unsigned int n, const int_fastdiv& divisor)		{	return ((int)n) / divisor; }
+int operator%(const unsigned int n, const int_fastdiv& divisor)		{	return ((int)n) % divisor; }
+int operator/(const short n, const int_fastdiv& divisor)			{	return ((int)n) / divisor; }
+int operator%(const short n, const int_fastdiv& divisor)			{	return ((int)n) % divisor; }
+int operator/(const unsigned short n, const int_fastdiv& divisor)	{	return ((int)n) / divisor; }
+int operator%(const unsigned short n, const int_fastdiv& divisor)	{	return ((int)n) % divisor; }
+int operator/(const char n, const int_fastdiv& divisor)				{	return ((int)n) / divisor; }
+int operator%(const char n, const int_fastdiv& divisor)				{	return ((int)n) % divisor; }
+int operator/(const unsigned char n, const int_fastdiv& divisor)	{	return ((int)n) / divisor; }
+int operator%(const unsigned char n, const int_fastdiv& divisor)	{	return ((int)n) % divisor; }
+
+}
+
+int cpu_check() {
+	using namespace fid;
+	const int divisor_count = 100000;
+	const int divident_count = 1000000;
+	std::cout << "Running CPU functional test on " << divisor_count << " divisors, with " << divident_count << " dividents for each divisor" << std::endl;
+	for (int d = 1; d < divisor_count; ++d) {
+		for (int sign = 1; sign >= -1; sign -= 2) {
+			int divisor = d * sign;
+
+			// std::cout << "Checking divisor " << divisor << "... ";
+
+			int_fastdiv fast_divisor(divisor);
+
+			for (int dd = 0; dd < divident_count; ++dd)	{
+				for (int ss = 1; ss >= -1; ss -= 2)	{
+					int divident = dd * ss;
+
+					int quotient = divident / divisor;
+					int fast_quotient = divident / fast_divisor;
+					if (quotient != fast_quotient) {
+						std::cout << "wrong result for divident " << divident << ", correct quotient = " << quotient << ", fast computed quotient = " << fast_quotient << std::endl;
+						return 1;
+					}
+				}
+			}
+
+			// std::cout << "done" << std::endl;
+		}
+	}
+
+	return 0;
+}
+
 #define MANUAL_TESTING 1
 #define STRESS_TESTING 0
 
@@ -389,7 +543,33 @@ try {
 		cout << to_binary(a) << endl;
 	}
 
-	ReportTestResult(VerifyDivision<4>("manual test", true), "integer<4>", "divides");
+	// fast_div
+	fid::int_fastdiv fast_divisor;
+	cout << "size of fastdiv: " << sizeof(fast_divisor) << endl;
+	fast_divisor.info();
+	fast_divisor = 2;
+	fast_divisor.info();
+	fast_divisor = 3;
+	fast_divisor.info();
+	fast_divisor = 4;
+	fast_divisor.info();
+	fast_divisor = 7;
+	fast_divisor.info();
+	fast_divisor = 8;
+	fast_divisor.info();
+
+	// int q = n / divisor;
+	// int q = hi32bits(n * M) >> s;
+	for (int i = 0; i < 10; i++) {
+		int divisor = rand();
+		fid::int_fastdiv fast_divisor(divisor);
+		cout << "divisor : " << divisor << std::endl;
+		fast_divisor.info();
+	}
+
+	//cpu_check();
+
+	//ReportTestResult(VerifyDivision<4>("manual test", true), "integer<4>", "divides");
 
 	cout << "done" << endl;
 
