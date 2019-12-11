fd9c34e2c0183e2c9d15c9515b4ae8ef7c7e492a
Ravenwater <theo@stillwater-sc.com>
2019-12-10 19:19:14 -0500

    adding playground for valid elementary function evaluations

--- examples/playground/efunc_valids.cpp
+++ examples/playground/efunc_valids.cpp
@@ -15,66 +15,70 @@
 
 int main(int argc, char** argv)
 try {
+	using namespace std;
 	using namespace sw::unum;
 	bool bSuccess = true;
 
-	valid<32, 2> x(1.0), y(-1.0), p(0);
+	cout << "valid elementary functions not implemented yet" << endl;
+	/*
+	valid<32, 2> x(1.0), y(-1.0), v(0.0);
 	std::complex< valid<32, 2> > c(x,y),d;
 
 	// sign and absolute value
 	bool s;
 	s = sign(x); std::cout << "Sign of " << x << " is " << s << '\n';
 	s = sign(y); std::cout << "Sign of " << y << " is " << s << '\n';
-	p =  abs(y); std::cout << " abs(" << y << ") = " << p << '\n';
-	p = fabs(y); std::cout << "fabs(" << y << ") = " << p << '\n';
+	v = abs(y); std::cout << "   abs(" << y << ")    = " << std::setw(10) << v << " " << color_print(v) << '\n';
+	v = fabs(y); std::cout << "  fabs(" << y << ")    = " << std::setw(10) << v << " " << color_print(v) << '\n';
 
 	// truncation functions
-	x = 1.5;
-	p =  ceil(x); std::cout << " ceil(" << x << ") = " << p << '\n';
-	p = floor(x); std::cout << "floor(" << x << ") = " << p << '\n';
-	p = round(x); std::cout << "round(" << x << ") = " << p << '\n';
-	p = trunc(x); std::cout << "trunc(" << x << ") = " << p << '\n';
+	x = 1.50001;
+	v = ceil(x); std::cout << " ceil(" << x << ")     = " << std::setw(10) << v << " " << color_print(v) << '\n';
+	v = floor(x); std::cout << "floor(" << x << ")     = " << std::setw(10) << v << " " << color_print(v) << '\n';
+	v = round(x); std::cout << "round(" << x << ")     = " << std::setw(10) << v << " " << color_print(v) << '\n';
+	v = trunc(x); std::cout << "trunc(" << x << ")     = " << std::setw(10) << v << " " << color_print(v) << '\n';
 
 	// complex functions
-	p = real(c); std::cout << "real(" << c << ") = " << p << '\n';
-	p = imag(c); std::cout << "imag(" << c << ") = " << p << '\n';
-	d = conj(c); std::cout << "conj(" << c << ") = " << d << '\n';
-	
+	v = real(c); std::cout << " real(" << c << ") = " << std::setw(10) << v << " " << color_print(v) << '\n';
+	v = imag(c); std::cout << " imag(" << c << ") = " << std::setw(10) << v << " " << color_print(v) << '\n';
+	d = conj(c); std::cout << " conj(" << c << ") = " << std::setw(10) << d << " " << color_print(d.real()) << "," << color_print(d.imag()) << '\n';
+
 	// sqrt and inverse sqrt
 	x = m_pi_4;
-	p =  sqrt(x); std::cout << " sqrt(" << x << ")    = " << std::setw(10) << p << " " << color_print(p) << '\n';
-	p = rsqrt(x); std::cout << "rsqrt(" << x << ")    = " << std::setw(10) << p << " " << color_print(p) << '\n';
+	v =  sqrt(x); std::cout << " sqrt(" << x << ")    = " << std::setw(10) << v << " " << color_print(v) << '\n';
+	v = rsqrt(x); std::cout << "rsqrt(" << x << ")    = " << std::setw(10) << v << " " << color_print(v) << '\n';
 
 	// trigonometric functions
-	p =  sin(x); std::cout << "  sin(" << x << ")    = " << std::setw(10) << p << " " << color_print(p) << '\n';
-	p = asin(x); std::cout << " asin(" << x << ")    = " << std::setw(10) << p << " " << color_print(p) << '\n';
-	p =  cos(x); std::cout << "  cos(" << x << ")    = " << std::setw(10) << p << " " << color_print(p) << '\n';
-	p = acos(x); std::cout << " acos(" << x << ")    = " << std::setw(10) << p << " " << color_print(p) << '\n';
-	p =  tan(x); std::cout << "  tan(" << x << ")    = " << std::setw(10) << p << " " << color_print(p) << '\n';
-	p = atan(x); std::cout << " atan(" << x << ")    = " << std::setw(10) << p << " " << color_print(p) << '\n';
+	v =  sin(x); std::cout << "  sin(" << x << ")    = " << std::setw(10) << v << " " << color_print(v) << '\n';
+	v = asin(x); std::cout << " asin(" << x << ")    = " << std::setw(10) << v << " " << color_print(v) << '\n';
+	v =  cos(x); std::cout << "  cos(" << x << ")    = " << std::setw(10) << v << " " << color_print(v) << '\n';
+	v = acos(x); std::cout << " acos(" << x << ")    = " << std::setw(10) << v << " " << color_print(v) << '\n';
+	v =  tan(x); std::cout << "  tan(" << x << ")    = " << std::setw(10) << v << " " << color_print(v) << '\n';
+	v = atan(x); std::cout << " atan(" << x << ")    = " << std::setw(10) << v << " " << color_print(v) << '\n';
 
 	// hyperbolic functions
-	p =  sinh(x); std::cout << " sinh(" << x << ")    = " << std::setw(10) << p << " " << color_print(p) << '\n';
-	p = asinh(x); std::cout << "asinh(" << x << ")    = " << std::setw(10) << p << " " << color_print(p) << '\n';
-	p =  cosh(x); std::cout << " cosh(" << x << ")    = " << std::setw(10) << p << " " << color_print(p) << '\n';
-	p = acosh(x); std::cout << "acosh(" << x << ")    = " << std::setw(10) << p << " " << color_print(p) << '\n';
-	p =  tanh(x); std::cout << " tanh(" << x << ")    = " << std::setw(10) << p << " " << color_print(p) << '\n';
-	p = atanh(x); std::cout << "atanh(" << x << ")    = " << std::setw(10) << p << " " << color_print(p) << '\n';
+	v =  sinh(x); std::cout << " sinh(" << x << ")    = " << std::setw(10) << v << " " << color_print(v) << '\n';
+	v = asinh(x); std::cout << "asinh(" << x << ")    = " << std::setw(10) << v << " " << color_print(v) << '\n';
+	v =  cosh(x); std::cout << " cosh(" << x << ")    = " << std::setw(10) << v << " " << color_print(v) << '\n';
+	v = acosh(x); std::cout << "acosh(" << x << ")    = " << std::setw(10) << v << " " << color_print(v) << '\n';
+	v =  tanh(x); std::cout << " tanh(" << x << ")    = " << std::setw(10) << v << " " << color_print(v) << '\n';
+	v = atanh(x); std::cout << "atanh(" << x << ")    = " << std::setw(10) << v << " " << color_print(v) << '\n';
 
 	// error and gamma functions
-	p =  erf(x); std::cout << "  erf(" << x << ")    = " << std::setw(10) << p << " " << color_print(p) << '\n';
-	p = erfc(x); std::cout << " erfc(" << x << ")    = " << std::setw(10) << p << " " << color_print(p) << '\n';
+	v =  erf(x); std::cout << "  erf(" << x << ")    = " << std::setw(10) << v << " " << color_print(v) << '\n';
+	v = erfc(x); std::cout << " erfc(" << x << ")    = " << std::setw(10) << v << " " << color_print(v) << '\n';
 
 	// exponential and logarithmic functions
 
-	p =   exp(x); std::cout << "  exp(" << x << ")    = " << std::setw(10) << p << " " << color_print(p) << '\n';
-	p =  exp2(x); std::cout << " exp2(" << x << ")    = " << std::setw(10) << p << " " << color_print(p) << '\n';
-	p = exp10(x); std::cout << "exp10(" << x << ")    = " << std::setw(10) << p << " " << color_print(p) << '\n';
-	p =   log(x); std::cout << "  log(" << x << ")    = " << std::setw(10) << p << " " << color_print(p) << '\n';
-	p =  log2(x); std::cout << " log2(" << x << ")    = " << std::setw(10) << p << " " << color_print(p) << '\n';
-	p = log10(x); std::cout << "log10(" << x << ")    = " << std::setw(10) << p << " " << color_print(p) << '\n';
+	v =   exp(x); std::cout << "  exp(" << x << ")    = " << std::setw(10) << v << " " << color_print(v) << '\n';
+	v =  exp2(x); std::cout << " exp2(" << x << ")    = " << std::setw(10) << v << " " << color_print(v) << '\n';
+	v = exp10(x); std::cout << "exp10(" << x << ")    = " << std::setw(10) << v << " " << color_print(v) << '\n';
+	v =   log(x); std::cout << "  log(" << x << ")    = " << std::setw(10) << v << " " << color_print(v) << '\n';
+	v =  log2(x); std::cout << " log2(" << x << ")    = " << std::setw(10) << v << " " << color_print(v) << '\n';
+	v = log10(x); std::cout << "log10(" << x << ")    = " << std::setw(10) << v << " " << color_print(v) << '\n';
 
-	p = pow(x, y); std::cout << "  pow(" << x << "," << y << ") = " << std::setw(10) << p << " " << color_print(p) << '\n';
+	v = pow(x, y); std::cout << "  pow(" << x << "," << y << ") = " << std::setw(10) << v << " " << color_print(v) << '\n';
+	*/
 
 	return (bSuccess ? EXIT_SUCCESS : EXIT_FAILURE);
 }
