894e646871768595ae6c2a57ef7793820deff34b
Ravenwater <theo@stillwater-sc.com>
2020-01-22 20:39:48 -0500

    WIP: adding engineering and science examples

---
+++ examples/science/iso_constants.cpp
@@ -0,0 +1,45 @@
+//  chem_equilibrium.cpp : example of calculating the chemical balance of a solution
+//
+// Copyright (C) 2017-2020 Stillwater Supercomputing, Inc.
+//
+// This file is part of the universal numbers project, which is released under an MIT Open Source license.
+#include <iostream>
+#include <universal/posit/posit>
+
+/*
+ * ISO constants
+ */
+
+int main()
+try {
+	using namespace std;
+
+	cout << "ISO Constants\n";
+
+    
+	return EXIT_SUCCESS;
+}
+catch (char const* msg) {
+	std::cerr << msg << std::endl;
+	return EXIT_FAILURE;
+}
+catch (const posit_arithmetic_exception& err) {
+	std::cerr << "Uncaught posit arithmetic exception: " << err.what() << std::endl;
+	return EXIT_FAILURE;
+}
+catch (const quire_exception& err) {
+	std::cerr << "Uncaught quire exception: " << err.what() << std::endl;
+	return EXIT_FAILURE;
+}
+catch (const posit_internal_exception& err) {
+	std::cerr << "Uncaught posit internal exception: " << err.what() << std::endl;
+	return EXIT_FAILURE;
+}
+catch (const std::runtime_error& err) {
+	std::cerr << "Uncaught runtime exception: " << err.what() << std::endl;
+	return EXIT_FAILURE;
+}
+catch (...) {
+	std::cerr << "Caught unknown exception" << std::endl;
+	return EXIT_FAILURE;
+}
