451df5d846fd3153196b97a708b8f377d9aae613
Ravenwater <theo@stillwater-sc.com>
2020-02-02 19:27:58 -0500

    compilation fix for gcc

--- include/universal/fixpnt/fixpnt_functions.hpp
+++ include/universal/fixpnt/fixpnt_functions.hpp
@@ -4,6 +4,7 @@
 // Copyright (C) 2017-2020 Stillwater Supercomputing, Inc.
 //
 // This file is part of the universal numbers project, which is released under an MIT Open Source license.
+#include <cmath> // for std:pow()
 #include <universal/bitblock/bitblock.hpp>
 
 namespace sw {
