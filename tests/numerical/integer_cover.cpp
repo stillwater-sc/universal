3476ecc48d277c5824c0cc213dc60719e9ed18b5
Ravenwater <theo@stillwater-sc.com>
2020-03-28 10:38:12 -0400

    revisiting the linear and integer covers of posits

--- tests/numerical/integer_cover.cpp
+++ tests/numerical/integer_cover.cpp
@@ -9,50 +9,108 @@
 #include "../tests/utils/posit_test_helpers.hpp"
 
 /*
-When using a discretization scheme, for example, an Analog-to-Digital converter, 
-we have a set of integers that can be turned into fractions by normalizing to the
-state space of possible samples. These sets can be projected to different ranges.
-For example, a ADC channel generates values from 0 to (2^width - 1), and they can
-represent a value range of -2^width-1,...,-1,0,1,... 2^width-1 -1.
+When interacting with integer algebras, for example, prime factorization
+algorithms, factorials, binomials, and discretization schemes, a posit
+would need to be able to represent the integers to be error-free.
 
-We are interested to see how well a posit configuration can capture these
-integer values, and how well they can capture the fractions when mapping to 
-different regions.
+We are interested to see how well a posit configuration can capture integer values.
+
+A sample output of this program to provide insight in the sampling behavior of 
+linear and tapered floating point:
+Posit Integer Cover
+posit sample coverage of 8-bit unsigned integer
+posit<12,0>: 2^8  integer cover is : float cover = 100%  double cover = 100%  posit cover = 23.44%
+posit<12,1>: 2^8  integer cover is : float cover = 100%  double cover = 100%  posit cover = 50%
+posit<12,2>: 2^8  integer cover is : float cover = 100%  double cover = 100%  posit cover = 75%
+posit<13,0>: 2^8  integer cover is : float cover = 100%  double cover = 100%  posit cover = 34.38%
+posit<13,1>: 2^8  integer cover is : float cover = 100%  double cover = 100%  posit cover = 75%
+posit<13,2>: 2^8  integer cover is : float cover = 100%  double cover = 100%  posit cover = 100%
+posit<14,0>: 2^8  integer cover is : float cover = 100%  double cover = 100%  posit cover = 43.75%
+posit<14,1>: 2^8  integer cover is : float cover = 100%  double cover = 100%  posit cover = 100%
+posit<14,1>: 2^8  integer cover is : float cover = 100%  double cover = 100%  posit cover = 100%
+posit<15,0>: 2^8  integer cover is : float cover = 100%  double cover = 100%  posit cover = 62.5%
+posit<15,1>: 2^8  integer cover is : float cover = 100%  double cover = 100%  posit cover = 100%
+posit<15,2>: 2^8  integer cover is : float cover = 100%  double cover = 100%  posit cover = 100%
+posit<16,0>: 2^8  integer cover is : float cover = 100%  double cover = 100%  posit cover = 75%
+posit<16,1>: 2^8  integer cover is : float cover = 100%  double cover = 100%  posit cover = 100%
+posit sample coverage of 10-bit unsigned integer
+posit<16,1>: 2^10 integer cover is : float cover = 100%  double cover = 100%  posit cover = 75%
+posit<17,1>: 2^10 integer cover is : float cover = 100%  double cover = 100%  posit cover = 100%
+posit<18,1>: 2^12 integer cover is : float cover = 100%  double cover = 100%  posit cover = 100%
+posit<15,2>: 2^10 integer cover is : float cover = 100%  double cover = 100%  posit cover = 75%
+posit<16,2>: 2^10 integer cover is : float cover = 100%  double cover = 100%  posit cover = 100%
+posit<17,2>: 2^10 integer cover is : float cover = 100%  double cover = 100%  posit cover = 100%
+posit<18,2>: 2^10 integer cover is : float cover = 100%  double cover = 100%  posit cover = 100%
+posit sample coverage of 12-bit unsigned integer
+posit<18,1>: 2^12 integer cover is : float cover = 100%  double cover = 100%  posit cover = 50%
+posit<19,1>: 2^12 integer cover is : float cover = 100%  double cover = 100%  posit cover = 75%
+posit<20,1>: 2^12 integer cover is : float cover = 100%  double cover = 100%  posit cover = 100%
+posit sample coverage of 14-bit unsigned integer
+posit<20,2>: 2^14 integer cover is : float cover = 100%  double cover = 100%  posit cover = 75%
+posit<24,1>: 2^14 integer cover is : float cover = 100%  double cover = 100%  posit cover = 100%
+posit<28,1>: 2^14 integer cover is : float cover = 100%  double cover = 100%  posit cover = 100%
+posit sample coverage of 16-bit unsigned integer
+posit<20,1>: 2^16 integer cover is : float cover = 100%  double cover = 100%  posit cover = 10.94%
+posit<24,1>: 2^16 integer cover is : float cover = 100%  double cover = 100%  posit cover = 50%
+posit<28,1>: 2^16 integer cover is : float cover = 100%  double cover = 100%  posit cover = 100%
+posit<32,1>: 2^16 integer cover is : float cover = 100%  double cover = 100%  posit cover = 100%
+posit<32,2>: 2^16 integer cover is : float cover = 100%  double cover = 100%  posit cover = 100%
+posit sample coverage of 20-bit unsigned integer
+posit<20,1>: 2^20 integer cover is : float cover = 100%  double cover = 100%  posit cover = 0.7568%
+posit<26,1>: 2^20 integer cover is : float cover = 100%  double cover = 100%  posit cover = 10.94%
+posit<32,2>: 2^20 integer cover is : float cover = 100%  double cover = 100%  posit cover = 100%
+posit sample coverage of 24-bit unsigned integer
+posit<26,1>: 2^24 integer cover is : float cover = 100%  double cover = 100%  posit cover = 0.7568%
+posit<32,2>: 2^24 integer cover is : float cover = 100%  double cover = 100%  posit cover = 75%
+posit<34,2>: 2^24 integer cover is : float cover = 100%  double cover = 100%  posit cover = 100%
 */
 
-template<size_t nbits, size_t es, size_t adc_width>
-double CalculateIntegerCover(bool verbose = false) {
-	sw::unum::posit<nbits, es> pLevel;// , pFraction;
+// Calculate the sample cover for a posit representing an unsigned long.
+// unsigned is the worst case as all the values are mapped to just a
+// quarter of the posit encodings (the North-East quadrant of the unit circle projection of the reals)
+// The number of samples in a posit quadrant is 2^(nbits - 2). 
+// Thus mathematically the 2^(nbits-2) samples need to cover the 2^ibits values of the integer.
+template<size_t nbits, size_t es, size_t ibits>
+std::string CalculateIntegerCover() {
+	float fInt;
+	double dInt;
+	sw::unum::posit<nbits, es> pInt;
 
-	constexpr unsigned long nrSamples = (unsigned long)1 << adc_width;
-	unsigned long cover = 0;
-	for (unsigned long level = 0; level < nrSamples; ++level) {
-		pLevel = level;
-		//pFraction = level / (double)nrSamples;
-		unsigned long rounded = (unsigned long)pLevel;
-		if (rounded == level) {
-			++cover;
+	constexpr uint64_t nrSamples = (uint64_t)1 << ibits;
+	uint64_t fcover = 0, dcover = 0, pcover = 0, rounded;
+	for (uint64_t integer = 0; integer < nrSamples; ++integer) {
+		// float cover
+		fInt = float(integer);
+		rounded = uint64_t(fInt);
+		if (rounded == integer) {
+			++fcover;
 		}
-		else {
-			if (verbose) std::cout << "level = " << level << " rounded to " << rounded << "\n";
+		// double cover
+		// float cover
+		dInt = double(integer);
+		rounded = uint64_t(dInt);
+		if (rounded == integer) {
+			++dcover;
+		}
+		// posit cover
+		pInt = integer;
+		rounded = uint64_t(pInt);
+		if (rounded == integer) {
+			++pcover;
 		}
 	}
-	return 100.0 * (double)cover / nrSamples;
+	std::stringstream ss;
+	ss << std::setprecision(4);
+	ss << "float cover = " << 100.0 * (double)fcover / nrSamples << "%  ";
+	ss << "double cover = " << 100.0 * (double)dcover / nrSamples << "%  ";
+	ss << "posit cover = " << 100.0 * (double)pcover / nrSamples << "%";
+	return ss.str();
 }
-
-#define MANUAL_TEST 1
 
 int main(int argc, char** argv)
 try {
 	using namespace std;
 	using namespace sw::unum;
-
-	// print detailed bit-level computational intermediate results
-	// bool verbose = false;
-
-	int nrOfFailedTestCases = 0;
-	// bool bReportIndividualTestCases = true;
-	std::string tag = "TwoSum failed: ";
 
 	// preserve the existing ostream precision
 	auto precision = cout.precision();
@@ -60,60 +118,72 @@ try {
 
 	cout << "Posit Integer Cover" << endl;
 
-#if MANUAL_TEST
-	cout << "8-bit ADC sample coverage\n";
-	cout << "posit<12,0>: 2^8 integer cover is : " << CalculateIntegerCover<12, 0, 8>() << "%\n";
-	cout << "posit<12,1>: 2^8 integer cover is : " << CalculateIntegerCover<12, 1, 8>() << "%\n";
-	cout << "posit<12,2>: 2^8 integer cover is : " << CalculateIntegerCover<12, 2, 8>() << "%\n";
+	cout << "posit sample coverage of 8-bit unsigned integer\n";
+	cout << "posit<12,0>: 2^8  integer cover is : " << CalculateIntegerCover<12, 0, 8>() << "\n";
+	cout << "posit<12,1>: 2^8  integer cover is : " << CalculateIntegerCover<12, 1, 8>() << "\n";
+	cout << "posit<12,2>: 2^8  integer cover is : " << CalculateIntegerCover<12, 2, 8>() << "\n";
 
-	cout << "posit<13,0>: 2^8 integer cover is : " << CalculateIntegerCover<13, 0, 8>() << "%\n";
-	cout << "posit<13,1>: 2^8 integer cover is : " << CalculateIntegerCover<13, 1, 8>() << "%\n";
-	cout << "posit<13,2>: 2^8 integer cover is : " << CalculateIntegerCover<13, 2, 8>() << "%\n";
+	cout << "posit<13,0>: 2^8  integer cover is : " << CalculateIntegerCover<13, 0, 8>() << "\n";
+	cout << "posit<13,1>: 2^8  integer cover is : " << CalculateIntegerCover<13, 1, 8>() << "\n";
+	cout << "posit<13,2>: 2^8  integer cover is : " << CalculateIntegerCover<13, 2, 8>() << "\n";
 
-	cout << "posit<14,0>: 2^8 integer cover is : " << CalculateIntegerCover<14, 0, 8>() << "%\n";
-	cout << "posit<14,1>: 2^8 integer cover is : " << CalculateIntegerCover<14, 1, 8>() << "%\n";
-	cout << "posit<14,1>: 2^8 integer cover is : " << CalculateIntegerCover<14, 2, 8>() << "%\n";
+	cout << "posit<14,0>: 2^8  integer cover is : " << CalculateIntegerCover<14, 0, 8>() << "\n";
+	cout << "posit<14,1>: 2^8  integer cover is : " << CalculateIntegerCover<14, 1, 8>() << "\n";
+	cout << "posit<14,1>: 2^8  integer cover is : " << CalculateIntegerCover<14, 2, 8>() << "\n";
 
-	cout << "posit<15,0>: 2^8 integer cover is : " << CalculateIntegerCover<15, 0, 8>() << "%\n";
-	cout << "posit<15,1>: 2^8 integer cover is : " << CalculateIntegerCover<15, 1, 8>() << "%\n";	
-	cout << "posit<15,2>: 2^8 integer cover is : " << CalculateIntegerCover<15, 2, 8>() << "%\n";
+	cout << "posit<15,0>: 2^8  integer cover is : " << CalculateIntegerCover<15, 0, 8>() << "\n";
+	cout << "posit<15,1>: 2^8  integer cover is : " << CalculateIntegerCover<15, 1, 8>() << "\n";	
+	cout << "posit<15,2>: 2^8  integer cover is : " << CalculateIntegerCover<15, 2, 8>() << "\n";
 
-	cout << "posit<16,0>: 2^8 integer cover is : " << CalculateIntegerCover<16, 0, 8>() << "%\n";
-	cout << "posit<16,1>: 2^8 integer cover is : " << CalculateIntegerCover<16, 1, 8>() << "%\n";
+	cout << "posit<16,0>: 2^8  integer cover is : " << CalculateIntegerCover<16, 0, 8>() << "\n";
+	cout << "posit<16,1>: 2^8  integer cover is : " << CalculateIntegerCover<16, 1, 8>() << "\n";
 
-	cout << "10-bit ADC sample coverage\n";
-	cout << "posit<16,1>: 2^10 integer cover is : " << CalculateIntegerCover<16, 1, 10>() << "%\n";
-	cout << "posit<17,1>: 2^10 integer cover is : " << CalculateIntegerCover<17, 1, 10>() << "%\n";
-	cout << "posit<18,1>: 2^12 integer cover is : " << CalculateIntegerCover<18, 1, 10>() << "%\n";
-	cout << "posit<15,2>: 2^10 integer cover is : " << CalculateIntegerCover<15, 2, 10>() << "%\n";
-	cout << "posit<16,2>: 2^10 integer cover is : " << CalculateIntegerCover<16, 2, 10>() << "%\n";
-	cout << "posit<17,2>: 2^10 integer cover is : " << CalculateIntegerCover<17, 2, 10>() << "%\n";
-	cout << "posit<18,2>: 2^10 integer cover is : " << CalculateIntegerCover<18, 2, 10>() << "%\n";
+	cout << "posit sample coverage of 10-bit unsigned integer\n";
+	cout << "posit<16,1>: 2^10 integer cover is : " << CalculateIntegerCover<16, 1, 10>() << "\n";
+	cout << "posit<17,1>: 2^10 integer cover is : " << CalculateIntegerCover<17, 1, 10>() << "\n";
+	cout << "posit<18,1>: 2^12 integer cover is : " << CalculateIntegerCover<18, 1, 10>() << "\n";
+	cout << "posit<15,2>: 2^10 integer cover is : " << CalculateIntegerCover<15, 2, 10>() << "\n";
+	cout << "posit<16,2>: 2^10 integer cover is : " << CalculateIntegerCover<16, 2, 10>() << "\n";
+	cout << "posit<17,2>: 2^10 integer cover is : " << CalculateIntegerCover<17, 2, 10>() << "\n";
+	cout << "posit<18,2>: 2^10 integer cover is : " << CalculateIntegerCover<18, 2, 10>() << "\n";
 
-	cout << "12-bit ADC sample coverage\n";
-	cout << "posit<18,1>: 2^12 integer cover is : " << CalculateIntegerCover<18, 1, 12>() << "%\n";
-	cout << "posit<19,1>: 2^12 integer cover is : " << CalculateIntegerCover<19, 1, 12>() << "%\n";
-	cout << "posit<20,1>: 2^12 integer cover is : " << CalculateIntegerCover<20, 1, 12>() << "%\n";
+	cout << "posit sample coverage of 12-bit unsigned integer\n";
+	cout << "posit<18,1>: 2^12 integer cover is : " << CalculateIntegerCover<18, 1, 12>() << "\n";
+	cout << "posit<19,1>: 2^12 integer cover is : " << CalculateIntegerCover<19, 1, 12>() << "\n";
+	cout << "posit<20,1>: 2^12 integer cover is : " << CalculateIntegerCover<20, 1, 12>() << "\n";
 
-	cout << "14-bit ADC sample coverage\n";
-	cout << "posit<20,2>: 2^14 integer cover is : " << CalculateIntegerCover<20, 2, 14>() << "%\n";
-	cout << "posit<24,1>: 2^14 integer cover is : " << CalculateIntegerCover<24, 1, 14>() << "%\n";
-	cout << "posit<28,1>: 2^14 integer cover is : " << CalculateIntegerCover<28, 1, 14>() << "%\n";
+	cout << "posit sample coverage of 14-bit unsigned integer\n";
+	cout << "posit<20,2>: 2^14 integer cover is : " << CalculateIntegerCover<20, 2, 14>() << "\n";
+	cout << "posit<24,1>: 2^14 integer cover is : " << CalculateIntegerCover<24, 1, 14>() << "\n";
+	cout << "posit<28,1>: 2^14 integer cover is : " << CalculateIntegerCover<28, 1, 14>() << "\n";
 
-	cout << "16-bit ADC sample coverage\n";
-	cout << "posit<20,1>: 2^16 integer cover is : " << CalculateIntegerCover<20, 1, 16>() << "%\n";
-	cout << "posit<24,1>: 2^16 integer cover is : " << CalculateIntegerCover<24, 1, 16>() << "%\n";
-	cout << "posit<28,1>: 2^16 integer cover is : " << CalculateIntegerCover<28, 1, 16>() << "%\n";
-	cout << "posit<32,1>: 2^16 integer cover is : " << CalculateIntegerCover<32, 1, 16>() << "%\n";
-	cout << "posit<32,2>: 2^16 integer cover is : " << CalculateIntegerCover<32, 2, 16>() << "%\n";
-#else
+	cout << "posit sample coverage of 16-bit unsigned integer\n";
+	cout << "posit<20,1>: 2^16 integer cover is : " << CalculateIntegerCover<20, 1, 16>() << "\n";
+	cout << "posit<24,1>: 2^16 integer cover is : " << CalculateIntegerCover<24, 1, 16>() << "\n";
+	cout << "posit<28,1>: 2^16 integer cover is : " << CalculateIntegerCover<28, 1, 16>() << "\n";
+	cout << "posit<32,1>: 2^16 integer cover is : " << CalculateIntegerCover<32, 1, 16>() << "\n";
+	cout << "posit<32,2>: 2^16 integer cover is : " << CalculateIntegerCover<32, 2, 16>() << "\n";
 
-#endif // MANUAL_TEST
+	cout << "posit sample coverage of 20-bit unsigned integer\n";
+	cout << "posit<20,1>: 2^20 integer cover is : " << CalculateIntegerCover<20, 1, 20>() << "\n";
+	cout << "posit<26,1>: 2^20 integer cover is : " << CalculateIntegerCover<26, 1, 20>() << "\n";
+	cout << "posit<32,2>: 2^20 integer cover is : " << CalculateIntegerCover<32, 2, 20>() << "\n";
+
+	cout << "posit sample coverage of 24-bit unsigned integer\n";
+	cout << "posit<26,1>: 2^24 integer cover is : " << CalculateIntegerCover<26, 1, 24>() << "\n";
+	cout << "posit<32,2>: 2^24 integer cover is : " << CalculateIntegerCover<32, 2, 24>() << "\n";
+	cout << "posit<34,2>: 2^24 integer cover is : " << CalculateIntegerCover<34, 2, 24>() << "\n";
+
+	/*
+	cout << "posit sample coverage of 32-bit unsigned integer\n";
+	cout << "posit<32,2>: 2^32 integer cover is : " << CalculateIntegerCover<32, 2, 32>() << "\n";
+	cout << "posit<40,2>: 2^32 integer cover is : " << CalculateIntegerCover<40, 2, 32>() << "\n";
+	*/
 
 	// restore the previous ostream precision
 	cout << setprecision(precision);
 
-	return nrOfFailedTestCases > 0 ? EXIT_FAILURE : EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
 catch (char const* msg) {
 	std::cerr << msg << std::endl;
