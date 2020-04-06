d16eabf78822815069d8b5f467125eb4845c804f
Ravenwater <theo@stillwater-sc.com>
2020-04-06 09:13:05 -0400

    perfect rounding of pi for posits

---
+++ examples/trigonometry/arbitrary_precision_pi.cpp
@@ -0,0 +1,95 @@
+// arbitrary_precision_pi.cpp: generating a 'perfect' approximation of pi for a given number system
+//
+// Copyright (C) 2017-2020 Stillwater Supercomputing, Inc.
+//
+// This file is part of the universal numbers project, which is released under an MIT Open Source license.
+
+// Configure the posit library with arithmetic exceptions
+// enable posit arithmetic exceptions
+#define POSIT_THROW_ARITHMETIC_EXCEPTION 1
+#include <universal/posit/posit>
+
+/*
+
+Mathematical 	C++ Symbol	Decimal Representation
+Expression
+pi				M_PI		3.14159265358979323846
+pi/2			M_PI_2		1.57079632679489661923
+pi/4			M_PI_4		0.785398163397448309616
+1/pi			M_1_PI		0.318309886183790671538
+2/pi			M_2_PI		0.636619772367581343076
+2/sqrt(pi)		M_2_SQRTPI	1.12837916709551257390
+sqrt(2)			M_SQRT2		1.41421356237309504880
+1/sqrt(2)		M_SQRT1_2	0.707106781186547524401
+e				M_E			2.71828182845904523536
+log_2(e)		M_LOG2E		1.44269504088896340736
+log_10(e)		M_LOG10E	0.434294481903251827651
+log_e(2)		M_LN2		0.693147180559945309417
+log_e(10)		M_LN10		2.30258509299404568402
+
+*/
+
+constexpr double pi = 3.14159265358979323846;  // best practice for C++
+
+// first 1000 digits of pi
+static std::string pi1000 = "3.\
+14159265358979323846264338327950288419716939937510\
+58209749445923078164062862089986280348253421170679\
+82148086513282306647093844609550582231725359408128\
+48111745028410270193852110555964462294895493038196\
+44288109756659334461284756482337867831652712019091\
+45648566923460348610454326648213393607260249141273\
+72458700660631558817488152092096282925409171536436\
+78925903600113305305488204665213841469519415116094\
+33057270365759591953092186117381932611793105118548\
+07446237996274956735188575272489122793818301194912\
+98336733624406566430860213949463952247371907021798\
+60943702770539217176293176752384674818467669405132\
+00056812714526356082778577134275778960917363717872\
+14684409012249534301465495853710507922796892589235\
+42019956112129021960864034418159813629774771309960\
+51870721134999999837297804995105973173281609631859\
+50244594553469083026425223082533446850352619311881\
+71010003137838752886587533208381420617177669147303\
+59825349042875546873115956286388235378759375195778\
+18577805321712268066130019278766111959092164201989";
+
+int main(int argc, char** argv)
+try {
+	using namespace std;
+	using namespace sw::unum;
+
+	int nrOfFailedTestCases = 0;
+
+	cout << "Perfect approximations of PI for different number systems" << endl;
+
+	cout << pi1000 << endl;
+
+	// 1000 digits -> 1.e1000 -> 2^3322 -> 1.051103774764883380737596422798e+1000 -> you will need 3322 bits to represent 1000 digits of pi
+
+	return (nrOfFailedTestCases > 0 ? EXIT_FAILURE : EXIT_SUCCESS);
+}
+catch (char const* msg) {
+	std::cerr << msg << std::endl;
+	return EXIT_FAILURE;
+}
+catch (const posit_arithmetic_exception& err) {
+	std::cerr << "Uncaught posit arithmetic exception: " << err.what() << std::endl;
+	return EXIT_FAILURE;
+}
+catch (const quire_exception& err) {
+	std::cerr << "Uncaught quire exception: " << err.what() << std::endl;
+	return EXIT_FAILURE;
+}
+catch (const posit_internal_exception& err) {
+	std::cerr << "Uncaught posit internal exception: " << err.what() << std::endl;
+	return EXIT_FAILURE;
+}
+catch (const std::runtime_error& err) {
+	std::cerr << "Uncaught runtime exception: " << err.what() << std::endl;
+	return EXIT_FAILURE;
+}
+catch (...) {
+	std::cerr << "Caught unknown exception" << std::endl;
+	return EXIT_FAILURE;
+}
