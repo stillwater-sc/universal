7bcce1594fef9c2d02f6543951a78c9fdb922094
Ravenwater <theo@stillwater-sc.com>
2020-04-26 12:08:59 -0400

    adding trouble code to segment for documentation

--- tests/fixpnt/complex_mod_addition.cpp
+++ tests/fixpnt/complex_mod_addition.cpp
@@ -100,6 +100,23 @@ try {
 	// than other compilers.
 			// no idea how to fix the code below to make it compile with g++
 /*
+		using namespace sw::unum::complex_literals;
+		using Real = sw::unum::fixpnt<8, 4>;
+		std::cout << std::fixed << std::setprecision(1);
+
+		std::complex<Real> z1 = 1i * 1i;     // imaginary unit squared
+		std::cout << "i * i = " << z1 << '\n';
+
+		std::complex<double> z2 = std::pow(1.0i, 2.0); // imaginary unit squared
+		std::cout << "pow(i, 2) = " << z2 << '\n';
+
+		double PI = std::acos(-1);
+		std::complex<Real> z3 = std::exp(1.0i * PI); // Euler's formula
+		std::cout << "exp(i * pi) = " << z3 << '\n';
+
+		std::complex<Real> z4 = 1.0 + 2i, z5 = 1.0 - 2i; // conjugates
+		std::cout << "(1+2i)*(1-2i) = " << z4 * z5 << '\n';
+
 	error: conversion from '__complex__ int' to non - scalar type 'std::complex<sw::unum::fixpnt<8, 4> >' requested
 		std::complex<Real> z1 = 1i * 1i;     // imaginary unit squared
 		                        ~~~^~~~
