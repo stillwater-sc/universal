94e01cacde04a86cd1be129f5a8d9eaa4d8efddd
Ravenwater <theo@stillwater-sc.com>
2020-06-13 09:01:36 -0400

    Adding logarithmic number system skeleton

---
+++ tests/lns/README.md
@@ -0,0 +1,5 @@
+# LNS: Logarithmic Number System
+
+A number X, is represented by the logarithm, x, of its absolute value:
+
+X -> {s, x = log2(|X|)
---
+++ tests/lns/arithmetic_add.cpp
@@ -0,0 +1,84 @@
+// arithmetic_add.cpp: functional tests for addition on arbitrary logarithmic number system
+//
+// Copyright (C) 2017-2020 Stillwater Supercomputing, Inc.
+//
+// This file is part of the universal numbers project, which is released under an MIT Open Source license.
+
+// minimum set of include files to reflect source code dependencies
+#include <universal/lns/lns.hpp>
+// test helpers, such as, ReportTestResults
+#include "../utils/test_helpers.hpp"
+
+// generate specific test case that you can trace with the trace conditions in areal.hpp
+// for most bugs they are traceable with _trace_conversion and _trace_add
+template<size_t nbits, typename Ty>
+void GenerateTestCase(Ty a, Ty b) {
+	Ty ref;
+	sw::unum::lns<nbits> pa, pb, pref, psum;
+	pa = a;
+	pb = b;
+	ref = a + b;
+	pref = ref;
+	psum = pa + pb;
+	std::cout << std::setprecision(nbits - 2);
+	std::cout << std::setw(nbits) << a << " + " << std::setw(nbits) << b << " = " << std::setw(nbits) << ref << std::endl;
+	std::cout << pa.get() << " + " << pb.get() << " = " << psum.get() << " (reference: " << pref.get() << ")   " ;
+	std::cout << (pref == psum ? "PASS" : "FAIL") << std::endl << std::endl;
+	std::cout << std::setprecision(5);
+}
+
+template<size_t nbits> 
+int ValidateAddition(const std::string& tag, bool bReportIndividualTestCases) {
+	int nrOfFailedTestCases = 0;
+
+	return nrOfFailedTestCases;
+}
+
+#define MANUAL_TESTING 1
+#define STRESS_TESTING 0
+
+int main(int argc, char** argv)
+try {
+	using namespace std;
+	using namespace sw::unum;
+
+	int nrOfFailedTestCases = 0;
+
+#if MANUAL_TESTING
+
+	// generate individual testcases to hand trace/debug
+	GenerateTestCase<16, double>(INFINITY, INFINITY);
+	GenerateTestCase<8, float>(0.5f, -0.5f);
+
+	// manual exhaustive test
+	nrOfFailedTestCases += ReportTestResult(ValidateAddition<8>("Manual Testing", true), "lns<8>", "addition");
+
+	nrOfFailedTestCases = 0;
+#else
+	cout << "Arbitrary LNS addition validation" << endl;
+
+	bool bReportIndividualTestCases = false;
+	std::string tag = "Addition failed: ";
+
+	nrOfFailedTestCases += ReportTestResult(ValidateAddition<8>(tag, bReportIndividualTestCases), "lns<8>", "addition");
+
+#if STRESS_TESTING
+
+#endif  // STRESS_TESTING
+
+#endif  // MANUAL_TESTING
+
+	return (nrOfFailedTestCases > 0 ? EXIT_FAILURE : EXIT_SUCCESS);
+}
+catch (char const* msg) {
+	std::cerr << msg << std::endl;
+	return EXIT_FAILURE;
+}
+catch (const std::runtime_error& err) {
+	std::cerr << "Uncaught runtime exception: " << err.what() << std::endl;
+	return EXIT_FAILURE;
+}
+catch (...) {
+	std::cerr << "Caught unknown exception" << std::endl;
+	return EXIT_FAILURE;
+}
