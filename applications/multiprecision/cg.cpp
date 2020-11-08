90e7cb263fe65a1cdea84ddbf53ff635d1c334c5
Ravenwater <theo@stillwater-sc.com>
2020-11-08 16:36:57 -0500

    adding CG precision experiment on convergence

--- applications/multiprecision/cg.cpp
+++ applications/multiprecision/cg.cpp
@@ -16,9 +16,20 @@
 // Configure the posit library with arithmetic exceptions
 // enable posit arithmetic exceptions
 #define POSIT_THROW_ARITHMETIC_EXCEPTION 1
+// and fast posits
+//#define POSIT_FAST_SPECIALIZATION 1
+#define POSIT_FAST_POSIT_32_2 1
 #include <universal/posit/posit>
 #include <universal/blas/blas.hpp>
 #include <universal/blas/generators.hpp>
+
+template<typename Scalar>
+std::ostream& operator<<(std::ostream& ostr, const std::vector<Scalar>& vec) {
+	for (auto v : vec) {
+		ostr << ' ' << v;
+	}
+	return ostr;
+}
 
 // cg: Solution of x in Ax=b using preconditioned Conjugate Gradient algorithm 
 // with different precision for matrix-vector multiply and residual calculation
@@ -33,6 +44,7 @@ template<typename Matrix, typename Vector, size_t MAX_ITERATIONS = 10>
 size_t cg(const Matrix& M, const Matrix& A, Vector& b, typename Matrix::value_type tolerance = typename Matrix::value_type(0.00001)) {
 	using Scalar = typename Matrix::value_type;
 	Scalar residual = Scalar(std::numeric_limits<Scalar>::max());
+	std::vector<Scalar> residuals; // store the residual trajectory
 	//size_t m = num_rows(A);
 	//size_t n = num_cols(A);
 	Vector x(size(b));
@@ -63,15 +75,44 @@ size_t cg(const Matrix& M, const Matrix& A, Vector& b, typename Matrix::value_ty
 		rho = rho - alpha * q;
 		// check for convergence of the system
 		residual = norm1(x_1 - x);
-		std::cout << '[' << itr << "] " << std::setw(12) << x << " residual " << residual << std::endl;
+//		std::cout << '[' << itr << "] " << std::setw(12) << x << " residual " << residual << std::endl;
+		residuals.push_back(residual);
 		++itr;
 	}
-	std::cout << "solution in " << itr << " iterations\n";
-	std::cout << "solution is " << x << '\n';
-	std::cout << "final residual is " << residual << '\n';
-	std::cout << "validation\n" << A * x << " = " << b << std::endl;
+	if (residual < tolerance) {
+		std::cout << "solution in " << itr << " iterations\n";
+	}
+	else {
+		std::cout << "failed to converge in " << itr << " iterations\n";
+	}
+//	std::cout << "solution is " << x << '\n';
+//	std::cout << "final residual is " << residual << '\n';
+//	std::cout << "validation\n" << A * x << " = " << b << '\n';
+	std::cout << "residuals " << residuals << std::endl;
 	return itr;
 }
+
+template<typename Scalar, size_t MAX_ITERATIONS>
+size_t Experiment(size_t DoF) {
+	using Matrix = sw::unum::blas::matrix<Scalar>;
+	using Vector = sw::unum::blas::vector<Scalar>;
+	// Initialize 'A', preconditioner 'M', 'b' & intial guess 'x' * _
+
+	Matrix A;
+	tridiag(A, DoF);  // this does a resize of A
+	Matrix M = sw::unum::blas::eye<Scalar>(DoF);
+	Vector b(DoF);
+	Vector x(DoF);
+	x = Scalar(1);
+	b = A * x;
+
+	M = sw::unum::blas::inv(A);
+	return cg<Matrix, Vector, MAX_ITERATIONS>(M, A, b);
+	
+}
+
+#define MANUAL 1
+#define STRESS 0
 
 int main(int argc, char** argv)
 try {
@@ -79,6 +120,10 @@ try {
 	using namespace sw::unum;
 	using namespace sw::unum::blas;
 
+	if (argc == 1) cout << argv[0] << '\n';
+	int nrOfFailedTestCases = 0;
+
+#if MANUAL
 	constexpr size_t nbits = 32;
 	constexpr size_t es = 2;
 	using Scalar = posit<nbits, es>;
@@ -86,8 +131,6 @@ try {
 	using Matrix = sw::unum::blas::matrix<Scalar>;
 	using Vector = sw::unum::blas::vector<Scalar>;
 
-	if (argc == 1) cout << argv[0] << '\n';
-	int nrOfFailedTestCases = 0;
 
 	// Initialize 'A', preconditioner 'M', 'b' & intial guess 'x' * _
 	constexpr size_t DoF = 8;
@@ -99,10 +142,19 @@ try {
 	x = Scalar(1);
 	b = A * x;
 
-	cout << A << endl;
-	cout << M << endl;
-	cout << b << endl;
-	constexpr size_t MAX_ITERATIONS = 10;
+	if (DoF < 10) {
+		cout << A << endl;
+		cout << M << endl;
+		cout << b << endl;
+	}
+	/*
+	* for second order elliptical PDEs, the resulting coefficient matrix exhibits
+	* a condition number k_2(A) = O(h^-2). Convergence rate of CG is sqrt(k_2)
+	* so convergence is expected to be O(h^-1). 
+	* The selected tridiagonal matrix has a discretization step given by DoF
+	* and thus we expect the converge in sqrt(128) (h = 1/DoF -> h^-1 is Dof)
+	*/
+	constexpr size_t MAX_ITERATIONS = 100;
 	size_t itr = cg<Matrix, Vector, MAX_ITERATIONS>(M, A, b);
 
 	if (itr == MAX_ITERATIONS) {
@@ -110,13 +162,19 @@ try {
 		++nrOfFailedTestCases;
 	}
 
-	M = sw::unum::blas::inv(A);
-	itr = cg<Matrix, Vector, MAX_ITERATIONS>(M, A, b);
+#else
+	// with a preconditioner M = A^-1
+	Experiment<float,200>(64);
+	Experiment<double, 100>(64);
+#if STRESS
+	Experiment<posit<32,2>, 200>(64);
+	Experiment<posit<64, 3>, 100>(64);
+	Experiment<posit<128, 4>, 100>(64);
+	Experiment<posit<256, 5>, 100>(64);
+#endif // STRESS
 
-	if (itr == MAX_ITERATIONS) {
-		std::cerr << "Solution failed to converge\n";
-		++nrOfFailedTestCases;
-	}
+#endif // MANUAL
+
 	return (nrOfFailedTestCases > 0 ? EXIT_FAILURE : EXIT_SUCCESS);
 }
 catch (char const* msg) {
