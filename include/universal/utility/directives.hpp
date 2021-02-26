4df18d69bc26268732696e04ae00c1f14d8a5fa8
Ravenwater <theo@stillwater-sc.com>
2021-02-26 06:28:28 -0500

    WIP: adding skeleton test suite runners for subnormal conversions

--- include/universal/utility/directives.hpp
+++ include/universal/utility/directives.hpp
@@ -1,3 +1,5 @@
+#pragma once
+
 // compiler_directives.hpp: 
 #if defined(_MSC_VER)
 #pragma warning(disable : 4514)  // unreferenced function is removed
@@ -8,3 +10,21 @@
 #pragma warning(disable : 5045)  // Compiler will insert Spectre mitigation for memory load if /Qspectre switch specified
 
 #endif
+
+#include <iostream>
+namespace sw::universal {
+
+	/// <summary>
+	/// print the cmd line if there is one or more parameters provided
+	/// </summary>
+	/// <param name="argc">number of arguments</param>
+	/// <param name="argv">array of char* representing the arguments</param>
+	void print_cmd_line(int argc, char* argv[]) {
+		if (argc > 1) {
+			for (int i = 0; i < argc; ++i) {
+				std::cout << argv[i] << ' ';
+			}
+			std::cout << std::endl;
+		}
+	}
+}
