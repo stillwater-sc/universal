7d12033c9f7452cbd0bbe4c84aaa70c6a14c942c
Ravenwater <theo@stillwater-sc.com>
2021-02-27 16:13:46 -0500

    expanding the decode performance tests for bfloats

--- performance/bfloat/performance.cpp
+++ performance/bfloat/performance.cpp
@@ -59,25 +59,105 @@ void DecodeWorkload(uint64_t NR_OPS) {
 	if (success == 0) cout << "DECODE FAIL\n"; // just a quick double check that all went well
 }
 
-// measure performance of conversion operators
-void TestDecodePerformance() {
-	using namespace std;
-	using namespace sw::universal;
-	cout << endl << "BFLOAT decode operator performance" << endl;
-
-	uint64_t NR_OPS = 10000000;
-	PerformanceRunner("bfloat<8,2,uint8_t>      decode         ", DecodeWorkload< sw::universal::bfloat<8, 2, uint8_t> >, NR_OPS);
-	PerformanceRunner("bfloat<16,5,uint16_t>    decode         ", DecodeWorkload< sw::universal::bfloat<16, 5, uint16_t> >, NR_OPS);
-	PerformanceRunner("bfloat<32,8,uint32_t>    decode         ", DecodeWorkload< sw::universal::bfloat<32, 8, uint32_t> >, NR_OPS);
-	PerformanceRunner("bfloat<64,11,uint64_t>   decode         ", DecodeWorkload< sw::universal::bfloat<64, 11, uint64_t> >, NR_OPS);
-/* 
+/*
 2/26/2021
 BFLOAT decode operator performance                                                           <---- this includes set_raw_bits()
 bfloat<8,2,uint8_t>      decode            10000000 per       0.0105318sec -> 949 Mops/sec
 bfloat<16,5,uint16_t>    decode            10000000 per        0.017448sec -> 573 Mops/sec
 bfloat<32,8,uint32_t>    decode            10000000 per       0.0158896sec -> 629 Mops/sec
 bfloat<64,11,uint64_t>   decode            10000000 per       0.0149587sec -> 668 Mops/sec
+
+2/27/2021
+BFLOAT decode operator performance
+single block representations
+bfloat<8,2,uint8_t>      decode              100000 per        9.81e-05sec ->   1 Gops/sec
+bfloat<16,5,uint16_t>    decode              100000 per       0.0001751sec -> 571 Mops/sec
+bfloat<32,8,uint32_t>    decode              100000 per       0.0001525sec -> 655 Mops/sec
+bfloat<64,11,uint64_t>   decode              100000 per       0.0001251sec -> 799 Mops/sec
+byte representations
+bfloat<8,2,uint8_t>      decode              100000 per        9.84e-05sec ->   1 Gops/sec
+bfloat<16,5,uint8_t>     decode              100000 per       0.0017394sec ->  57 Mops/sec
+bfloat<32,8,uint8_t>     decode              100000 per       0.0054993sec ->  18 Mops/sec
+bfloat<64,11,uint8_t>    decode              100000 per       0.0114794sec ->   8 Mops/sec
+bfloat<128,11,uint8_t>   decode              100000 per       0.0246191sec ->   4 Mops/sec
+2-byte representations
+bfloat<8,2,uint16_t>     decode              100000 per       0.0001714sec -> 583 Mops/sec
+bfloat<16,5,uint16_t>    decode              100000 per       0.0001713sec -> 583 Mops/sec
+bfloat<32,8,uint16_t>    decode              100000 per        0.004117sec ->  24 Mops/sec
+bfloat<64,11,uint16_t>   decode              100000 per       0.0091907sec ->  10 Mops/sec
+bfloat<128,11,uint16_t>  decode              100000 per       0.0209605sec ->   4 Mops/sec
+4-byte representations
+bfloat<8,2,uint32_t>     decode              100000 per       0.0001122sec -> 891 Mops/sec
+bfloat<16,5,uint32_t>    decode              100000 per       0.0005336sec -> 187 Mops/sec
+bfloat<32,8,uint32_t>    decode              100000 per        0.000147sec -> 680 Mops/sec
+bfloat<64,11,uint32_t>   decode              100000 per        0.009177sec ->  10 Mops/sec
+bfloat<128,11,uint32_t>  decode              100000 per       0.0209432sec ->   4 Mops/sec
+8-byte representations
+bfloat<8,2,uint64_t>     decode              100000 per       0.0001053sec -> 949 Mops/sec
+bfloat<16,5,uint64_t>    decode              100000 per       0.0001713sec -> 583 Mops/sec
+bfloat<32,8,uint64_t>    decode              100000 per       0.0001472sec -> 679 Mops/sec
+bfloat<64,11,uint64_t>   decode              100000 per       0.0001225sec -> 816 Mops/sec
+bfloat<128,11,uint64_t>  decode              100000 per       0.0210058sec ->   4 Mops/sec
+very large representations
+bfloat<80,11,uint64_t>   decode              100000 per       0.0121534sec ->   8 Mops/sec
+bfloat<96,11,uint64_t>   decode              100000 per       0.0156355sec ->   6 Mops/sec
+bfloat<128,11,uint64_t>  decode              100000 per       0.0210453sec ->   4 Mops/sec
+bfloat<256,11,uint64_t>  decode              100000 per       0.0433087sec ->   2 Mops/sec
+bfloat<256,11,uint64_t>  decode              100000 per       0.0447077sec ->   2 Mops/sec
 */
+
+/// <summary>
+/// measure performance of decode operator
+/// NOTE: es is <= 11 due to limits of dynamic range of a 64-bit double
+/// </summary>
+void TestDecodePerformance() {
+	using namespace std;
+	using namespace sw::universal;
+	cout << endl << "BFLOAT decode operator performance" << endl;
+
+	uint64_t NR_OPS = 100000;
+	// single block representations
+	cout << "single block representations\n";
+	PerformanceRunner("bfloat<8,2,uint8_t>      decode         ", DecodeWorkload< sw::universal::bfloat<8, 2, uint8_t> >, NR_OPS);
+	PerformanceRunner("bfloat<16,5,uint16_t>    decode         ", DecodeWorkload< sw::universal::bfloat<16, 5, uint16_t> >, NR_OPS);
+	PerformanceRunner("bfloat<32,8,uint32_t>    decode         ", DecodeWorkload< sw::universal::bfloat<32, 8, uint32_t> >, NR_OPS);
+	PerformanceRunner("bfloat<64,11,uint64_t>   decode         ", DecodeWorkload< sw::universal::bfloat<64, 11, uint64_t> >, NR_OPS);
+
+	// multi-block representations
+	cout << "byte representations\n";
+	PerformanceRunner("bfloat<8,2,uint8_t>      decode         ", DecodeWorkload< sw::universal::bfloat<8, 2, uint8_t> >, NR_OPS);
+	PerformanceRunner("bfloat<16,5,uint8_t>     decode         ", DecodeWorkload< sw::universal::bfloat<16, 5, uint8_t> >, NR_OPS);
+	PerformanceRunner("bfloat<32,8,uint8_t>     decode         ", DecodeWorkload< sw::universal::bfloat<32, 8, uint8_t> >, NR_OPS);
+	PerformanceRunner("bfloat<64,11,uint8_t>    decode         ", DecodeWorkload< sw::universal::bfloat<64, 11, uint8_t> >, NR_OPS);
+	PerformanceRunner("bfloat<128,11,uint8_t>   decode         ", DecodeWorkload< sw::universal::bfloat<128, 11, uint8_t> >, NR_OPS);
+
+	cout << "2-byte representations\n";
+	PerformanceRunner("bfloat<8,2,uint16_t>     decode         ", DecodeWorkload< sw::universal::bfloat<8, 2, uint16_t> >, NR_OPS);
+	PerformanceRunner("bfloat<16,5,uint16_t>    decode         ", DecodeWorkload< sw::universal::bfloat<16, 5, uint16_t> >, NR_OPS);
+	PerformanceRunner("bfloat<32,8,uint16_t>    decode         ", DecodeWorkload< sw::universal::bfloat<32, 8, uint16_t> >, NR_OPS);
+	PerformanceRunner("bfloat<64,11,uint16_t>   decode         ", DecodeWorkload< sw::universal::bfloat<64, 11, uint16_t> >, NR_OPS);
+	PerformanceRunner("bfloat<128,11,uint16_t>  decode         ", DecodeWorkload< sw::universal::bfloat<128, 11, uint16_t> >, NR_OPS);
+
+	cout << "4-byte representations\n";
+	PerformanceRunner("bfloat<8,2,uint32_t>     decode         ", DecodeWorkload< sw::universal::bfloat<8, 2, uint32_t> >, NR_OPS);
+	PerformanceRunner("bfloat<16,5,uint32_t>    decode         ", DecodeWorkload< sw::universal::bfloat<16, 5, uint32_t> >, NR_OPS);
+	PerformanceRunner("bfloat<32,8,uint32_t>    decode         ", DecodeWorkload< sw::universal::bfloat<32, 8, uint32_t> >, NR_OPS);
+	PerformanceRunner("bfloat<64,11,uint32_t>   decode         ", DecodeWorkload< sw::universal::bfloat<64, 11, uint32_t> >, NR_OPS);
+	PerformanceRunner("bfloat<128,11,uint32_t>  decode         ", DecodeWorkload< sw::universal::bfloat<128, 11, uint32_t> >, NR_OPS);
+
+	cout << "8-byte representations\n";
+	PerformanceRunner("bfloat<8,2,uint64_t>     decode         ", DecodeWorkload< sw::universal::bfloat<8, 2, uint8_t> >, NR_OPS);
+	PerformanceRunner("bfloat<16,5,uint64_t>    decode         ", DecodeWorkload< sw::universal::bfloat<16, 5, uint16_t> >, NR_OPS);
+	PerformanceRunner("bfloat<32,8,uint64_t>    decode         ", DecodeWorkload< sw::universal::bfloat<32, 8, uint32_t> >, NR_OPS);
+	PerformanceRunner("bfloat<64,11,uint64_t>   decode         ", DecodeWorkload< sw::universal::bfloat<64, 11, uint64_t> >, NR_OPS);
+	PerformanceRunner("bfloat<128,11,uint64_t>  decode         ", DecodeWorkload< sw::universal::bfloat<128, 11, uint64_t> >, NR_OPS);
+
+	cout << "very large representations\n";
+	PerformanceRunner("bfloat<80,11,uint64_t>   decode         ", DecodeWorkload< sw::universal::bfloat<80, 11, uint64_t> >, NR_OPS);
+	PerformanceRunner("bfloat<96,11,uint64_t>   decode         ", DecodeWorkload< sw::universal::bfloat<96, 11, uint64_t> >, NR_OPS);
+	PerformanceRunner("bfloat<128,11,uint64_t>  decode         ", DecodeWorkload< sw::universal::bfloat<128, 11, uint64_t> >, NR_OPS); 
+	PerformanceRunner("bfloat<256,11,uint64_t>  decode         ", DecodeWorkload< sw::universal::bfloat<256, 11, uint64_t> >, NR_OPS);
+	PerformanceRunner("bfloat<256,11,uint64_t>  decode         ", DecodeWorkload< sw::universal::bfloat<256, 11, uint64_t> >, NR_OPS);
 }
 
 // measure performance of arithmetic operators
@@ -92,6 +172,7 @@ void TestArithmeticOperatorPerformance() {
 	PerformanceRunner("bfloat<16,5,uint16_t>    add/subtract   ", AdditionSubtractionWorkload< sw::universal::bfloat<16,5,uint16_t> >, NR_OPS);
 	PerformanceRunner("bfloat<32,8,uint32_t>    add/subtract   ", AdditionSubtractionWorkload< sw::universal::bfloat<32,8,uint32_t> >, NR_OPS);
 	PerformanceRunner("bfloat<64,11,uint64_t>   add/subtract   ", AdditionSubtractionWorkload< sw::universal::bfloat<64,11,uint64_t> >, NR_OPS);
+	PerformanceRunner("bfloat<128,11,uint64_t>  add/subtract   ", AdditionSubtractionWorkload< sw::universal::bfloat<128,11,uint64_t> >, NR_OPS / 2);
 //	PerformanceRunner("bfloat<128,15,uint64_t>  add/subtract   ", AdditionSubtractionWorkload< sw::universal::bfloat<128,15,uint64_t> >, NR_OPS / 2);
 //	PerformanceRunner("bfloat<256,15,uint64_t   add/subtract   ", AdditionSubtractionWorkload< sw::universal::bfloat<256,15,uint64_t> >, NR_OPS / 4);
 //	PerformanceRunner("bfloat<512,15,uint64_t>  add/subtract   ", AdditionSubtractionWorkload< sw::universal::bfloat<512,15,uint64_t> >, NR_OPS / 8);
