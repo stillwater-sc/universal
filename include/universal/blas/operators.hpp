762015470428cc8407504648c4c9e8020cae9869
rainmaker6 <masteroftheirclassic@gmail.com>
2021-03-27 12:19:13 +0530

    Updates

--- include/universal/blas/operators.hpp
+++ include/universal/blas/operators.hpp
@@ -50,7 +50,35 @@ matrix<Scalar> diag(const vector<Scalar>& d) {
 	}
 	return A;
 }
-
+//compute minor in-place
+template<typename Scalar>
+matrix<Scalar> minor(const matrix<Scalar>& A, size_t x=1){
+    using size_type = typename matrix<Scalar>::size_type;
+    matrix<Scalar> tmp(A);
+    for (size_t i = 0; i < x; i++){ 
+        tmp[i,i] = size_type(1.0);
+    }
+    for (size_t i = x; i < num_rows(A); i++){
+        for (size_t j = x; j < num_cols(A); j++){
+            tmp[i][j] = A[i][j];
+        }
+    }
+    return tmp;
+}
+//get xth column
+template<typename Scalar>
+void get_col(matrix<Scalar>& A, vector<Scalar>& v, size_t x){
+    using size_type = typename matrix<Scalar>::size_type;
+    for (size_t i = 0; i < num_rows(A); i++)
+    v[i] = A[i][x];
+}
+template<typename Scalar>
+Scalar norm(vector<Scalar>& x){
+    using size_type = typename matrix<Scalar>::size_type;
+    Scalar s;
+    for (size_t i = 0; i < x.size(); i++) s+=i*i;
+    return size_type(std::sqrt(sum));
+}
 // return lower triangular matrix of A
 template<typename Scalar>
 matrix<Scalar> tril(const matrix<Scalar>& A, size_t k = 0) {
