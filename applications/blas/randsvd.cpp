2363e80bce8729221c58f455feb13e0f1c60595c
Ravenwater <theo@stillwater-sc.com>
2021-04-09 16:29:23 -0400

    removing warnings from blas applications

--- applications/blas/randsvd.cpp
+++ applications/blas/randsvd.cpp
@@ -7,7 +7,7 @@
 #pragma warning(disable : 4100) // argc/argv unreferenced formal parameter
 #pragma warning(disable : 4514 4571)
 #pragma warning(disable : 4625 4626) // 4625: copy constructor was implicitly defined as deleted, 4626: assignment operator was implicitely defined as deleted
-#pragma warning(disable : 5025 5026 5027)
+#pragma warning(disable : 5025 5026 5027 5045)
 #pragma warning(disable : 4710 4774)
 #pragma warning(disable : 4820)
 #endif
@@ -23,7 +23,6 @@
 #define POSIT_THROW_ARITHMETIC_EXCEPTION 1
 #include <universal/number/posit/posit>
 #define BLAS_TRACE_ROUNDING_EVENTS 1
-#include <universal/blas/blas.hpp>
 #include <universal/blas/generators.hpp>
 
 template<typename Scalar>
@@ -46,7 +45,7 @@ try {
 
 	if (argc == 1) cout << argv[0] << endl;
 
-	RandsvdMatrixTest< float >();
+//	RandsvdMatrixTest< float >();
 	RandsvdMatrixTest< posit<32, 2> >();
 
 	return EXIT_SUCCESS;
