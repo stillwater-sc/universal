b17d1e6b834169c0920f77861f7d31d5a6fec800
Ravenwater <theo@stillwater-sc.com>
2021-04-11 13:35:41 -0400

    fixing uninitialized warning in rpoly

--- benchmark/energy/blas/gemm.cpp
+++ benchmark/energy/blas/gemm.cpp
@@ -31,6 +31,7 @@ std::string conditional_fdp(const sw::universal::blas::vector< sw::universal::po
 }
 
 #if DECIMAL_OPERATIONS_COUNT
+//
 // create the static storage for the occurrence measurements of the decimal number system
 bool sw::universal::decimal::enableAdd = true;
 sw::universal::occurrence<sw::universal::decimal> sw::universal::decimal::ops;
@@ -47,11 +48,11 @@ try {
 
 	constexpr size_t N = 5;
 
-	auto A = eye<Scalar>(N);
-	auto B = frank<Scalar>(N);
+	Matrix A = eye<Scalar>(N);
+	Matrix B = frank<Scalar>(N);
 	decimal proxy;
 	proxy.resetStats();
-	auto C = A * B;
+	Matrix C = A * B;
 	cout << C << endl;
 	proxy.printStats(cout);
 
