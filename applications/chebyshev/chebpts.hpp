cc3d1c6fc35fb580d97fb01b3637d3ddb5811b75
Ravenwater <theo@stillwater-sc.com>
2021-04-30 10:58:32 -0400

    compilation fix for g++: removing name collision of PI constant

--- applications/chebyshev/chebpts.hpp
+++ applications/chebyshev/chebpts.hpp
@@ -18,23 +18,23 @@ namespace sw::universal {
 			std::cerr << "Parameter must be a positive integer. Provided n == " << n << '\n';
 			return blas::vector<Scalar>(1);
 		}
-		constexpr double M_PI = 3.14159265358979323846;
+		constexpr double _PI = 3.14159265358979323846;
 		blas::vector<Scalar>x(n);
 		int m = n - 1; 
 		switch (kind) {
 			case 1: // Chebyshev 1st Kind 
 				for(int k = n; k > 0; --k){
-					x(n-k) = sin(M_PI*(n - 2*k + 1 )/(2*n)); 
+					x(n-k) = sin(_PI*(n - 2*k + 1 )/(2*n)); 
 				}
 				break;
 			case 2: // Chebyshev 2nd Kind (default)
 				for(int k = m; k >= 0; --k){
-					x(m-k) = sin(M_PI*(m - 2*k)/(2*m)); 
+					x(m-k) = sin(_PI*(m - 2*k)/(2*m)); 
 				}
 				break;
 			default: // Chebyshev 2nd Kind
 				for(int k = m; k >= 0; --k){
-					x(m-k) = sin(M_PI*(m - 2*k)/(2*m)); 
+					x(m-k) = sin(_PI*(m - 2*k)/(2*m)); 
 				}
 				break;
 		}
