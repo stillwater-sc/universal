36e47349bb15e23f1a109550e887b4256f34b3d3
Ravenwater <theo@stillwater-sc.com>
2021-10-15 17:26:35 -0400

    fixing smallest subnormal value of 80-bit extended precision format

--- include/universal/native/ieee754_clang.hpp
+++ include/universal/native/ieee754_clang.hpp
@@ -28,9 +28,9 @@ public:
 	static constexpr uint64_t fmsb     = 0x0040'0000ull;
 	static constexpr uint64_t qnanmask = 0x7FC0'0000ull;
 	static constexpr uint64_t snanmask = 0x7FA0'0000ull;
-	static constexpr float    minNormal = 1.1754943508222875079687365372222e-38f; // == 2^-126
-	static constexpr float    minSubnormal = 1.4012984643248170709237295832899e-45f; // == 2^-149
-	static constexpr int      minNormalExp = -126;
+	static constexpr float    minNormal       = 1.1754943508222875079687365372222e-38f; // == 2^-126
+	static constexpr float    minSubnormal    = 1.4012984643248170709237295832899e-45f; // == 2^-149
+	static constexpr int      minNormalExp    = -126;
 	static constexpr int      minSubnormalExp = -149;
 };
 
@@ -50,9 +50,9 @@ public:
 	static constexpr uint64_t fmsb     = 0x0008'0000'0000'0000ull;
 	static constexpr uint64_t qnanmask = 0x7FF8'0000'0000'0000ull;
 	static constexpr uint64_t snanmask = 0x7FF4'0000'0000'0000ull;
-	static constexpr double   minNormal = 2.2250738585072013830902327173324e-308; // == 2^-1022
-	static constexpr double   minSubnormal = 4.9406564584124654417656879286220e-324; // == 2^-1074
-	static constexpr int      minNormalExp = -1022;
+	static constexpr double   minNormal       = 2.2250738585072013830902327173324e-308; // == 2^-1022
+	static constexpr double   minSubnormal    = 4.9406564584124654417656879286220e-324; // == 2^-1074
+	static constexpr int      minNormalExp    = -1022;
 	static constexpr int      minSubnormalExp = -1074;
 };
 
@@ -75,10 +75,10 @@ public:
 	static constexpr uint64_t fmsb     = 0x8000'0000'0000'0000ull;
 	static constexpr uint64_t qnanmask = 0x7FF8'0000'0000'0000ull;
 	static constexpr uint64_t snanmask = 0x7FF4'0000'0000'0000ull;
-	static constexpr long double minNormal = 1.6810515715560467531313389086609e-4932; // == 2^-16383
-	static constexpr long double minSubnormal = 3.64519953188247460253e-4951; // not quite 2^16446
-	static constexpr int         minNormalExp = -16383;
-	static constexpr int         minSubnormalExp = -16446;
+	static constexpr long double minNormal       = 1.6810515715560467531313389086609e-4932l; // == 2^-16383
+	static constexpr long double minSubnormal    = 3.6451995318824746025284059336194e-4951l; // == 2^-16445
+	static constexpr int         minNormalExp    = -16381;
+	static constexpr int         minSubnormalExp = -16445;
 };
 
 } // namespace sw::universal
