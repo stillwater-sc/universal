469417872aae70a4f291110d5260ec97c4bcf11f
Ravenwater <theo@stillwater-sc.com>
2021-10-15 17:33:42 -0400

    fixing lns long double support directive

--- tests/lns/arithmetic_mul.cpp
+++ tests/lns/arithmetic_mul.cpp
@@ -5,7 +5,7 @@
 // This file is part of the universal numbers project, which is released under an MIT Open Source license.
 
 // minimum set of include files to reflect source code dependencies
-#include <universal/number/lns/lns_impl.hpp>
+#include <universal/number/lns/lns.hpp>
 #include <universal/verification/test_status.hpp> // ReportTestResult
 #include <universal/verification/test_case.hpp>
 
