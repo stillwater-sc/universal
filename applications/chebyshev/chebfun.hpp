212490fec28b49b4efdffa39bc54bdec1ef96fc4
jamesquinlan <jquinlan@une.edu>
2021-10-17 21:33:59 -0400

    edit chebmat

--- applications/chebyshev/chebfun.hpp
+++ applications/chebyshev/chebfun.hpp
@@ -35,6 +35,10 @@ class chebfun {
             std::cout << "Chebfun constructor test =  " << f(1.5) << std::endl;
         }
 
+        // chebfun(Scalar(*f)(Scalar)) {
+        //     std::cout << "Chebfun constructor test =  " << f(1.5) << std::endl;
+
+
         Scalar operator[](size_type i){return domain[i];}
 
    
