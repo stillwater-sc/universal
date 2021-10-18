41f53d1ec65aaf77c3d0f4f182e84914b2f5d151
jamesquinlan <jquinlan@une.edu>
2021-10-18 07:03:40 -0400

    wip

--- applications/chebyshev/chebtests.cpp
+++ applications/chebyshev/chebtests.cpp
@@ -125,12 +125,16 @@ try {
 	//std::cout << chebpoly<Scalar>(3) + rpad<Scalar>(chebpoly<Scalar>(1), 3-1) << std::endl;
 	//chebpoly<Scalar>(n);
 
+
+
+
 	// 13.
 	
-//	auto f = chebyshev::chebfun<float>([]<typename T>(T x) {return sin(x);});
-    auto f = chebyshev::chebfun<float>(std::sin);
-	// auto M = chebyshev::chebmat<float>(5);
-	//std::cout <<  << std::endl;
+
+	//	auto f = chebyshev::chebfun<float>([]<typename T>(T x) {return sin(x);});
+	//	auto f = chebyshev::chebfun<float>(std::sin);
+	auto M = chebyshev::chebmat<float>(5);
+	std::cout << M << std::endl;
 
 	auto A = chebyshev::chebmat<float>(3);
 	blas::vector<Scalar> x(3);
