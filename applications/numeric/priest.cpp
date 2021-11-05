d1a02ff360621ce897e5a17a39270424259bd3b7
Ravenwater <theo@stillwater-sc.com>
2021-11-05 14:13:58 -0400

    adding Douglas Priest arbitrary precision arithmetic experiment

---
+++ applications/numeric/priest.cpp
@@ -0,0 +1,115 @@
+// priest.cpp: experiments with Douglas Priest arbitrary precision floating-point arithmetic
+//
+// Copyright (C) 2017-2021 Stillwater Supercomputing, Inc.
+//
+// This file is part of the universal numbers project, which is released under an MIT Open Source license.
+#include <universal/utility/directives.hpp>
+#include <limits>
+#include <utility>
+#if (__cplusplus == 202003L) || (_MSVC_LANG == 202003L)
+#include <numbers>    // high-precision numbers
+#endif
+#include <universal/utility/number_system_properties.hpp> //minmax_range etc. for native types
+#include <universal/common/numeric_limits_utility.hpp>
+#include <universal/verification/performance_runner.hpp>
+
+// select the number systems we would like to compare
+#include <universal/number/fixpnt/fixpnt.hpp>
+#include <universal/number/areal/areal.hpp>
+#include <universal/number/cfloat/cfloat.hpp>
+#include <universal/number/posit/posit.hpp>
+#include <universal/number/lns/lns.hpp>
+
+/*
+Definition of FAITHFUL arithmetic
+   For a t-digit number a and b, and op element {+,-,*,/}, let c = a op b exactly.
+   Suppose x and y are consecutive t-digit floating-point numbers with the same 
+   sign as c such at |x| <= |c| < |y|. Then the floating-point arithmetic is
+   called faithful if fl(a op b) = x whenever c = x and fl(a op b) is either x or y
+   whenever c != x.
+
+ */
+
+template<typename Real>
+std::pair<Real, Real> SumErr(Real a, Real b) {
+	Real c, d, e, f, g, h;
+
+	if (abs(a) < abs(b)) std::swap(a, b);
+	c = a + b;
+	e = c - a;
+	g = c - e;
+	h = g - a;
+	f = b - h;
+	d = f - e;
+
+	return std::pair(c, d);
+}
+
+int main()
+try {
+	using namespace sw::universal;
+
+	std::cout << "Douglas Priest arbitrary precision arithmetic experiments\n";
+
+	using Sngle = float;
+	using Longd = long double;
+	using Fixed = fixpnt<32,16>;
+	using Posit = posit<32,2>;
+	using Float = cfloat<32, 8, uint32_t>;
+	using Areal = areal<32, 8, uint32_t>;
+	using Lns   = lns<32, uint32_t>;
+
+
+	std::streamsize precision = std::cout.precision();
+	std::cout << std::setprecision(std::numeric_limits<Longd>::max_digits10);
+	std::cout << "float       digits of precision : " << std::numeric_limits<float>::max_digits10 << '\n';
+	std::cout << "double      digits of precision : " << std::numeric_limits<double>::max_digits10 << '\n';
+	std::cout << "long double digits of precision : " << std::numeric_limits<Longd>::max_digits10 << '\n';
+
+	Float fa, fb;
+	Sngle a, b, c, d;
+
+	fa = 1.0f;
+	++fa;
+	fb = 1.0f;
+	--fb;
+	a = Sngle(fa);
+	b = Sngle(fb);
+	auto result = SumErr(a, b);
+	c = result.first;
+	d = result.second;
+	std::cout << c << " + " << d << " = " << a << " + " << b << '\n';
+	std::cout << to_binary(c) << " : " << to_binary(a + b) << '\n';
+	std::cout << to_binary(d) << '\n';
+	std::cout << "a : " << to_binary(a) << '\n';
+	std::cout << "b : " << to_binary(b) << '\n';
+	std::cout << "c : " << to_binary(c) << '\n';
+	std::cout << "d : " << to_binary(d) << '\n';
+
+
+
+	std::cout << std::setprecision(precision);
+	std::cout << std::endl;
+	
+	return EXIT_SUCCESS;
+}
+catch (char const* msg) {
+	std::cerr << "Caught exception: " << msg << std::endl;
+	return EXIT_FAILURE;
+}
+catch (const sw::universal::universal_arithmetic_exception& err) {
+	std::cerr << "Caught unexpected universal arithmetic exception: " << err.what() << std::endl;
+	return EXIT_FAILURE;
+}
+catch (const sw::universal::universal_internal_exception& err) {
+	std::cerr << "Caught unexpected universal internal exception: " << err.what() << std::endl;
+	return EXIT_FAILURE;
+}
+catch (const std::runtime_error& err) {
+	std::cerr << "Caught runtime exception: " << err.what() << std::endl;
+	return EXIT_FAILURE;
+}
+catch (...) {
+	std::cerr << "Caught unknown exception" << std::endl;
+	return EXIT_FAILURE;
+}
