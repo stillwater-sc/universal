cc593b332718785882fefc3de94d1f3bedff41b1
jamesquinlan <jquinlan@une.edu>
2021-11-17 15:41:09 -0500

    vec2mat semantics

--- applications/chebyshev/chebtests.cpp
+++ applications/chebyshev/chebtests.cpp
@@ -152,8 +152,8 @@ try {
 >>>>>>> wip
 
 	auto A = chebyshev::chebmat<float>(3);
-	blas::vector<Scalar> x(3);
-	x(0)=6;x(1)=-5;x(2)=1;
+	blas::matrix<Scalar> x(3,1);
+	x(0,0)=6;x(1,0)=-5;x(2,0)=1;
 	std::cout << A << x << std::endl;
 
 <<<<<<< HEAD
