7630f11f1b0a0e4e2eebd60736de18c5ac68df5d
Ravenwater <theo@stillwater-sc.com>
2021-12-05 16:41:52 -0500

    creating native type tags that are uniform across all compile environments

--- include/universal/native/manipulators.hpp
+++ include/universal/native/manipulators.hpp
@@ -12,8 +12,30 @@ namespace sw::universal {
 		typename = typename std::enable_if< std::is_floating_point<Real>::value, Real >::type
 	>
 	std::string type_tag(const Real f) {
+		constexpr size_t nbits = ieee754_parameter<Real>::nbits;
+		std::string realType;
+		switch (nbits) {
+		case 16:
+			realType = "half";
+			break;
+		case 32:
+			realType = "float";
+			break;
+		case 64:
+			realType = "double";
+			break;
+		case 80:
+			realType = "extended precision";
+			break;
+		case 128:
+			realType = "quad";
+			break;
+		default:
+			realType = "unknown";
+		}
 		std::stringstream s;
-		s << typeid(Real).name();
+		// s << typeid(Real).name();  gcc and clang obfuscate the native types
+		s << realType;
 		if (f == 0.0) s << ' '; // to get rid of unreferenced formal parameter warning
 		return s.str();
 	}
@@ -23,8 +45,6 @@ namespace sw::universal {
 		typename = typename std::enable_if< std::is_floating_point<Real>::value, Real >::type
 	>
 	std::string type_tag() {
-//		constexpr size_t nbits = ieee754_parameter<Real>::nbits;
-//		constexpr size_t es = ieee754_parameter<Real>::ebits;
 		Real f(0);
 		return type_tag(f);
 	}
