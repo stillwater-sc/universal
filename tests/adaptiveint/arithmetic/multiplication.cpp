2d275c817949fc2adf416f0bb5c22035f1d09930
Ravenwater <theo@stillwater-sc.com>
2022-04-17 16:05:21 -0400

    adding test suites skeletons

---
+++ tests/adaptiveint/arithmetic/multiplication.cpp
@@ -0,0 +1,111 @@
+// multiplication.cpp: test suite runner for multiplicationon adaptive precision binary integers
+//
+// Copyright (C) 2017-2022 Stillwater Supercomputing, Inc.
+//
+// This file is part of the universal numbers project, which is released under an MIT Open Source license.
+#include <universal/utility/directives.hpp>
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cmath>
+#include <limits>
+
+// minimum set of include files to reflect source code dependencies
+#include <universal/number/adaptiveint/adaptiveint.hpp>
+#include <universal/verification/test_reporters.hpp>
+
+// generate specific test case that you can trace with the trace conditions in mpreal.hpp
+// for most bugs they are traceable with _trace_conversion and _trace_add
+template<typename Ty, typename BlockType = std::uint32_t>
+void GenerateTestCase(Ty _a, Ty _b) {
+	Ty ref;
+	sw::universal::adaptiveint<BlockType> a, b, aref, aproduct;
+	a = _a;
+	b = _b;
+	aproduct = a * b;
+	ref = _a * _b;
+	aref = ref;
+	constexpr size_t ndigits = 30;
+	std::cout << std::setw(ndigits) << _a << " * " << std::setw(ndigits) << _b << " = " << std::setw(ndigits) << ref << std::endl;
+//	std::cout << a << " * " << b << " = " << aproduct << " (reference: " << aref << ")   " ;
+	std::cout << to_binary(a) << " * " << to_binary(b) << " = " << to_binary(aproduct) << " : " << (long long)(aproduct) << " (reference: " << to_binary(aref) << ")   ";
+	std::cout << (aref == aproduct? "PASS" : "FAIL") << std::endl << std::endl;
+}
+
+// Regression testing guards: typically set by the cmake configuration, but MANUAL_TESTING is an override
+#define MANUAL_TESTING 1
+// REGRESSION_LEVEL_OVERRIDE is set by the cmake file to drive a specific regression intensity
+// It is the responsibility of the regression test to organize the tests in a quartile progression.
+//#undef REGRESSION_LEVEL_OVERRIDE
+#ifndef REGRESSION_LEVEL_OVERRIDE
+#undef REGRESSION_LEVEL_1
+#undef REGRESSION_LEVEL_2
+#undef REGRESSION_LEVEL_3
+#undef REGRESSION_LEVEL_4
+#define REGRESSION_LEVEL_1 1
+#define REGRESSION_LEVEL_2 1
+#define REGRESSION_LEVEL_3 0
+#define REGRESSION_LEVEL_4 0
+#endif
+
+int main()
+try {
+	using namespace sw::universal;
+
+	std::string test_suite = "adaptive precision binary integer multiplication";
+	std::string test_tag = "adaptiveint multiplication";
+	bool reportTestCases = true;
+	int nrOfFailedTestCases = 0;
+
+	std::cout << test_suite << '\n';
+
+#if MANUAL_TESTING
+//	bool bReportIndividualTestCases = false;
+
+	// generate individual testcases to hand trace/debug
+//	GenerateTestCase(1, 2);
+
+	ReportTestSuiteResults(test_suite, nrOfFailedTestCases);
+	return EXIT_SUCCESS; // ignore failures
+#else
+
+#if REGRESSION_LEVEL_1
+
+#endif
+
+#if REGRESSION_LEVEL_2
+
+#endif
+
+#if REGRESSION_LEVEL_3
+
+#endif
+
+#if REGRESSION_LEVEL_4
+
+#endif
+
+	ReportTestSuiteResults(test_suite, nrOfFailedTestCases);
+	return (nrOfFailedTestCases > 0 ? EXIT_FAILURE : EXIT_SUCCESS);
+#endif  // MANUAL_TESTING
+}
+catch (char const* msg) {
+	std::cerr << "Caught ad-hoc exception: " << msg << std::endl;
+	return EXIT_FAILURE;
+}
+catch (const sw::universal::universal_arithmetic_exception& err) {
+	std::cerr << "Caught unexpected universal arithmetic exception : " << err.what() << std::endl;
+	return EXIT_FAILURE;
+}
+catch (const sw::universal::universal_internal_exception& err) {
+	std::cerr << "Caught unexpected universal internal exception: " << err.what() << std::endl;
+	return EXIT_FAILURE;
+}
+catch (const std::runtime_error& err) {
+	std::cerr << "Uncaught runtime exception: " << err.what() << std::endl;
+	return EXIT_FAILURE;
+}
+catch (...) {
+	std::cerr << "Caught unknown exception" << '\n';
+	return EXIT_FAILURE;
+}
