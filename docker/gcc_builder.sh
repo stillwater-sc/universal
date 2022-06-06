0e7b1bd5e2685ce28634fce8cb6681f941175713
Ravenwater <theo@stillwater-sc.com>
2022-06-06 08:35:28 -0400

    adding documentation to test script

--- docker/gcc_builder.sh
+++ docker/gcc_builder.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
 # script to create a GCC builder container to compile and test Universal with GCC
+# Usage: ./gcc_builder.sh BUILD_ALL
+#        ./gcc_builder.sh BUILD_APPLICATIONS
+#        etc. for each available build target
+# 
+# to be target specific, the Dockerfile turns off the default configuration that builds the demonstration programs
 
 # pick up the build target: default argument is set in the Dockerfile
 TARGET=$1
