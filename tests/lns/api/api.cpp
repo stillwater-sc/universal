fa91e6c64f19e4517f8763cfb5c9d168708c510a
Ravenwater <theo@stillwater-sc.com>
2022-07-14 18:54:34 -0400

    WIP: creating a common arithmetic behavior configuration enum

--- tests/lns/api/api.cpp
+++ tests/lns/api/api.cpp
@@ -9,15 +9,6 @@
 #include <universal/number/cfloat/cfloat.hpp>  // bit field comparisons
 #include <universal/verification/test_suite.hpp>
 
-template<size_t nbits> 
-int VerifyAddition(bool reportTestCases) {
-	int nrOfFailedTestCases = 0;
-
-	if (reportTestCases) std::cout << '\n';
-
-	return nrOfFailedTestCases;
-}
-
 // Regression testing guards: typically set by the cmake configuration, but MANUAL_TESTING is an override
 #define MANUAL_TESTING 1
 // REGRESSION_LEVEL_OVERRIDE is set by the cmake file to drive a specific regression intensity
