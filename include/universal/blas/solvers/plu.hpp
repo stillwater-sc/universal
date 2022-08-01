6845dfefa02934d8ec98eb8a2de5d84679cd249c
jamesquinlan <jquinlan@une.edu>
2022-08-01 10:10:36 -0400

    Codacy improvement

--- include/universal/blas/solvers/plu.hpp
+++ include/universal/blas/solvers/plu.hpp
@@ -1,8 +1,8 @@
 #pragma once
-// lu.hpp: dense matrix PLU decomposition
+// plu.hpp: dense matrix PLU decomposition (PA = LU)
 //
 // Copyright (C) 2017-2021 Stillwater Supercomputing, Inc.
-// Quinlan, J.
+// James Quinlan
 //
 // This file is part of the universal numbers project, which is released under an MIT Open Source license.
 
@@ -21,10 +21,8 @@ std::tuple<matrix<Scalar>, matrix<Scalar>, matrix<Scalar>> plu(const matrix<Scal
     using Matrix = sw::universal::blas::matrix<Scalar>;
     using namespace std;
 
+    Scalar x;
     size_t n = num_rows(A);
-    Scalar absmax; // absmax = abs(A(i,j))
-    size_t argmax; // index of max element
-
     Matrix P(n,n);
     Matrix L(n,n);
     Matrix U(n,n);
@@ -33,16 +31,13 @@ std::tuple<matrix<Scalar>, matrix<Scalar>, matrix<Scalar>> plu(const matrix<Scal
     L = 1;
     U = A;
 
-    size_t i = 0, j = 0, k = 0;
-    Scalar x;
-
     // Elimination Process
-    for (i = 0; i < n-1; ++i){ // i-th row
-        absmax = abs(U(i,i));
-        argmax = i;
+    for (size_t i = 0; i < n-1; ++i){ // i-th row
+        Scalar absmax = abs(U(i,i)); 
+        size_t argmax = i;
 
         // Find largest element in ith column
-        for (k = i + 1; k < n; ++k){ // i = subsequent row (ele. in column k)
+        for (size_t k = i + 1; k < n; ++k){ // i = subsequent row (ele. in column k)
             if (abs(U(k,i)) > absmax){
                 absmax = abs(U(k,i));
                 argmax = k;
@@ -63,7 +58,7 @@ std::tuple<matrix<Scalar>, matrix<Scalar>, matrix<Scalar>> plu(const matrix<Scal
             
         }
         // Continue with row reduction
-        for (k = i + 1; k < n; ++k){  // objective row
+        for (size_t k = i + 1; k < n; ++k){  // objective row
             L(k,i) = U(k,i) / U(i,i);
             for (j = i; j < n; ++j){
                 U(k,j) = U(k,j) - L(k,i)*U(i,j);
