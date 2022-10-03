6e48d05016dea7ded1b3503e754890f1f666f881
Ravenwater <theo@stillwater-sc.com>
2022-10-03 10:19:46 -0400

    skeletons for fast implementations of standard posits

---
+++ include/universal/number/posit/specialized/posit_64_2.hpp
@@ -0,0 +1,41 @@
+#pragma once
+// posit_64_2.hpp: specialized 64-bit posit using fast compute specialized for posit<64,2>
+//
+// Copyright (C) 2017-2022 Stillwater Supercomputing, Inc.
+//
+// This file is part of the universal numbers project, which is released under an MIT Open Source license.
+
+// DO NOT USE DIRECTLY!
+// the compile guards in this file are only valid in the context of the specialization logic
+// configured in the main <universal/posit/posit>
+
+#ifndef POSIT_FAST_POSIT_64_2
+#define POSIT_FAST_POSIT_64_2 0
+#endif
+
+	// guard for the fact that we don't have a specialization yet
+#if POSIT_FAST_POSIT_64_2
+#undef POSIT_FAST_POSIT_64_2
+#define POSIT_FAST_POSIT_64_2 0
+#pragma message("Fast specialization of posit<64,2> requested but ignored as fast implemention is TBD")
+#endif
+
+namespace sw { namespace universal {
+
+	// test the fast specialization variable to indicate that we are running a special template specialization
+#if POSIT_FAST_POSIT_64_2
+#ifdef _MSC_VER
+#pragma message("Fast specialization of posit<64,2>")
+//#else
+//#warning("Fast specialization of posit<64,2>")
+#endif
+
+// fast specialized posit<64,2>
+// TODO
+class posit<NBITS_IS_64, EBITS_IS_2> {
+
+};
+
+#endif // POSIT_FAST_POSIT_64_2
+
+}} // namespace sw::universal
