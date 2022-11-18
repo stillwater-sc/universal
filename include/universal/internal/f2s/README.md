04a06e10c88768199cf227d3845ff51baf7fd855
Ravenwater <theo@stillwater-sc.com>
2022-11-17 21:59:18 -0500

    introducing floating-point 2 string class for Grisu algorithms

---
+++ include/universal/internal/f2s/README.md
@@ -0,0 +1,4 @@
+# Simplified floating-point for printing floating-point numbers
+
+The Dragon and Grisu algorithms to print decimal representation strings of binary floating-point
+depend on a simplified floating-point facility presented in the GFP directory
--- internal/f2s/api/api.cpp
+++ internal/f2s/api/api.cpp
@@ -1,19 +1,19 @@
 //  api.cpp : test suite runner for the class interface of the simplified floating-point type
 //
-// Copyright (C) 2017-2022 Stillwater Supercomputing, Inc.
+// Copyright (C) 2022-2022 Stillwater Supercomputing, Inc.
 //
 // This file is part of the universal numbers project, which is released under an MIT Open Source license.
 #include <universal/utility/directives.hpp>
 #include <universal/utility/long_double.hpp>
 #include <universal/number/cfloat/cfloat.hpp>
-#include <universal/internal/gfp/gfp.hpp>
+#include <universal/internal/f2s/f2s.hpp>
 #include <universal/verification/test_suite.hpp>
 
 int main()
 try {
 	using namespace sw::universal;
 
-	std::string test_suite  = "gfp API validation";
+	std::string test_suite  = "f2s API validation";
 	std::string test_tag    = "API";
 	bool reportTestCases    = true;
 	int nrOfFailedTestCases = 0;
@@ -24,7 +24,7 @@ try {
 
 
 	{
-		gfp<uint32_t> a, b, c;
+		f2s<uint32_t> a, b, c;
 		a = 1.0e0f;
 		std::cout << to_binary(a) << '\n';
 		b = 1.0e0f;
@@ -33,51 +33,15 @@ try {
 	}
 
 	{
-		gfp<uint64_t> a, b, c;
+		f2s<uint64_t> a, b, c;
 		a = 1.0e0;
 		std::cout << to_binary(a) << '\n';
 		b = 1.0e0;
 		c = a + b;
 		std::cout << a << " * " << b << " = " << c << '\n';
 	}
-
-	{
-		gfp<uint64_t> a, b, c;
-		a.set(false, 0, 0xf'ffff'ffffull, 52); // emulate a double
-		b.set(false, 0, 0x1'ffff'ffffull, 52); // emulate a double);
-		std::cout << to_hex(b.significant()) << '\n';
-		c = a * b;
-		std::cout << a << " * " << b << " = " << c << '\n';
-		int alpha = 0;
-		std::cout << "alpha : " << alpha << "  k : " << c.calculate_k(alpha) << '\n';
-	}
-
-	{
-		int alpha = 0;
-		for (int binaryScale = -10; binaryScale <= 64; ++binaryScale) {
-			std::cout << "binaryScale : " << binaryScale << " vs decimalScale : " << decimalScale(binaryScale, 64, alpha) << '\n';
-		}
-	}
-
-	{
-		using Scalar = std::uint32_t;
-		gfp<Scalar> a;
-		a = 1.0f;
-		std::cout << to_binary(a) << " : " << float(a) << '\n';
-
-		a = 0.03125f;
-		std::cout << to_binary(a) << " : " << float(a) << '\n';
-	}
-
-	{
-		using Scalar = std::uint64_t;
-		gfp<Scalar> a;
-		a = 1.0;
-		std::cout << to_binary(a) << " : " << double(a) << '\n';
-
-		a = 0.03125;
-		std::cout << to_binary(a) << " : " << double(a) << '\n';
-	}
+	return 0;
+	
 
 	{
 		half f{ 0.03125 };
@@ -85,6 +49,18 @@ try {
 
 		duble d{ 0.0312 };
 		std::cout << "floating-point value : " << to_binary(d) << " : " << d << " : " << to_triple(d) << '\n';
+	}
+
+	{
+		f2s<uint64_t> a;
+		// print out the cached powers
+		for (int mk = 0; mk < 88; ++mk) {
+			CachedPower cp = CachedPowers[mk];
+			std::cout << mk << " : " << to_binary(cp.significand, 64, true) << " : ";
+			a.set(false, cp.binary_exponent, cp.significand);
+			std::cout << double(a) << '\n';
+
+		}
 	}
 
 	{
