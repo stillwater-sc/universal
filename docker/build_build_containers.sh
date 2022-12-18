1c40ed8a4aec00483b591c8b2b8991da99ffb172
Ravenwater <theo@stillwater-sc.com>
2022-12-18 08:27:56 -0500

    streamlining build containers

---
+++ docker/build_build_containers.sh
@@ -0,0 +1,24 @@
+# script file to generate the docker build containers with specific compilers installed
+# precondition: docker login success
+
+# GCC compiler containers
+docker build --target gcc9builder -t stillwater/universal:gcc9builder -f Dockerfile.gcc9builder .
+docker push stillwater/universal:gcc9builder
+docker build --target gcc10builder -t stillwater/universal:gcc10builder -f Dockerfile.gcc10builder .
+docker push stillwater/universal:gcc10builder
+docker build --target gcc11builder -t stillwater/universal:gcc11builder -f Dockerfile.gcc11builder .
+docker push stillwater/universal:gcc11builder
+docker build --target gcc12builder -t stillwater/universal:gcc12builder -f Dockerfile.gcc12builder .
+docker push stillwater/universal:gcc12builder
+
+# CLang compiler containers
+docker build --target clang11builder -t stillwater/universal:clang11builder -f Dockerfile.clang11builder .
+docker push stillwater/universal:clang11builder
+docker build --target clang12builder -t stillwater/universal:clang12builder -f Dockerfile.clang12builder .
+docker push stillwater/universal:clang12builder
+docker build --target clang13builder -t stillwater/universal:clang13builder -f Dockerfile.clang13builder .
+docker push stillwater/universal:clang13builder
+docker build --target clang14builder -t stillwater/universal:clang14builder -f Dockerfile.clang14builder .
+docker push stillwater/universal:clang14builder
+docker build --target clang15builder -t stillwater/universal:clang15builder -f Dockerfile.clang15builder .
+docker push stillwater/universal:clang15builder
