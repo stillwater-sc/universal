5fa34790a7ae721282fc4b0646aad6084f32167f
Ravenwater <theo@stillwater-sc.com>
2023-04-23 08:50:19 -0400

    expanding type_tag to integral types to compensate for gcc and clang symbol mangling

--- include/universal/native/manipulators.hpp
+++ include/universal/native/manipulators.hpp
@@ -1,7 +1,7 @@
 #pragma once
 // manipulators.hpp: definition of manipulation functions for native types
 //
-// Copyright (C) 2017-2022 Stillwater Supercomputing, Inc.
+// Copyright (C) 2017-2023 Stillwater Supercomputing, Inc.
 //
 // This file is part of the universal numbers project, which is released under an MIT Open Source license.
 #include <string>
@@ -9,23 +9,23 @@
 namespace sw { namespace universal {
 
 	template<typename IntegralType, 
-		std::enable_if_t< std::is_integral<IntegralType>::value, bool> = true
+		std::enable_if_t< ::std::is_integral<IntegralType>::value, bool> = true
 	>
 	std::string type_tag(IntegralType = {}) {
 		// can't use a simple typeid(Real).name() because gcc and clang obfuscate the native types
 		constexpr unsigned nbits = sizeof(IntegralType) * 8;
 		std::string type_string;
 		if constexpr (nbits == 8) {
-			type_string = std::string("char");
+			type_string = ::std::is_signed<IntegralType>::value ? std::string("int8_t") : std::string("uint8_t");
 		}
 		else if constexpr (nbits == 16) {
-			type_string = std::string("short");
+			type_string = ::std::is_signed<IntegralType>::value ? std::string("int16_t") : std::string("uint16_t");
 		}
 		else if constexpr (nbits == 32) {
-			type_string = std::string("long");
+			type_string = ::std::is_signed<IntegralType>::value ? std::string("int32_t") : std::string("uint32_t");
 		}
 		else if constexpr (nbits == 64) {
-			type_string = std::string("long long");
+			type_string = ::std::is_signed<IntegralType>::value ? std::string("int64_t") : std::string("uint64_t");
 		}
 		else {
 			type_string = std::string("unknown");
@@ -34,7 +34,7 @@ namespace sw { namespace universal {
 	}
 
 	template<typename RealType,
-		std::enable_if_t< std::is_floating_point<RealType>::value, bool> = true
+		std::enable_if_t< ::std::is_floating_point<RealType>::value, bool> = true
 	>
 	std::string type_tag(RealType = {}) {
 		// can't use a simple typeid(Real).name() because gcc and clang obfuscate the native types
