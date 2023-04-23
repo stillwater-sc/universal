9fef1dfcfd631032d9a35343022a0d2ab1be1422
Ravenwater <theo@stillwater-sc.com>
2023-04-23 10:07:26 -0400

    bug fix on float bit manipulation

--- static/native/float/bit_manipulation.cpp
+++ static/native/float/bit_manipulation.cpp
@@ -16,28 +16,23 @@ namespace sw { namespace universal {
 
 	template<typename Real,
 		typename = typename std::enable_if< std::is_floating_point<Real>::value, Real >::type>
-	int VerifyFloatFieldExtraction(bool reportTestCases) {
+	int VerifyRealFieldExtraction(bool reportTestCases) {
 		using namespace sw::universal;
+
+		constexpr unsigned fbits = ieee754_parameter<Real>::fbits;
+
 		int nrOfFailedTests = 0;
 
 		bool sign{ false };
 		std::uint64_t rawExponent{ 0 };
 		int exponent{ 0 };
 		std::uint64_t rawFraction{ 0 };
-		int fbits{ 64 };
-
+		
 		Real a;
 		a = 1.0;
+		std::cout << to_binary(a) << " : " << a << '\n';
+		ReportValue(a);
 
-		if constexpr (sizeof(Real) == 4) {
-			fbits = 23;
-		}
-		else if constexpr (sizeof(Real) == 8) {
-			fbits = 52;
-		}
-		else if constexpr (sizeof(Real) == 16) {
-			fbits = 64;
-		}
 		extractFields(a, sign, rawExponent, rawFraction);
 		exponent = static_cast<int>(rawExponent) - ieee754_parameter<Real>::bias;
 		if (sign != false || exponent != 0 || rawFraction != 0) {
@@ -49,33 +44,6 @@ namespace sw { namespace universal {
 	}
 
 } } // namespace sw::universal
-
-union float_decoder {
-	float_decoder() : f{ 0.0f } {}
-	float_decoder(float _f) : f{ _f } {}
-	float f;
-	struct {
-		uint32_t fraction : 23;
-		uint32_t exponent : 8;
-		uint32_t sign : 1;
-	} parts;
-};
-
-template<typename Integer,
-	typename = typename std::enable_if< std::is_integral<Integer>::value, Integer >::type
->
-inline std::string to_binary(const Integer& number, int nbits = 0, bool bNibbleMarker = true) {
-	std::stringstream s;
-	if (nbits == 0) nbits = 8 * sizeof(number);
-	s << 'b';
-	uint64_t mask = (uint64_t(1) << (nbits - 1));
-	for (int i = int(nbits) - 1; i >= 0; --i) {
-		s << ((number & mask) ? '1' : '0');
-		if (bNibbleMarker && i > 0 && i % 4 == 0) s << '\'';
-		mask >>= 1;
-	}
-	return s.str();
-}
 
 // Regression testing guards: typically set by the cmake configuration, but MANUAL_TESTING is an override
 #define MANUAL_TESTING 0
@@ -99,7 +67,7 @@ try {
 
 	std::string test_suite  = "native IEEE-754 bit manipulation verification";
 	std::string test_tag    = "floating-point field extraction";
-	bool reportTestCases    = false;
+	bool reportTestCases    = true;
 	int nrOfFailedTestCases = 0;
 
 	ReportTestSuiteHeader(test_suite, reportTestCases);
@@ -114,22 +82,31 @@ try {
 	decoder.parts.sign = 0b1;
 
 	std::cout << decoder.f << '\n';
-#ifdef BIT_CAST_SUPPORT
-	uint32_t bc = std::bit_cast<uint32_t, float>(decoder.f);
+
+	uint32_t bc = sw::bit_cast<uint32_t, float>(decoder.f);
 	std::cout << to_binary(bc, 32) << '\n';
-#endif
-	
-	nrOfFailedTestCases += ReportTestResult(VerifyFloatFieldExtraction<float>(reportTestCases), "float", test_tag);
+
+	float f{1.0f};	
+	std::cout << "size of float       : " << sizeof(f) << '\n';
+		ReportValue(f);
+	double d{1.0};	
+	std::cout << "size of double      : " << sizeof(d) << '\n';
+		ReportValue(d);
+	long double ld{1.0l};	
+	std::cout << "size of long double : " << sizeof(ld) << '\n';
+		ReportValue(ld);
+
+	nrOfFailedTestCases += ReportTestResult(VerifyRealFieldExtraction<float>(reportTestCases), "float", test_tag);
 
 	ReportTestSuiteResults(test_suite, nrOfFailedTestCases);
 	return EXIT_SUCCESS; // ignore failures
 #else
 
 #if REGRESSION_LEVEL_1
-	nrOfFailedTestCases += ReportTestResult(VerifyFloatFieldExtraction<float>(reportTestCases), "float", test_tag);
-	nrOfFailedTestCases += ReportTestResult(VerifyFloatFieldExtraction<double>(reportTestCases), "double", test_tag);
+	nrOfFailedTestCases += ReportTestResult(VerifyRealFieldExtraction<float>(reportTestCases), "float", test_tag);
+	nrOfFailedTestCases += ReportTestResult(VerifyRealFieldExtraction<double>(reportTestCases), "double", test_tag);
 #if LONG_DOUBLE_SUPPORT
-	nrOfFailedTestCases += ReportTestResult(VerifyFloatFieldExtraction<long double>(reportTestCases), "long double", test_tag);
+	nrOfFailedTestCases += ReportTestResult(VerifyRealFieldExtraction<long double>(reportTestCases), "long double", test_tag);
 #endif
 
 #endif
