7dfc63d49a9ef08570ad55813922a2b3aa30e4c8
Ravenwater <theo@stillwater-sc.com>
2023-05-05 16:06:45 -0400

    cleaning up backsub with FDP

--- include/universal/blas/solvers/backsub.hpp
+++ include/universal/blas/solvers/backsub.hpp
@@ -32,19 +32,20 @@ Vector backsub(const Matrix& A, const Vector& b) {
 }
 
 
-template<unsigned nbits, unsigned es>
+template<unsigned nbits, unsigned es, unsigned capacity = 10>
 vector<posit<nbits,es>> backsub(const matrix<posit<nbits,es>> & A, const vector<posit<nbits,es>>& b) {
-	size_t n = size(b);
-    using Vector = vector<posit<nbits,es>>;
-    constexpr unsigned capacity = 10;
+    using Scalar = posit<nbits, es>;
+    using Vector = vector<Scalar>;
+    using Quire  = quire<nbits,es,capacity>;
+	unsigned n = static_cast<unsigned>(size(b));
 
     Vector x(n);
-	for (int i = n-1; i >=0; --i){
-        quire<nbits,es,capacity> q{0};
-        for (int j = i; j < n; ++j){
+	for (int i = n-1; i >=0; --i) {
+        Quire q{0};
+        for (unsigned j = i; j < n; ++j) {
             q += quire_mul(A(i,j), x(j));
         }
-        posit<nbits,es> y;
+        Scalar y;
         convert(q.to_value(), y); 
         x(i) = (b(i) - y)/A(i,i);
     }
