d525f2f41593528cbdaa91d22fadd3a8979a913a
Ravenwater <theo@stillwater-sc.com>
2023-06-04 07:06:55 -0400

    augmenting the skeleton structure to best known practice

--- include/universal/number/skeleton_1param/oneparam.hpp
+++ include/universal/number/skeleton_1param/oneparam.hpp
@@ -1,37 +1,37 @@
-// arbitrary logarithmic number arithmetic type standard header
+// arbitrary one-parameter arithmetic type standard header
 //
-// Copyright (C) 2017-2021 Stillwater Supercomputing, Inc.
+// Copyright (C) 2017-2023 Stillwater Supercomputing, Inc.
 //
 // This file is part of the universal numbers project, which is released under an MIT Open Source license.
-#ifndef _LNS_STANDARD_HEADER_
-#define _LNS_STANDARD_HEADER_
+#ifndef _ONEPARAM_STANDARD_HEADER_
+#define _ONEPARAM_STANDARD_HEADER_
 
 ////////////////////////////////////////////////////////////////////////////////////////
 ///  BEHAVIORAL COMPILATION SWITCHES
 
 ////////////////////////////////////////////////////////////////////////////////////////
 // enable/disable the ability to use literals in binary logic and arithmetic operators
-#if !defined(LNS_ENABLE_LITERALS)
+#if !defined(ONEPARAM_ENABLE_LITERALS)
 // default is to enable them
-#define LNS_ENABLE_LITERALS 1
+#define ONEPARAM_ENABLE_LITERALS 1
 #endif
 
 ////////////////////////////////////////////////////////////////////////////////////////
 // enable throwing specific exceptions for logarithmic number system arithmetic errors
 // left to application to enable
-#if !defined(LNS_THROW_ARITHMETIC_EXCEPTION)
+#if !defined(ONEPARAM_THROW_ARITHMETIC_EXCEPTION)
 // default is to use std::cerr for signalling an error
-#define LNS_THROW_ARITHMETIC_EXCEPTION 0
+#define ONEPARAM_THROW_ARITHMETIC_EXCEPTION 0
 #endif
 
 ////////////////////////////////////////////////////////////////////////////////////////
 /// INCLUDE FILES that make up the library
-#include "lns.hpp"
-#include "numeric_limits.hpp"
-#include "exceptions.hpp"
+#include <universal/number/oneparam/oneparam_impl.hpp>
+#include <universal/number/oneparam/numeric_limits.hpp>
+#include <universal/number/oneparam/exceptions.hpp>
 
 ///////////////////////////////////////////////////////////////////////////////////////
 /// math functions
-#include "math_functions.hpp"
+#include <universal/number/oneparam/mathlib.hpp>
 
 #endif
