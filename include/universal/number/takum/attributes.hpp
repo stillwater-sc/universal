fc124abfb00ee73a61b7dee634b13cdb8dc861b6
Ravenwater <theo@stillwater-sc.com>
2024-03-31 10:34:58 -0400

    SFINAE manipulators for takum

--- include/universal/number/takum/attributes.hpp
+++ include/universal/number/takum/attributes.hpp
@@ -5,13 +5,13 @@
 // SPDX-License-Identifier: MIT
 //
 // This file is part of the universal numbers project, which is released under an MIT Open Source license.
-#include <cmath> // for std:pow()
 
 namespace sw { namespace universal {
 
 // functions to provide details about
 // the properties of the number system configuration
 // in terms of native types.
+
 // Since many number system configurations cannot be represented by 
 // native types, these are all convenience functions.
 // They should not be used for the core algorithms.
@@ -33,6 +33,43 @@ std::string takum_range(const takum<nbits, bt>& v = {}) {
 		<< v.minpos() << " ... "
 		<< v.maxpos() << " ]";
 	return s.str();
+}
+
+
+// report dynamic range of a type, specialized for a takum
+template<unsigned nbits, typename bt>
+std::string dynamic_range(const takum<nbits, bt>& a) {
+	std::stringstream s;
+	takum<nbits, bt> b(SpecificValue::maxneg), c(SpecificValue::minneg), d(SpecificValue::minpos), e(SpecificValue::maxpos);
+	s << type_tag(a) << ": ";
+	s << "minpos scale " << std::setw(10) << d.scale() << "     ";
+	s << "maxpos scale " << std::setw(10) << e.scale() << '\n';
+	s << "[" << b << " ... " << c << ", -0, +0, " << d << " ... " << e << "]\n";
+	s << "[" << to_binary(b) << " ... " << to_binary(c) << ", -0, +0, " << to_binary(d) << " ... " << to_binary(e) << "]\n";
+	takum<nbits, bt> ninf(SpecificValue::infneg), pinf(SpecificValue::infpos);
+	s << "inclusive range = (" << to_binary(ninf) << ", " << to_binary(pinf) << ")\n";
+	s << "inclusive range = (" << ninf << ", " << pinf << ")\n";
+	return s.str();
+}
+
+
+
+template<unsigned nbits, typename bt>
+int minpos_scale(const takum<nbits, bt>& b) {
+	takum<nbits, bt> c(b);
+	return c.minpos().scale();
+}
+
+template<unsigned nbits, typename bt>
+int maxpos_scale(const takum<nbits, bt>& b) {
+	takum<nbits, bt> c(b);
+	return c.maxpos().scale();
+}
+
+template<unsigned nbits, typename bt>
+int max_negative_scale(const takum<nbits, bt>& b) {
+	takum<nbits, bt> c(b);
+	return c.maxneg().scale();
 }
 
 }} // namespace sw::universal
--- include/universal/number/takum/manipulators.hpp
+++ include/universal/number/takum/manipulators.hpp
@@ -49,13 +49,110 @@ namespace sw { namespace universal {
 		return inside;
 	}
 
+
+	// Generate a string representing the cfloat components: sign, exponent, faction and value
+	template<typename TakumType,
+		std::enable_if_t< is_takum<TakumType>, bool> = true
+	>
+	inline std::string components(const TakumType& v) {
+		constexpr unsigned es    = TakumType::es;
+		constexpr unsigned fbits = TakumType::fbits;
+		using bt = typename TakumType::BlockType;
+
+		bool sign{ false };
+		blockbinary<es, bt> e;
+		blockbinary<fbits, bt> f;
+		decode(v, sign, e, f);
+
+		// TODO: hardcoded field width is governed by pretty printing cfloat tables, which by construction will always be small cfloats
+		std::stringstream s; 
+		s << std::setw(14) << to_binary(v)
+			<< " Sign : " << std::setw(2) << sign
+			<< " Exponent : " << std::setw(5) << e
+			<< " Fraction : " << std::setw(8) << f
+			<< " Value : " << std::setw(16) << v;
+
+		return s.str();
+	}
+
+	// generate a binary string for cfloat
+	template<typename TakumType,
+		std::enable_if_t< is_takum<TakumType>, bool> = true
+	>
+	inline std::string to_hex(const TakumType& v, bool nibbleMarker = false, bool hexPrefix = true) {
+		constexpr unsigned nbits = TakumType::nbits; 
+		constexpr char hexChar[16] = {
+			'0', '1', '2', '3', '4', '5', '6', '7',
+			'8', '9', 'A', 'B', 'C', 'D', 'E', 'F',
+		};
+		std::stringstream s;
+		if (hexPrefix) s << "0x" << std::hex;
+		int nrNibbles = int(1ull + ((nbits - 1ull) >> 2ull));
+		for (int n = nrNibbles - 1; n >= 0; --n) {
+			uint8_t nibble = v.nibble(unsigned(n));
+			s << hexChar[nibble];
+			if (nibbleMarker && n > 0 && (n % 4) == 0) s << '\'';
+		}
+		return s.str();
+	}
+
+	template<typename TakumType,
+		std::enable_if_t< is_takum<TakumType>, bool> = true
+	>
+	inline std::string hex_print(const TakumType& c) {
+		constexpr unsigned nbits = TakumType::nbits;
+		
+		std::stringstream s;
+		s << nbits << 'x' << to_hex(c) << 't';
+		return s.str();
+	}
+
+	template<typename TakumType,
+		std::enable_if_t< is_takum<TakumType>, bool> = true
+	>
+	inline std::string pretty_print(const TakumType& number, bool nibbleMarker = false) {
+		std::stringstream s;
+		// sign bit
+		s << (number.sign() ? '1' : '0');
+		s << '.';
+		// direction bit
+		bool D = number.direct();
+		s << (D ? '1' : '0');
+		s << '.';
+		// regime field
+		int bit = static_cast<int>(TakumType::nbits) - 3;
+		for (int i = 0; (i < 3) && (bit >= 0); ++i) {
+			s << (number.at(static_cast<unsigned>(bit--)) ? '1' : '0');
+		}
+		s << '.';
+		// exponent field
+		unsigned regime = number.regime();
+		int r = static_cast<int>(D ? regime : 7 - regime);
+		for (int i = r - 1; i >= 0 && bit >= 0; --i) {
+			s << (number.at(static_cast<unsigned>(bit--)) ? '1' : '0');
+			if (i > 0 && (i % 4) == 0 && nibbleMarker) s << '\'';
+		}
+		s << '.';
+		// fraction field
+		while (bit >= 0) {
+			s << (number.at(static_cast<unsigned>(bit)) ? '1' : '0');
+			if (bit > 0 && (bit % 4) == 0 && nibbleMarker) s << '\'';
+			--bit;
+		}
+		return s.str();
+	}
+
+	template<typename TakumType,
+		std::enable_if_t< is_takum<TakumType>, bool> = true
+	>
+	inline std::string info_print(const TakumType& p, int printPrecision = 17) {
+		return std::string("TBD");
+	}
+
 	template<typename TakumType,
 		std::enable_if_t< is_takum<TakumType>, bool> = true
 	>
 	inline std::string color_print(const TakumType& number, bool nibbleMarker = false) {
-
-		std::stringstream s;
-
 		Color red(ColorCode::FG_RED);
 		Color yellow(ColorCode::FG_YELLOW);
 		Color blue(ColorCode::FG_BLUE);
@@ -64,9 +161,15 @@ namespace sw { namespace universal {
 		Color cyan(ColorCode::FG_CYAN);
 		Color white(ColorCode::FG_WHITE);
 		Color def(ColorCode::FG_DEFAULT);
+
+
+		std::stringstream s;
+		// sign bit
 		s << red << (number.sign() ? '1' : '0');
+		// direction bit
 		bool D = number.direct();
 		s << green << (D ? '1' : '0');
+		// regime field
 		s << yellow;
 		int bit = static_cast<int>(TakumType::nbits) - 3;
 		for (int i = 0; (i < 3) && (bit >= 0); ++i) {
