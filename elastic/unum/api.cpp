113be9957dd6a380a72854f461662bb2ab21df00
Ravenwater <theo@stillwater-sc.com>
2024-07-25 10:46:48 -0400

    header updates for elastic unum

--- elastic/unum/api.cpp
+++ elastic/unum/api.cpp
@@ -1,6 +1,7 @@
 // api.cpp: class interface tests for arbitrary configuration unum types
 //
-// Copyright (C) 2017-2021 Stillwater Supercomputing, Inc.
+// Copyright (C) 2017 Stillwater Supercomputing, Inc.
+//  SPDX-License-Identifier: MIT
 //
 // This file is part of the universal numbers project, which is released under an MIT Open Source license.
 #include <iostream>
