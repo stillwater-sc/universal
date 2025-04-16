c0328997c20a944c7e4c13b3d9c84baa976a2757
C-Wirth <151962189+C-Wirth@users.noreply.github.com>
2025-04-16 17:03:59 -0400

    Upated pull request for closure plots functions after rebase (#451)
    * changes made to error.hpp and viewing.ipynb
    
    * lots of refactoring with closure plot function
    - added closure_of_8_bit_systems.cpp
    - renamed and refactored draw_closure_plots.ipynb
    - Refactored generateClosurePlots.cpp to a header file to align with the libray
    the file is now called generateClosurePlots.hpp
    
    * saving progress before rebase
    
    * Addressed review comments
    
    * last changes made to error.hpp for PR #451

---
+++ playground/generate_a_closure_map.cpp
@@ -0,0 +1,54 @@
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <filesystem>
+#include <universal/number/posit/posit.hpp>
+#include <universal/number/cfloat/cfloat.hpp>
+#include <universal/number/lns/lns.hpp> 
+#include <universal/utility/generateClosurePlots.hpp>
+
+
+/// Version 13 April 2025
+/// 
+/// About:
+/// This file allows the user to generate a .txt and .csv file for a closure mapping associated with a specified Real number system.
+///
+/// The closure plot can then be built from draw_closure_plots.ipynb *edits must be made to the last driver function there
+///
+///The user must configure the system below.
+
+
+///////////////////////////////     CONFIGURE Real NUMBER SYSTEM BELOW       ///////////////////////////////
+namespace sw::universal{
+constexpr unsigned NBITS{ 2 };
+constexpr unsigned ES{ 4 };
+using Real= posit<NBITS, ES>; 
+std::string type = "posit";
+///////////////////////////////     DONT EDIT BELOW THIS LINE       ///////////////////////////////
+
+template int buildClosurePlot<Real>(std::string, std::ostream&, std::ostream&);
+}
+
+/// <summary>
+/// This utility calls the buildclosureplot() function of genereateClosurePlots.hpp 
+/// 
+/// the generated closure plots can be found in the dir: build/mappings/user_generated
+/// </summary>
+int main() {
+    using namespace sw::universal;
+
+    //handle the name and output directory
+    std::string sys_name_str = type + "_" + std::to_string(NBITS) + "_" + std::to_string(ES); 
+    std::filesystem::path mappings{"mappings/user_generated/"};
+    std::filesystem::create_directories(mappings / sys_name_str); 
+    
+    //output files
+    std::ofstream sys_txt_file(mappings / sys_name_str / (sys_name_str + ".txt")); 
+    std::ofstream sys_csv_file(mappings / sys_name_str / (sys_name_str + ".csv"));
+
+    //actual function call
+    buildClosurePlot<Real>(sys_name_str, sys_txt_file, sys_csv_file);
+
+    return EXIT_SUCCESS;
+}
