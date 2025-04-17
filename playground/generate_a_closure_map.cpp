a9d7193f9335b5a24248e225aebd7a48db16c801
Ravenwater <theo@stillwater-sc.com>
2025-04-17 13:05:18 -0400

    WIP: restructuring closure plot machinery

--- playground/generate_a_closure_map.cpp
+++ playground/generate_a_closure_map.cpp
@@ -1,4 +1,12 @@
 
+// x_over_one_minus_x.hpp: generic implementation of the function x / (1 - x)
+//
+// Copyright (C) 2017 Stillwater Supercomputing, Inc.
+// SPDX-License-Identifier: MIT
+//
+// Author: Colby Wirth
+// 
+// This file is part of the universal numbers project, which is released under an MIT Open Source license.
 #include <iostream>
 #include <string>
 #include <fstream>
@@ -6,37 +14,33 @@
 #include <universal/number/posit/posit.hpp>
 #include <universal/number/cfloat/cfloat.hpp>
 #include <universal/number/lns/lns.hpp> 
-#include <universal/utility/generateClosurePlots.hpp>
-
+#include <universal/utility/generateClosurePlots.hpp>   // contains buildClosurePlot<ArithmeticType>() function
 
 /// Version 13 April 2025
 /// 
 /// About:
-/// This file allows the user to generate a .txt and .csv file for a closure mapping associated with a specified Real number system.
+/// This file allows the user to generate a .txt and .csv file for a closure mapping 
+/// associated with a specified Real number system.
 ///
-/// The closure plot can then be built from draw_closure_plots.ipynb *edits must be made to the last driver function there
+/// The closure plot can then be built from draw_closure_plots.ipynb 
+/// *edits must be made to the last driver function there
 ///
-///The user must configure the system below.
 
 
-///////////////////////////////     CONFIGURE Real NUMBER SYSTEM BELOW       ///////////////////////////////
-namespace sw::universal{
-constexpr unsigned NBITS{ 2 };
-constexpr unsigned ES{ 4 };
-using Real= posit<NBITS, ES>; 
-std::string type = "posit";
-///////////////////////////////     DONT EDIT BELOW THIS LINE       ///////////////////////////////
-
-template int buildClosurePlot<Real>(std::string, std::ostream&, std::ostream&);
-}
 
 /// <summary>
-/// This utility calls the buildclosureplot() function of genereateClosurePlots.hpp 
+/// Generate .txt and .csv files containing the data to construct a closure map
+/// for the specified number system.
 /// 
-/// the generated closure plots can be found in the dir: build/mappings/user_generated
+/// the generated closure plots can be found in the dir: ./build/mappings/user_generated
 /// </summary>
 int main() {
     using namespace sw::universal;
+
+    constexpr unsigned NBITS{ 8 };
+    constexpr unsigned ES{ 2 };
+    using Real = posit<NBITS, ES>;
+    std::string type = "posit";
 
     //handle the name and output directory
     std::string sys_name_str = type + "_" + std::to_string(NBITS) + "_" + std::to_string(ES); 
