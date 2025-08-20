ade71f8ea482d35682cf5b2a919fa0a3084f9d39
Theodore Omtzigt <theo@stillwater-sc.com>
2025-08-20 10:42:05 -0400

    header edits

--- include/sw/universal/number/bfloat/math/pow.hpp
+++ include/sw/universal/number/bfloat/math/pow.hpp
@@ -1,7 +1,8 @@
 #pragma once
 // pow.hpp: pow functions for brain floating-point
 //
-// Copyright (C) 2017-2022 Stillwater Supercomputing, Inc.
+// Copyright (C) 2017 Stillwater Supercomputing, Inc.
+// SPDX-License-Identifier: MIT
 //
 // This file is part of the universal numbers project, which is released under an MIT Open Source license.
 #include <cmath>
