58811d753b1acc2e32b88205bc39916ba410acbd
Theodore Omtzigt <theo@stillwater-sc.com>
2025-08-26 08:45:26 -0400

    WIP: code hygiene for docker build scripts

--- docker/build_release_container.sh
+++ docker/build_release_container.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+set -euo pipefile
+export DOCKER_BUILDKIT=1
+export COMPOSE_DOCKER_CLI_BUILD=1
 
 # To turn off security features use:
 # docker run --security-opt seccomp:unconfined ...
@@ -8,11 +11,13 @@ MAJOR=v3
 MINOR=85
 VERSION="$MAJOR.$MINOR"
 
-if [[ $# == 0 ]]; then
+if [[ $# -eq 0 ]]; then
 	# default is to build with GCC 13
-	docker build --force-rm -t "stillwater/universal:$VERSION" -t stillwater/universal:latest -f "Dockerfile.gcc13" ..
+	docker build --pull --force-rm -t "stillwater/universal:$VERSION" -t stillwater/universal:latest -f "Dockerfile.gcc13" ..
 else 
 	# pick up the compiler to use
 	COMPILER=$1
-	docker build --force-rm -t "stillwater/universal:$VERSION" -t stillwater/universal:latest -f "Dockerfile.$COMPILER" ..
+	df="Dockerfile.$COMPILER"
+	[[ -f "$df" ]] || { echo "Dockerfile '$df' not found"; exit 1; }
+	docker build --pull --force-rm -t "stillwater/universal:$VERSION" -t stillwater/universal:latest -f "$df" ..
 fi
--- docker/build_test_container.sh
+++ docker/build_test_container.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+set -euo pipefail
+export DOCKER_BUILDKIT=1
+export COMPOSE_DOCKER_CLI_BUILD=1
 
 # script to create a test container with a specific compiler and build target
 # Usage: ./build_test_container.sh COMPILER TARGET
@@ -14,16 +17,16 @@ MAJOR=v3
 MINOR=85
 VERSION="$MAJOR.$MINOR"
 
-if [[ $# == 0 ]]; then
+if [[ $# -eq 0 ]]; then
 	# default is to build with Clang 15
-	docker build --force-rm -t "stillwater/universal:$VERSION" -t stillwater/universal:latest -f Dockerfile.clang15 ..
+	docker build --force-rm -t "stillwater/universal:${VERSION}-test" -t stillwater/universal:latest-test -f Dockerfile.clang15 ..
 else 
 	# pick up the compiler to use
 	COMPILER=$1
-	if [[ $# == 2 ]]; then
+	if [[ $# -eq 2 ]]; then
 		TARGET=$2
 	else
 		TARGET=BUILD_ALL
 	fi
-	docker build --force-rm -t "stillwater/universal:$VERSION" --build-arg "target=$TARGET" -f "Dockerfile.$COMPILER" ..
+	docker build --force-rm -t "stillwater/universal:${VERSION}-test" --build-arg "target=$TARGET" -f "Dockerfile.$COMPILER" ..
 fi
