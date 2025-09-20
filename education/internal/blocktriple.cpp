d8a7db4dbd5f11b9be04f40391ea7a6ea0a39c25
Theodore Omtzigt <theo@stillwater-sc.com>
2025-09-20 08:28:08 -0400

    WIP: adding blocktriple multiply demonstration

--- education/internal/blocktriple.cpp
+++ education/internal/blocktriple.cpp
@@ -28,15 +28,29 @@ int main() {
         std::cout << "Example 2: Addition with Different Scales\n";
         std::cout << "-----------------------------------------\n";
 
-		constexpr unsigned fbits = 23; // emulating single precision
-		constexpr unsigned baseBits = fbits - 4; // BlockTripleOperator::ADD creates a normalized mantissa of hiddenbit + fbits + 3 guard bits for rounding, we need to use fbits-4 here to get the radix on the right place
-        blocktriple<fbits-4, BlockTripleOperator::ADD, uint32_t> a, b, sum;
-		a.set(false, 0, 0x0040'0000ULL, false, false); // 1.0
-		b.set(false, 0, 0x0044'0000ULL, false, false); // 1.0625
-        std::cout << "a    : " << to_binary(a, true) << " : " << a << '\n';
-        std::cout << "b    : " << to_binary(b, true) << " : " << b << '\n';
+        /*
+        * The algorithm that blocktriple uses is to set up the arithmetic operation
+		* such that the result has enough bits to capture all rounding and overflow information.
+		* Thus the sequence is:
+        *  1. 'normalize' the input arguments to fit in the target format.
+		*  2. execute the arithmetic operation
+		*  3. scale and round the result to the target format
+        */
+		constexpr unsigned fbits = 23; // number of fraction bits in the target floating-point format
+        // BlockTripleOperator::ADD creates a normalized mantissa of 3 integer bits + fbits + 3 guard bits for rounding
+		// the 3 integer bits are required to capture 2's complement results of overflow
+		// addition and subtraction are the most complex operations as they require scale alignment
+		// addition/subtraction is done in 2's complement
+        blocktriple<fbits, BlockTripleOperator::ADD, uint32_t> a, b, sum;
+		a.set(false, 0, 0x0400'0000ULL, false, false);  // 1.0
+		b.set(false, -4, 0x0440'0000ULL, false, false); // 1.0625 * 2^-4 = 0.06640625
+        std::cout << "a       : " << to_binary(a, true) << " : " << a << '\n';
+        std::cout << "b       : " << to_binary(b, true) << " : " << b << '\n';
         sum.add(a, b);
-        std::cout << "sum  : " << to_binary(sum, true) << " : " << sum << '\n';
+        std::cout << "sum     : " << to_binary(sum, true) << " : " << sum << '\n';
+		// convert to single precision floating-point
+		// convert() would need to do all the heavy lifting of rounding and normalization
+		// as reference, convert() for cfloat is about 200 lines of code
         std::cout << std::endl;
     }
 
@@ -45,9 +59,19 @@ int main() {
         std::cout << "Example 3: Multiplication Scaling\n";
         std::cout << "---------------------------------\n";
 
-        blocktriple<23, BlockTripleOperator::MUL, uint32_t> multiplicand, multiplier, product;
-
-
+        constexpr unsigned fbits = 10; // number of fraction bits in the target half precision floating-point format
+        // BlockTripleOperator::MUL creates a normalized mantissa of 2*fbits + 2 capacity bits for overflow
+		// multiplication is done in 1's complement
+        blocktriple<fbits, BlockTripleOperator::MUL, uint32_t> a, b, product;
+		a.set(false, 0, 0x00'0400ULL, false, false); // 1.0  in half precision
+		b.set(false, 0, 0x00'0440ULL, false, false); // 1.0625 in half precision
+		std::cout << "a       : " << to_binary(a, true) << '\n';  // given the radix point, the 'value' of a is not 1.0
+        std::cout << "b       : " << to_binary(b, true) << '\n';
+        product.mul(a, b);
+        std::cout << "product : " << to_binary(product, true) << " : " << product << '\n';
+		// convert to half precision floating-point
+		// convert() would need to do all the heavy lifting of rounding and normalization
+		// as reference, convert() for cfloat is about 200 lines of code
         std::cout << std::endl;
     }
 
