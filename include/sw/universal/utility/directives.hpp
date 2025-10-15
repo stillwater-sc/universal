fe1508a09fac05ad82cf5e92bc4c98be03783b5b
Theodore Omtzigt <theo@stillwater-sc.com>
2025-10-15 18:13:00 -0400

    Further code hygiene for GCC and Clang by making #warning message optional

--- include/sw/universal/utility/directives.hpp
+++ include/sw/universal/utility/directives.hpp
@@ -35,21 +35,29 @@
 // Macro for consistent compile-time messages across compilers
 // Usage: UNIVERSAL_COMPILER_MESSAGE("Fast specialization of posit<8,0>")
 //
+// To enable these messages, define UNIVERSAL_VERBOSE_BUILD before including headers
+// or add -DUNIVERSAL_VERBOSE_BUILD to compiler flags
+//
 // MSVC: Uses #pragma message for clean output
-// GCC/Clang: Uses #warning directive for clean warning output (not verbose notes)
+// GCC/Clang: Uses #warning directive for warning output
 // Other: No-op
 
-#if defined(_MSC_VER)
-	// MSVC: Use pragma message
-	#define UNIVERSAL_COMPILER_MESSAGE(msg) __pragma(message("Universal: " msg))
-#elif defined(__GNUC__) || defined(__clang__)
-	// GCC/Clang: Use #warning directive for clean output
-	// The _Pragma trick allows us to use #warning from within a macro
-	#define STRINGIZE_IMPL(x) #x
-	#define STRINGIZE(x) STRINGIZE_IMPL(x)
-	#define UNIVERSAL_COMPILER_MESSAGE(msg) _Pragma(STRINGIZE(GCC warning msg))
+#ifdef UNIVERSAL_VERBOSE_BUILD
+	#if defined(_MSC_VER)
+		// MSVC: Use pragma message
+		#define UNIVERSAL_COMPILER_MESSAGE(msg) __pragma(message("Universal: " msg))
+	#elif defined(__GNUC__) || defined(__clang__)
+		// GCC/Clang: Use #warning directive for clean output
+		// The _Pragma trick allows us to use #warning from within a macro
+		#define STRINGIZE_IMPL(x) #x
+		#define STRINGIZE(x) STRINGIZE_IMPL(x)
+		#define UNIVERSAL_COMPILER_MESSAGE(msg) _Pragma(STRINGIZE(GCC warning msg))
+	#else
+		// Other compilers: no-op
+		#define UNIVERSAL_COMPILER_MESSAGE(msg)
+	#endif
 #else
-	// Other compilers: no-op
+	// Verbose build not enabled: no-op
 	#define UNIVERSAL_COMPILER_MESSAGE(msg)
 #endif
 
