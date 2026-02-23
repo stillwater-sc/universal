ffda79d595408610ed7ec17a67bdbce3076c6479
Theodore Omtzigt <theo@stillwater-sc.com>
2026-02-23 14:20:12 -0500

    fix headers in SORN tests

--- static/sorn/arithmetic/division.cpp
+++ static/sorn/arithmetic/division.cpp
@@ -1,6 +1,7 @@
 // division.cpp: test suite runner for division arithmetic on SORNs
 //
-// Copyright (C) 2017-2022 Stillwater Supercomputing, Inc.
+// Copyright (C) 2017 Stillwater Supercomputing, Inc.
+// SPDX-License-Identifier: MIT
 //
 // This file is part of the universal numbers project, which is released under an MIT Open Source license.
 
