84e3d809f7756837010a0976916f7261098d4af3
Theodore Omtzigt <theo@stillwater-sc.com>
2026-04-13 11:07:01 -0400

    feat(edecimal): adding an performance test (#707)

--- elastic/decimal/performance/performance.cpp
+++ elastic/decimal/performance/performance.cpp
@@ -13,6 +13,97 @@
 // minimum set of include files to reflect source code dependencies
 #include <universal/number/edecimal/edecimal.hpp>
 #include <universal/verification/test_status.hpp>
+#include <universal/benchmark/performance_runner.hpp>
+
+namespace sw::universal::internal {
+
+	// Generic set of adds and subtracts for a given number system type
+	template<typename Scalar>
+	void AdditionSubtractionWorkload(size_t NR_OPS) {
+	Scalar a{0}, b{0}, c{0}, d{0};
+	    a = "1234567890123456789012345";
+	    b = a;
+		// we are doing two adds per loop, so half the NR_OPS
+		NR_OPS >>= 1;
+		for (size_t i = 0; i < NR_OPS; ++i) {
+			c = a + b;
+			d = c - a;
+		}
+		//		std::cout << a << ' ' << b << ' ' << c << ' ' << d << '\n';
+		if (d == a)
+			std::cout << " ";
+		else
+			std::cout << "-";
+	}
+
+	// Generic set of multiplies for a given number system type
+	template<typename Scalar>
+	void MultiplicationWorkload(size_t NR_OPS) {
+		Scalar a{0}, b{0}, c{0}, d{0};
+	    a = "1234567890123456789012345";
+	    b = 10;
+	    d = a * b;
+		for (size_t i = 0; i < NR_OPS; ++i) {
+			c = a * b;
+		}
+		//		std::cout << a << ' ' << b << ' ' << c << ' ' << d << '\n';
+		if (c == d)
+			std::cout << " ";
+		else
+			std::cout << "-";
+	}
+
+	// Generic set of divides for a given number system type
+	template<typename Scalar>
+	void DivisionWorkload(size_t NR_OPS) {
+		Scalar a{0}, b{0}, c{0}, d{0};
+	    a = "1234567890123456789012345";
+	    b = 5;
+	    d = a / b;
+		for (size_t i = 0; i < NR_OPS; ++i) {
+			c = a / b;
+		}
+		//		std::cout << a << ' ' << b << ' ' << c << ' ' << d << '\n';
+		if (c == d)
+			std::cout << " ";
+		else
+			std::cout << "-";
+	}
+
+	// Generic set of remainders for a given number system type
+	template<typename Scalar>
+	void RemainderWorkload(size_t NR_OPS) {
+		Scalar a{0}, b{0}, c{0}, d{0};
+	    a = "1234567890123456789012345";
+	    b = 5;
+		for (size_t i = 0; i < NR_OPS; ++i) {
+			c = a % b;
+		}
+		//		std::cout << a << ' ' << b << ' ' << c << ' ' << d << '\n';
+		if (c == 0)
+			std::cout << " ";
+		else
+			std::cout << "-";
+	}
+
+	void TestArithmeticOperatorPerformance() {
+	    using namespace sw::universal;
+	    std::cout << "\nArithmetic operator performance\n";
+
+	    size_t NR_OPS = 128ull * 1024ull * 4ull;
+	    PerformanceRunner("edecimal 25 digits   add/subtract  ", AdditionSubtractionWorkload<sw::universal::edecimal>, NR_OPS);
+
+	    NR_OPS = 64ull * 1024ull;
+	    PerformanceRunner("edecimal 25 digits   multiplication", MultiplicationWorkload<sw::universal::edecimal>, NR_OPS);
+
+	    NR_OPS = 16ull * 1024ull;
+	    PerformanceRunner("edecimal 25 digits   division      ", DivisionWorkload<sw::universal::edecimal>, NR_OPS);
+
+	    NR_OPS = 16ull * 1024ull;
+	    PerformanceRunner("edecimal 25 digits   remainder     ", RemainderWorkload<sw::universal::edecimal>, NR_OPS);
+    }
+
+}  // namespace sw::universal::internal
 
 // Regression testing guards: typically set by the cmake configuration, but MANUAL_TESTING is an override
 #define MANUAL_TESTING 1
@@ -42,6 +133,8 @@ try {
 	std::cout << test_suite << '\n';
 
 #if MANUAL_TESTING
+
+	internal::TestArithmeticOperatorPerformance();
 
 #else
 
