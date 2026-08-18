7f1572716c80ac923470321c3d5d4702e4351c64
mikemikimike <13286568797@163.com>
2026-08-18 09:41:16 +0800

    fix: return zero when dividing by infinity (#1337)
    * fix: return zero when dividing by infinity
    
    * fix: preserve NaN for infinity division cases
    
    * test: cover constexpr infinity division signs

--- include/sw/universal/number/td_cascade/td_cascade_impl.hpp
+++ include/sw/universal/number/td_cascade/td_cascade_impl.hpp
@@ -344,6 +344,13 @@ public:
             return *this;
         if (rhs.isnan())
             return *this = rhs;
+        if (rhs.isinf()) {
+            if (isinf())
+                return *this = td_cascade(SpecificValue::qnan);
+            cascade[0] = (cascade.isneg() == rhs.cascade.isneg()) ? 0.0 : -0.0;
+            cascade[1] = cascade[2] = 0.0;
+            return *this;
+        }
         if (rhs.iszero()) {
             if (iszero()) {
                 *this = td_cascade(SpecificValue::qnan);
